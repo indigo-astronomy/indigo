80133529f0caeeffefe99c271256200f532fdd8e
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2016-10-14 20:40:05 +0200

    again

--- indigo_bus/indigo_driver_xml.c
+++ indigo_bus/indigo_driver_xml.c
@@ -164,8 +164,6 @@ static indigo_result xml_device_adapter_update_property(indigo_client *client, i
       break;
     case INDIGO_BLOB_VECTOR:
       if (client->enable_blob != INDIGO_ENABLE_BLOB_NEVER) {
-        handle2 = dup(handle);
-        fh = fdopen(handle2,"w");
         indigo_xml_printf(handle, "<setBLOBVector device='%s' name='%s' state='%s'%s>\n", property->device, indigo_property_name(client->version, property), indigo_property_state_text[property->state], message_attribute(message));
         if (property->state == INDIGO_OK_STATE) {
           for (int i = 0; i < property->count; i++) {
@@ -173,12 +171,14 @@ static indigo_result xml_device_adapter_update_property(indigo_client *client, i
             long input_length = item->blob.size;
             unsigned char *data = item->blob.value;
             indigo_xml_printf(handle, "<oneBLOB name='%s' format='%s' size='%ld'>\n", indigo_item_name(client->version, property, item), item->blob.format, item->blob.size);
+            handle2 = dup(handle);
+            fh = fdopen(handle2, "w");
             if (property->version >= INDIGO_VERSION_2_0) {
               while (input_length) {
                 #define RAW_BUF_SIZE 98304
                 #define BASE64_BUF_SIZE 131072
                 char encoded_data[BASE64_BUF_SIZE];
-                long len = (RAW_BUF_SIZE < input_length) ?  RAW_BUF_SIZE : input_length;                
+                long len = (RAW_BUF_SIZE < input_length) ?  RAW_BUF_SIZE : input_length;
                 long enclen = base64_encode((unsigned char*)encoded_data, (unsigned char*)data, len);
                 indigo_xml_fwrite(fh, encoded_data, enclen);
                 input_length -= len;
@@ -196,11 +196,10 @@ static indigo_result xml_device_adapter_update_property(indigo_client *client, i
                 data += len;
               }
             }
+            fflush(fh);
+            fclose(fh);
             indigo_xml_printf(handle, "</oneBLOB>\n");
           }
-          fflush(fh);
-          fclose(fh);
-          indigo_xml_printf(handle, "</oneBLOB>\n");
         }
         indigo_xml_printf(handle, "</setBLOBVector>\n");
       }
