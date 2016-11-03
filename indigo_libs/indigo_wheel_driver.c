8286772c2ef71a0b7522a52f1b132d041df93936
Rumen G.Bogdanovski <rumen@skyarchive.org>
2016-11-03 21:42:13 +0200

    indigo_wheel_driver: change min value to 1

--- indigo_libs/indigo_wheel_driver.c
+++ indigo_libs/indigo_wheel_driver.c
@@ -50,7 +50,7 @@ indigo_result indigo_wheel_attach(indigo_device *device, indigo_version version)
 			WHEEL_SLOT_PROPERTY = indigo_init_number_property(NULL, device->name, "WHEEL_SLOT", WHEEL_MAIN_GROUP, "Current slot", INDIGO_IDLE_STATE, INDIGO_RW_PERM, 1);
 			if (WHEEL_SLOT_PROPERTY == NULL)
 				return INDIGO_FAILED;
-			indigo_init_number_item(WHEEL_SLOT_ITEM, "SLOT", "Slot number", 0, 9, 1, 0);
+			indigo_init_number_item(WHEEL_SLOT_ITEM, "SLOT", "Slot number", 1, 9, 1, 0);
 			// -------------------------------------------------------------------------------- WHEEL_SLOT_NAME
 			WHEEL_SLOT_NAME_PROPERTY = indigo_init_text_property(NULL, device->name, "WHEEL_SLOT_NAME", WHEEL_MAIN_GROUP, "Slot names", INDIGO_IDLE_STATE, INDIGO_RW_PERM, WHEEL_SLOT_ITEM->number.max);
 			if (WHEEL_SLOT_NAME_PROPERTY == NULL)
