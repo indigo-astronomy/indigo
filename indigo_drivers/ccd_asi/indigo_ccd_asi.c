c1736a6a04ecdf258e381aefd8adeb5512ca9d4e
Rumen G.Bogdanovski <rumenastro@gmail.com>
2016-11-24 21:43:45 +0200

    indigo_ccd_asi: still broken:)

--- indigo_drivers/ccd_asi/indigo_ccd_asi.c
+++ indigo_drivers/ccd_asi/indigo_ccd_asi.c
@@ -112,7 +112,8 @@ static bool asi_start_exposure(indigo_device *device, double exposure, bool dark
 	pthread_mutex_lock(&PRIVATE_DATA->usb_mutex);
 
 	//start exposure - NEEDS MUCH MORE
-	res = ASISetROIFormat(id, frame_width, frame_height,  horizontal_bin, ASI_IMG_RAW16);
+	//res = ASISetROIFormat(id, frame_width, frame_height,  horizontal_bin, ASI_IMG_RAW16);
+	res = ASISetROIFormat(id, PRIVATE_DATA->info.MaxWidth, PRIVATE_DATA->info.MaxHeight,  1, ASI_IMG_RAW16);
 	if (res) {
 		pthread_mutex_unlock(&PRIVATE_DATA->usb_mutex);
 		INDIGO_LOG(indigo_log("indigo_ccd_asi: ASISetROIFormat(%d) = %d", id, res));
@@ -151,7 +152,7 @@ static bool asi_read_pixels(indigo_device *device) {
 		INDIGO_LOG(indigo_log("indigo_ccd_asi: ASIGetDataAfterExp(%d) = %d", PRIVATE_DATA->dev_id, res));
 		return false;
 	}
-
+INDIGO_LOG(indigo_log("indigo_ccd_asi: ASIGetDataAfterExp(%d) = %d", PRIVATE_DATA->dev_id, res));
 	pthread_mutex_unlock(&PRIVATE_DATA->usb_mutex);
 	return true;
 }
@@ -248,6 +249,14 @@ static indigo_result ccd_attach(indigo_device *device) {
 	device->device_context = NULL;
 	if (indigo_ccd_attach(device, DRIVER_VERSION) == INDIGO_OK) {
 		DEVICE_CONTEXT->private_data = private_data;
+
+		if (PRIVATE_DATA->is_camera) {
+			CCD_MODE_PROPERTY->perm = INDIGO_RW_PERM;
+			CCD_MODE_PROPERTY->count = 0;
+			CCD_INFO_WIDTH_ITEM->number.value = PRIVATE_DATA->info.MaxWidth;
+			CCD_INFO_HEIGHT_ITEM->number.value = PRIVATE_DATA->info.MaxHeight;
+			CCD_INFO_BITS_PER_PIXEL_ITEM->number.value = 0;
+		}
 
 		// adjust info know before opening camera...
 		// make hidden properties visible if needed
