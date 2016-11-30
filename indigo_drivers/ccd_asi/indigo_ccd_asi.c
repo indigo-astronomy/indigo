981f3f2e11ffb48b08545c4bd70f354fbef831f7
Rumen G.Bogdanovski <rumenastro@gmail.com>
2016-11-30 16:04:54 +0200

    indigo_ccd_asi: untie start cooler from set targtet temp

--- indigo_drivers/ccd_asi/indigo_ccd_asi.c
+++ indigo_drivers/ccd_asi/indigo_ccd_asi.c
@@ -765,11 +765,11 @@ static indigo_result ccd_change_property(indigo_device *device, indigo_client *c
 		if (CONNECTION_CONNECTED_ITEM->sw.value && !CCD_COOLER_PROPERTY->hidden) {
 			PRIVATE_DATA->target_temperature = CCD_TEMPERATURE_ITEM->number.value;
 			CCD_TEMPERATURE_ITEM->number.value = PRIVATE_DATA->current_temperature;
-			if (CCD_COOLER_OFF_ITEM->sw.value) {
+			/* if (CCD_COOLER_OFF_ITEM->sw.value) {
 				indigo_set_switch(CCD_COOLER_PROPERTY, CCD_COOLER_ON_ITEM, true);
 				CCD_COOLER_PROPERTY->state = INDIGO_BUSY_STATE;
 				indigo_update_property(device, CCD_COOLER_PROPERTY, NULL);
-			}
+			} */
 			CCD_TEMPERATURE_PROPERTY->state = INDIGO_BUSY_STATE;
 			indigo_update_property(device, CCD_TEMPERATURE_PROPERTY, NULL);
 		}
