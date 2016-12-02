5ba1b7cf2cb9f7137abe331d8a710b0a264533a5
Rumen G.Bogdanovski <rumen@skyarchive.org>
2016-12-02 23:14:03 +0200

    indigo_ccd_asi: reverted to caps.Name

--- indigo_drivers/ccd_asi/indigo_ccd_asi.c
+++ indigo_drivers/ccd_asi/indigo_ccd_asi.c
@@ -648,7 +648,7 @@ static indigo_result init_camera_property(indigo_device *device, ASI_CONTROL_CAP
 	assert(offset < MAX_ASI_ADVANCED_ITEMS);
 	res = ASISetControlValue(id, ctrl_caps.ControlType, ctrl_caps.DefaultValue, false);
 	if (res) INDIGO_LOG(indigo_log("indigo_ccd_asi: ASISetControlValue(%d, %s) = %d", id, ctrl_caps.Name, res));
-	indigo_init_number_item(ASI_ADVANCED_PROPERTY->items+offset, ctrl_caps.Name, ctrl_caps.Description, ctrl_caps.MinValue, ctrl_caps.MaxValue, 1, ctrl_caps.DefaultValue);
+	indigo_init_number_item(ASI_ADVANCED_PROPERTY->items+offset, ctrl_caps.Name, ctrl_caps.Name, ctrl_caps.MinValue, ctrl_caps.MaxValue, 1, ctrl_caps.DefaultValue);
 	return INDIGO_OK;
 }
 
