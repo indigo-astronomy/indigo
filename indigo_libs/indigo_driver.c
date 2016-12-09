4de0ddd8d976bd1f9ce23af15fba32e383f2b396
Rumen G.Bogdanovski <rumenastro@gmail.com>
2016-12-09 16:00:41 +0200

    indigo_driver: fix inclusion of libusb.h

--- indigo_libs/indigo_driver.c
+++ indigo_libs/indigo_driver.c
@@ -44,6 +44,7 @@
 #elif defined(INDIGO_FREEBSD)
 #include <libusb.h>
 #elif defined(INDIGO_LINUX)
+#include <libusb-1.0/libusb.h>
 #include <linux/serial.h>
 #include <sys/ioctl.h>
 #include <dirent.h>
