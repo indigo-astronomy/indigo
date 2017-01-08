10dff1c22cadeedf92425c290417a6ef2297cfb2
Rumen G.Bogdanovski <rumenastro@gmail.com>
2017-01-08 19:38:09 +0200

    indigo_ccd_asi: display target temp in property message

--- indigo_drivers/ccd_asi/indigo_ccd_asi.c
+++ indigo_drivers/ccd_asi/indigo_ccd_asi.c
@@ -761,7 +761,7 @@ static indigo_result ccd_change_property(indigo_device *device, indigo_client *c
 				indigo_update_property(device, CCD_COOLER_PROPERTY, NULL);
 			} */
 			CCD_TEMPERATURE_PROPERTY->state = INDIGO_BUSY_STATE;
-			indigo_update_property(device, CCD_TEMPERATURE_PROPERTY, NULL);
+			indigo_update_property(device, CCD_TEMPERATURE_PROPERTY, "Target Temperature = %.2f", PRIVATE_DATA->target_temperature);
 		}
 		return INDIGO_OK;
 	// ------------------------------------------------------------------------------- GAIN
