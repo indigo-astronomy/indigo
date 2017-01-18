13af3a79f02d5204801e5c026f3df82f2c8ef648
Rumen G.Bogdanovski <rumenastro@gmail.com>
2017-01-18 02:48:30 +0200

    indigo_timers: clock_gettime() workaround cleanup

--- indigo_libs/indigo_timer.c
+++ indigo_libs/indigo_timer.c
@@ -17,25 +17,22 @@
 #include "indigo_driver.h"
 
 
-#ifdef __MACH__
+#ifdef __MACH__ /* Mac OSX prior Sierra is missling clock_gettime() */
 #include <mach/clock.h>
 #include <mach/mach.h>
-#endif
-
-
 void utc_time(struct timespec *ts) {
-#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
-  clock_serv_t cclock;
-  mach_timespec_t mts;
-  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-  clock_get_time(cclock, &mts);
-  mach_port_deallocate(mach_task_self(), cclock);
-  ts->tv_sec = mts.tv_sec;
-  ts->tv_nsec = mts.tv_nsec;
-#else
-  clock_gettime(CLOCK_REALTIME, ts);
-#endif
+	clock_serv_t cclock;
+	mach_timespec_t mts;
+	host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+	clock_get_time(cclock, &mts);
+	mach_port_deallocate(mach_task_self(), cclock);
+	ts->tv_sec = mts.tv_sec;
+	ts->tv_nsec = mts.tv_nsec;
 }
+#else
+#define utc_time(ts) clock_gettime(CLOCK_REALTIME, ts)
+#endif
+
 
 #define NANO	1000000000L
 
