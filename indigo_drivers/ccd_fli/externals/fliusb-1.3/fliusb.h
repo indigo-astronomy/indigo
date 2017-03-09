737b83edc82dcce1892c75ed94ba08d753d3e52f
Rumen G.Bogdanovski <rumenastro@gmail.com>
2017-03-09 23:24:41 +0200

    fliusb.ko: revert timeout - didnot help

--- indigo_drivers/ccd_fli/externals/fliusb-1.3/fliusb.h
+++ indigo_drivers/ccd_fli/externals/fliusb-1.3/fliusb.h
@@ -70,7 +70,7 @@ enum {
 };
 
 /* Default values (module parameters override these) */
-#define FLIUSB_TIMEOUT (3*HZ)  /* in seconds */
+#define FLIUSB_TIMEOUT (HZ)  /* in seconds */
 #ifndef SGREAD
   #define FLIUSB_BUFFERSIZE 65536
 #else
