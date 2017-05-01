c31b5b58c46f922a551d830f65ff3f56e19eb5b9
Rumen G.Bogdanovski <rumen@skyarchive.org>
2017-05-01 03:25:38 +0300

    move sent: and received: messaged to TRACE

--- indigo_libs/indigo_io.c
+++ indigo_libs/indigo_io.c
@@ -161,6 +161,6 @@ bool indigo_printf(int handle, const char *format, ...) {
 	va_start(args, format);
 	int length = vsnprintf(buffer, 1024, format, args);
 	va_end(args);
-	INDIGO_DEBUG_PROTOCOL(indigo_debug("sent: %s", buffer));
+	INDIGO_TRACE_PROTOCOL(indigo_trace("sent: %s", buffer));
 	return indigo_write(handle, buffer, length);
 }
