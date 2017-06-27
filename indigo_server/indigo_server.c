d105f02bb8626e340605770c1a8ca92202e6eecf
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2017-06-27 22:11:09 +0200

    ccd_dsi added to indigo_server and xcode project

--- indigo_server/indigo_server.c
+++ indigo_server/indigo_server.c
@@ -66,6 +66,7 @@
 #include "focuser_fli/indigo_focuser_fli.h"
 #include "focuser_usbv3/indigo_focuser_usbv3.h"
 #include "ccd_sbig/indigo_ccd_sbig.h"
+#include "ccd_dsi/indigo_ccd_dsi.h"
 #ifdef INDIGO_MACOS
 #include "ccd_ica/indigo_ccd_ica.h"
 #endif
@@ -96,6 +97,7 @@ driver_entry_point static_drivers[] = {
 	indigo_focuser_fli,
 	indigo_focuser_usbv3,
 	indigo_ccd_sbig,
+	indigo_ccd_dsi,
 #ifdef INDIGO_MACOS
 	indigo_ccd_ica,
 #endif
