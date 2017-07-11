44e24b5d001f9537cf7343fbfdb85f4624b2d8a1
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2017-07-12 00:58:16 +0200

    request, response, event and device info code moved to specific subclasses in ica driver

--- indigo_mac_drivers/ccd_ica/indigo_ica_ptp_canon.h
+++ indigo_mac_drivers/ccd_ica/indigo_ica_ptp_canon.h
@@ -10,8 +10,147 @@
 
 #import "indigo_ica_ptp.h"
 
+enum PTPCanonOperationCodeEnum {
+  PTPRequestCodeCanonGetStorageIDs = 0x9101,
+  PTPRequestCodeCanonGetStorageInfo = 0x9102,
+  PTPRequestCodeCanonGetObjectInfo = 0x9103,
+  PTPRequestCodeCanonGetObject = 0x9104,
+  PTPRequestCodeCanonDeleteObject = 0x9105,
+  PTPRequestCodeCanonFormatStore = 0x9106,
+  PTPRequestCodeCanonGetPartialObject = 0x9107,
+  PTPRequestCodeCanonGetDeviceInfoEx = 0x9108,
+  PTPRequestCodeCanonGetObjectInfoEx = 0x9109,
+  PTPRequestCodeCanonGetThumbEx = 0x910A,
+  PTPRequestCodeCanonSendPartialObject = 0x910B,
+  PTPRequestCodeCanonSetObjectAttributes = 0x910C,
+  PTPRequestCodeCanonGetObjectTime = 0x910D,
+  PTPRequestCodeCanonSetObjectTime = 0x910E,
+  PTPRequestCodeCanonRemoteRelease = 0x910F,
+  PTPRequestCodeCanonSetDevicePropValueEx = 0x9110,
+  PTPRequestCodeCanonGetRemoteMode = 0x9113,
+  PTPRequestCodeCanonSetRemoteMode = 0x9114,
+  PTPRequestCodeCanonSetEventMode = 0x9115,
+  PTPRequestCodeCanonGetEvent = 0x9116,
+  PTPRequestCodeCanonTransferComplete = 0x9117,
+  PTPRequestCodeCanonCancelTransfer = 0x9118,
+  PTPRequestCodeCanonResetTransfer = 0x9119,
+  PTPRequestCodeCanonPCHDDCapacity = 0x911A,
+  PTPRequestCodeCanonSetUILock = 0x911B,
+  PTPRequestCodeCanonResetUILock = 0x911C,
+  PTPRequestCodeCanonKeepDeviceOn = 0x911D,
+  PTPRequestCodeCanonSetNullPacketMode = 0x911E,
+  PTPRequestCodeCanonUpdateFirmware = 0x911F,
+  PTPRequestCodeCanonTransferCompleteDT = 0x9120,
+  PTPRequestCodeCanonCancelTransferDT = 0x9121,
+  PTPRequestCodeCanonSetWftProfile = 0x9122,
+  PTPRequestCodeCanonGetWftProfile = 0x9123,
+  PTPRequestCodeCanonSetProfileToWft = 0x9124,
+  PTPRequestCodeCanonBulbStart = 0x9125,
+  PTPRequestCodeCanonBulbEnd = 0x9126,
+  PTPRequestCodeCanonRequestDevicePropValue = 0x9127,
+  PTPRequestCodeCanonRemoteReleaseOn = 0x9128,
+  PTPRequestCodeCanonRemoteReleaseOff = 0x9129,
+  PTPRequestCodeCanonRegistBackgroundImage = 0x912A,
+  PTPRequestCodeCanonChangePhotoStudioMode = 0x912B,
+  PTPRequestCodeCanonGetPartialObjectEx = 0x912C,
+  PTPRequestCodeCanonResetMirrorLockupState = 0x9130,
+  PTPRequestCodeCanonPopupBuiltinFlash = 0x9131,
+  PTPRequestCodeCanonEndGetPartialObjectEx = 0x9132,
+  PTPRequestCodeCanonMovieSelectSWOn = 0x9133,
+  PTPRequestCodeCanonMovieSelectSWOff = 0x9134,
+  PTPRequestCodeCanonGetCTGInfo = 0x9135,
+  PTPRequestCodeCanonGetLensAdjust = 0x9136,
+  PTPRequestCodeCanonSetLensAdjust = 0x9137,
+  PTPRequestCodeCanonGetMusicInfo = 0x9138,
+  PTPRequestCodeCanonCreateHandle = 0x9139,
+  PTPRequestCodeCanonSendPartialObjectEx = 0x913A,
+  PTPRequestCodeCanonEndSendPartialObjectEx = 0x913B,
+  PTPRequestCodeCanonSetCTGInfo = 0x913C,
+  PTPRequestCodeCanonSetRequestOLCInfoGroup = 0x913D,
+  PTPRequestCodeCanonSetRequestRollingPitchingLevel = 0x913E,
+  PTPRequestCodeCanonGetCameraSupport = 0x913F,
+  PTPRequestCodeCanonSetRating = 0x9140,
+  PTPRequestCodeCanonRequestInnerDevelopStart = 0x9141,
+  PTPRequestCodeCanonRequestInnerDevelopParamChange = 0x9142,
+  PTPRequestCodeCanonRequestInnerDevelopEnd = 0x9143,
+  PTPRequestCodeCanonGpsLoggingDataMode = 0x9144,
+  PTPRequestCodeCanonGetGpsLogCurrentHandle = 0x9145,
+  PTPRequestCodeCanonInitiateViewfinder = 0x9151,
+  PTPRequestCodeCanonTerminateViewfinder = 0x9152,
+  PTPRequestCodeCanonGetViewFinderData = 0x9153,
+  PTPRequestCodeCanonDoAf = 0x9154,
+  PTPRequestCodeCanonDriveLens = 0x9155,
+  PTPRequestCodeCanonDepthOfFieldPreview = 0x9156,
+  PTPRequestCodeCanonClickWB = 0x9157,
+  PTPRequestCodeCanonZoom = 0x9158,
+  PTPRequestCodeCanonZoomPosition = 0x9159,
+  PTPRequestCodeCanonSetLiveAfFrame = 0x915A,
+  PTPRequestCodeCanonTouchAfPosition = 0x915B,
+  PTPRequestCodeCanonSetLvPcFlavoreditMode = 0x915C,
+  PTPRequestCodeCanonSetLvPcFlavoreditParam = 0x915D,
+  PTPRequestCodeCanonAfCancel = 0x9160,
+  PTPRequestCodeCanonSetDefaultCameraSetting = 0x91BE,
+  PTPRequestCodeCanonGetAEData = 0x91BF,
+  PTPRequestCodeCanonNotifyNetworkError = 0x91E8,
+  PTPRequestCodeCanonAdapterTransferProgress = 0x91E9,
+  PTPRequestCodeCanonTransferComplete2 = 0x91F0,
+  PTPRequestCodeCanonCancelTransfer2 = 0x91F1,
+  PTPRequestCodeCanonFAPIMessageTX = 0x91FE,
+  PTPRequestCodeCanonFAPIMessageRX = 0x91FF
+};
+
+enum PTPCanonResponseCodeEnum {
+  PTPResponseCodeUnknownCommand = 0xA001,
+  PTPResponseCodeOperationRefused = 0xA005,
+  PTPResponseCodeLensCover = 0xA006,
+  PTPResponseCodeBatteryLow = 0xA101,
+  PTPResponseCodeNotReady = 0xA102
+};
+
+enum PTPCanonEventCodeEnum {
+  PTPEventCodeCanonRequestGetEvent = 0xC101,
+  PTPEventCodeCanonObjectAddedEx = 0xC181,
+  PTPEventCodeCanonObjectRemoved = 0xC182,
+  PTPEventCodeCanonRequestGetObjectInfoEx = 0xC183,
+  PTPEventCodeCanonStorageStatusChanged = 0xC184,
+  PTPEventCodeCanonStorageInfoChanged = 0xC185,
+  PTPEventCodeCanonRequestObjectTransfer = 0xC186,
+  PTPEventCodeCanonObjectInfoChangedEx = 0xC187,
+  PTPEventCodeCanonObjectContentChanged = 0xC188,
+  PTPEventCodeCanonPropValueChanged = 0xC189,
+  PTPEventCodeCanonAvailListChanged = 0xC18A,
+  PTPEventCodeCanonCameraStatusChanged = 0xC18B,
+  PTPEventCodeCanonWillSoonShutdown = 0xC18D,
+  PTPEventCodeCanonShutdownTimerUpdated = 0xC18E,
+  PTPEventCodeCanonRequestCancelTransfer = 0xC18F,
+  PTPEventCodeCanonRequestObjectTransferDT = 0xC190,
+  PTPEventCodeCanonRequestCancelTransferDT = 0xC191,
+  PTPEventCodeCanonStoreAdded = 0xC192,
+  PTPEventCodeCanonStoreRemoved = 0xC193,
+  PTPEventCodeCanonBulbExposureTime = 0xC194,
+  PTPEventCodeCanonRecordingTime = 0xC195,
+  PTPEventCodeCanonRequestObjectTransferTS = 0xC1A2,
+  PTPEventCodeCanonAfResult = 0xC1A3,
+  PTPEventCodeCanonCTGInfoCheckComplete = 0xC1A4,
+  PTPEventCodeCanonOLCInfoChanged = 0xC1A5,
+  PTPEventCodeCanonRequestObjectTransferFTP = 0xC1F1
+};
+
+@interface PTPCanonRequest : PTPRequest
++(NSString *)operationCodeName:(PTPRequestCode)operationCode;
+@end
+
+@interface PTPCanonResponse : PTPResponse
++(NSString *)responseCodeName:(PTPResponseCode)responseCode;
+@end
+
+@interface PTPCanonEvent : PTPEvent
++(NSString *)eventCodeName:(PTPEventCode)eventCode;
+@end
+
+
+@interface PTPCanonDeviceInfo : PTPDeviceInfo
+@end
+
 @interface PTPCanonCamera : PTPCamera
-
--(id)initWithICCamera:(ICCameraDevice *)icCamera delegate:(NSObject<PTPDelegateProtocol> *)delegate;
-
 @end
--- indigo_mac_drivers/ccd_ica/indigo_ica_ptp_canon.m
+++ indigo_mac_drivers/ccd_ica/indigo_ica_ptp_canon.m
@@ -8,6 +8,193 @@
 
 #import "indigo_ica_ptp_canon.h"
 
+@implementation PTPCanonRequest
++(NSString *)operationCodeName:(PTPRequestCode)operationCode {
+  switch (operationCode) {
+    case PTPRequestCodeCanonGetStorageIDs: return @"PTPRequestCodeCanonGetStorageIDs";
+    case PTPRequestCodeCanonGetStorageInfo: return @"PTPRequestCodeCanonGetStorageInfo";
+    case PTPRequestCodeCanonGetObjectInfo: return @"PTPRequestCodeCanonGetObjectInfo";
+    case PTPRequestCodeCanonGetObject: return @"PTPRequestCodeCanonGetObject";
+    case PTPRequestCodeCanonDeleteObject: return @"PTPRequestCodeCanonDeleteObject";
+    case PTPRequestCodeCanonFormatStore: return @"PTPRequestCodeCanonFormatStore";
+    case PTPRequestCodeCanonGetPartialObject: return @"PTPRequestCodeCanonGetPartialObject";
+    case PTPRequestCodeCanonGetDeviceInfoEx: return @"PTPRequestCodeCanonGetDeviceInfoEx";
+    case PTPRequestCodeCanonGetObjectInfoEx: return @"PTPRequestCodeCanonGetObjectInfoEx";
+    case PTPRequestCodeCanonGetThumbEx: return @"PTPRequestCodeCanonGetThumbEx";
+    case PTPRequestCodeCanonSendPartialObject: return @"PTPRequestCodeCanonSendPartialObject";
+    case PTPRequestCodeCanonSetObjectAttributes: return @"PTPRequestCodeCanonSetObjectAttributes";
+    case PTPRequestCodeCanonGetObjectTime: return @"PTPRequestCodeCanonGetObjectTime";
+    case PTPRequestCodeCanonSetObjectTime: return @"PTPRequestCodeCanonSetObjectTime";
+    case PTPRequestCodeCanonRemoteRelease: return @"PTPRequestCodeCanonRemoteRelease";
+    case PTPRequestCodeCanonSetDevicePropValueEx: return @"PTPRequestCodeCanonSetDevicePropValueEx";
+    case PTPRequestCodeCanonGetRemoteMode: return @"PTPRequestCodeCanonGetRemoteMode";
+    case PTPRequestCodeCanonSetRemoteMode: return @"PTPRequestCodeCanonSetRemoteMode";
+    case PTPRequestCodeCanonSetEventMode: return @"PTPRequestCodeCanonSetEventMode";
+    case PTPRequestCodeCanonGetEvent: return @"PTPRequestCodeCanonGetEvent";
+    case PTPRequestCodeCanonTransferComplete: return @"PTPRequestCodeCanonTransferComplete";
+    case PTPRequestCodeCanonCancelTransfer: return @"PTPRequestCodeCanonCancelTransfer";
+    case PTPRequestCodeCanonResetTransfer: return @"PTPRequestCodeCanonResetTransfer";
+    case PTPRequestCodeCanonPCHDDCapacity: return @"PTPRequestCodeCanonPCHDDCapacity";
+    case PTPRequestCodeCanonSetUILock: return @"PTPRequestCodeCanonSetUILock";
+    case PTPRequestCodeCanonResetUILock: return @"PTPRequestCodeCanonResetUILock";
+    case PTPRequestCodeCanonKeepDeviceOn: return @"PTPRequestCodeCanonKeepDeviceOn";
+    case PTPRequestCodeCanonSetNullPacketMode: return @"PTPRequestCodeCanonSetNullPacketMode";
+    case PTPRequestCodeCanonUpdateFirmware: return @"PTPRequestCodeCanonUpdateFirmware";
+    case PTPRequestCodeCanonTransferCompleteDT: return @"PTPRequestCodeCanonTransferCompleteDT";
+    case PTPRequestCodeCanonCancelTransferDT: return @"PTPRequestCodeCanonCancelTransferDT";
+    case PTPRequestCodeCanonSetWftProfile: return @"PTPRequestCodeCanonSetWftProfile";
+    case PTPRequestCodeCanonGetWftProfile: return @"PTPRequestCodeCanonGetWftProfile";
+    case PTPRequestCodeCanonSetProfileToWft: return @"PTPRequestCodeCanonSetProfileToWft";
+    case PTPRequestCodeCanonBulbStart: return @"PTPRequestCodeCanonBulbStart";
+    case PTPRequestCodeCanonBulbEnd: return @"PTPRequestCodeCanonBulbEnd";
+    case PTPRequestCodeCanonRequestDevicePropValue: return @"PTPRequestCodeCanonRequestDevicePropValue";
+    case PTPRequestCodeCanonRemoteReleaseOn: return @"PTPRequestCodeCanonRemoteReleaseOn";
+    case PTPRequestCodeCanonRemoteReleaseOff: return @"PTPRequestCodeCanonRemoteReleaseOff";
+    case PTPRequestCodeCanonRegistBackgroundImage: return @"PTPRequestCodeCanonRegistBackgroundImage";
+    case PTPRequestCodeCanonChangePhotoStudioMode: return @"PTPRequestCodeCanonChangePhotoStudioMode";
+    case PTPRequestCodeCanonGetPartialObjectEx: return @"PTPRequestCodeCanonGetPartialObjectEx";
+    case PTPRequestCodeCanonResetMirrorLockupState: return @"PTPRequestCodeCanonResetMirrorLockupState";
+    case PTPRequestCodeCanonPopupBuiltinFlash: return @"PTPRequestCodeCanonPopupBuiltinFlash";
+    case PTPRequestCodeCanonEndGetPartialObjectEx: return @"PTPRequestCodeCanonEndGetPartialObjectEx";
+    case PTPRequestCodeCanonMovieSelectSWOn: return @"PTPRequestCodeCanonMovieSelectSWOn";
+    case PTPRequestCodeCanonMovieSelectSWOff: return @"PTPRequestCodeCanonMovieSelectSWOff";
+    case PTPRequestCodeCanonGetCTGInfo: return @"PTPRequestCodeCanonGetCTGInfo";
+    case PTPRequestCodeCanonGetLensAdjust: return @"PTPRequestCodeCanonGetLensAdjust";
+    case PTPRequestCodeCanonSetLensAdjust: return @"PTPRequestCodeCanonSetLensAdjust";
+    case PTPRequestCodeCanonGetMusicInfo: return @"PTPRequestCodeCanonGetMusicInfo";
+    case PTPRequestCodeCanonCreateHandle: return @"PTPRequestCodeCanonCreateHandle";
+    case PTPRequestCodeCanonSendPartialObjectEx: return @"PTPRequestCodeCanonSendPartialObjectEx";
+    case PTPRequestCodeCanonEndSendPartialObjectEx: return @"PTPRequestCodeCanonEndSendPartialObjectEx";
+    case PTPRequestCodeCanonSetCTGInfo: return @"PTPRequestCodeCanonSetCTGInfo";
+    case PTPRequestCodeCanonSetRequestOLCInfoGroup: return @"PTPRequestCodeCanonSetRequestOLCInfoGroup";
+    case PTPRequestCodeCanonSetRequestRollingPitchingLevel: return @"PTPRequestCodeCanonSetRequestRollingPitchingLevel";
+    case PTPRequestCodeCanonGetCameraSupport: return @"PTPRequestCodeCanonGetCameraSupport";
+    case PTPRequestCodeCanonSetRating: return @"PTPRequestCodeCanonSetRating";
+    case PTPRequestCodeCanonRequestInnerDevelopStart: return @"PTPRequestCodeCanonRequestInnerDevelopStart";
+    case PTPRequestCodeCanonRequestInnerDevelopParamChange: return @"PTPRequestCodeCanonRequestInnerDevelopParamChange";
+    case PTPRequestCodeCanonRequestInnerDevelopEnd: return @"PTPRequestCodeCanonRequestInnerDevelopEnd";
+    case PTPRequestCodeCanonGpsLoggingDataMode: return @"PTPRequestCodeCanonGpsLoggingDataMode";
+    case PTPRequestCodeCanonGetGpsLogCurrentHandle: return @"PTPRequestCodeCanonGetGpsLogCurrentHandle";
+    case PTPRequestCodeCanonInitiateViewfinder: return @"PTPRequestCodeCanonInitiateViewfinder";
+    case PTPRequestCodeCanonTerminateViewfinder: return @"PTPRequestCodeCanonTerminateViewfinder";
+    case PTPRequestCodeCanonGetViewFinderData: return @"PTPRequestCodeCanonGetViewFinderData";
+    case PTPRequestCodeCanonDoAf: return @"PTPRequestCodeCanonDoAf";
+    case PTPRequestCodeCanonDriveLens: return @"PTPRequestCodeCanonDriveLens";
+    case PTPRequestCodeCanonDepthOfFieldPreview: return @"PTPRequestCodeCanonDepthOfFieldPreview";
+    case PTPRequestCodeCanonClickWB: return @"PTPRequestCodeCanonClickWB";
+    case PTPRequestCodeCanonZoom: return @"PTPRequestCodeCanonZoom";
+    case PTPRequestCodeCanonZoomPosition: return @"PTPRequestCodeCanonZoomPosition";
+    case PTPRequestCodeCanonSetLiveAfFrame: return @"PTPRequestCodeCanonSetLiveAfFrame";
+    case PTPRequestCodeCanonTouchAfPosition: return @"PTPRequestCodeCanonTouchAfPosition";
+    case PTPRequestCodeCanonSetLvPcFlavoreditMode: return @"PTPRequestCodeCanonSetLvPcFlavoreditMode";
+    case PTPRequestCodeCanonSetLvPcFlavoreditParam: return @"PTPRequestCodeCanonSetLvPcFlavoreditParam";
+    case PTPRequestCodeCanonAfCancel: return @"PTPRequestCodeCanonAfCancel";
+    case PTPRequestCodeCanonSetDefaultCameraSetting: return @"PTPRequestCodeCanonSetDefaultCameraSetting";
+    case PTPRequestCodeCanonGetAEData: return @"PTPRequestCodeCanonGetAEData";
+    case PTPRequestCodeCanonNotifyNetworkError: return @"PTPRequestCodeCanonNotifyNetworkError";
+    case PTPRequestCodeCanonAdapterTransferProgress: return @"PTPRequestCodeCanonAdapterTransferProgress";
+    case PTPRequestCodeCanonTransferComplete2: return @"PTPRequestCodeCanonTransferComplete2";
+    case PTPRequestCodeCanonCancelTransfer2: return @"PTPRequestCodeCanonCancelTransfer2";
+    case PTPRequestCodeCanonFAPIMessageTX: return @"PTPRequestCodeCanonFAPIMessageTX";
+    case PTPRequestCodeCanonFAPIMessageRX: return @"PTPRequestCodeCanonFAPIMessageRX";
+  }
+  return [PTPRequest operationCodeName:operationCode];
+}
+
+-(NSString *)operationCodeName {
+  return [PTPCanonRequest operationCodeName:self.operationCode];
+}
+
+@end
+
+@implementation PTPCanonResponse : PTPResponse
+
++ (NSString *)responseCodeName:(PTPResponseCode)responseCode {
+  switch (responseCode) {
+    case PTPResponseCodeUnknownCommand: return @"PTPResponseCodeUnknownCommand";
+    case PTPResponseCodeOperationRefused: return @"PTPResponseCodeOperationRefused";
+    case PTPResponseCodeLensCover: return @"PTPResponseCodeLensCover";
+    case PTPResponseCodeBatteryLow: return @"PTPResponseCodeBatteryLow";
+    case PTPResponseCodeNotReady: return @"PTPResponseCodeNotReady";
+  }
+  return [PTPResponse responseCodeName:responseCode];
+}
+
+-(NSString *)responseCodeName {
+  return [PTPCanonResponse responseCodeName:self.responseCode];
+}
+
+@end
+
+@implementation PTPCanonEvent : PTPEvent
+
++(NSString *)eventCodeName:(PTPEventCode)eventCode {
+  switch (eventCode) {
+    case PTPEventCodeCanonRequestGetEvent: return @"PTPEventCodeCanonRequestGetEvent";
+    case PTPEventCodeCanonObjectAddedEx: return @"PTPEventCodeCanonObjectAddedEx";
+    case PTPEventCodeCanonObjectRemoved: return @"PTPEventCodeCanonObjectRemoved";
+    case PTPEventCodeCanonRequestGetObjectInfoEx: return @"PTPEventCodeCanonRequestGetObjectInfoEx";
+    case PTPEventCodeCanonStorageStatusChanged: return @"PTPEventCodeCanonStorageStatusChanged";
+    case PTPEventCodeCanonStorageInfoChanged: return @"PTPEventCodeCanonStorageInfoChanged";
+    case PTPEventCodeCanonRequestObjectTransfer: return @"PTPEventCodeCanonRequestObjectTransfer";
+    case PTPEventCodeCanonObjectInfoChangedEx: return @"PTPEventCodeCanonObjectInfoChangedEx";
+    case PTPEventCodeCanonObjectContentChanged: return @"PTPEventCodeCanonObjectContentChanged";
+    case PTPEventCodeCanonPropValueChanged: return @"PTPEventCodeCanonPropValueChanged";
+    case PTPEventCodeCanonAvailListChanged: return @"PTPEventCodeCanonAvailListChanged";
+    case PTPEventCodeCanonCameraStatusChanged: return @"PTPEventCodeCanonCameraStatusChanged";
+    case PTPEventCodeCanonWillSoonShutdown: return @"PTPEventCodeCanonWillSoonShutdown";
+    case PTPEventCodeCanonShutdownTimerUpdated: return @"PTPEventCodeCanonShutdownTimerUpdated";
+    case PTPEventCodeCanonRequestCancelTransfer: return @"PTPEventCodeCanonRequestCancelTransfer";
+    case PTPEventCodeCanonRequestObjectTransferDT: return @"PTPEventCodeCanonRequestObjectTransferDT";
+    case PTPEventCodeCanonRequestCancelTransferDT: return @"PTPEventCodeCanonRequestCancelTransferDT";
+    case PTPEventCodeCanonStoreAdded: return @"PTPEventCodeCanonStoreAdded";
+    case PTPEventCodeCanonStoreRemoved: return @"PTPEventCodeCanonStoreRemoved";
+    case PTPEventCodeCanonBulbExposureTime: return @"PTPEventCodeCanonBulbExposureTime";
+    case PTPEventCodeCanonRecordingTime: return @"PTPEventCodeCanonRecordingTime";
+    case PTPEventCodeCanonRequestObjectTransferTS: return @"PTPEventCodeCanonRequestObjectTransferTS";
+    case PTPEventCodeCanonAfResult: return @"PTPEventCodeCanonAfResult";
+    case PTPEventCodeCanonCTGInfoCheckComplete: return @"PTPEventCodeCanonCTGInfoCheckComplete";
+    case PTPEventCodeCanonOLCInfoChanged: return @"PTPEventCodeCanonOLCInfoChanged";
+    case PTPEventCodeCanonRequestObjectTransferFTP: return @"PTPEventCodeCanonRequestObjectTransferFTP";
+  }
+  return [PTPEvent eventCodeName:eventCode];
+}
+
+-(NSString *)eventCodeName {
+  return [PTPCanonEvent eventCodeName:self.eventCode];
+}
+
+@end
+
+@implementation PTPCanonDeviceInfo : PTPDeviceInfo
+
+-(NSString *)debug {
+  NSMutableString *s = [NSMutableString stringWithFormat:@"%@ %@, PTP V%.2f + %@ V%.2f\n", self.model, self.version, self.standardVersion / 100.0, self.vendorExtensionDesc, self.vendorExtensionVersion / 100.0];
+  if (self.operationsSupported.count > 0) {
+    [s appendFormat:@"\nOperations:\n"];
+    for (NSNumber *code in self.operationsSupported)
+      [s appendFormat:@"%@\n", [PTPCanonRequest operationCodeName:code.intValue]];
+  }
+  if (self.eventsSupported.count > 0) {
+    [s appendFormat:@"\nEvents:\n"];
+    for (NSNumber *code in self.eventsSupported)
+      [s appendFormat:@"%@\n", [PTPCanonEvent eventCodeName:code.intValue]];
+  }
+  if (self.propertiesSupported.count > 0) {
+    [s appendFormat:@"\nProperties:\n"];
+    for (NSNumber *code in self.propertiesSupported) {
+      PTPProperty *property = self.properties[code];
+      if (property)
+        [s appendFormat:@"%@\n", property];
+      else
+        [s appendFormat:@"%@\n", [PTPProperty propertyCodeName:code.intValue vendorExtension:self.vendorExtension]];
+    }
+  }
+  return s;
+}
+
+@end
+
 @implementation PTPCanonCamera
 
 -(id)initWithICCamera:(ICCameraDevice *)icCamera delegate:(NSObject<PTPDelegateProtocol> *)delegate {
@@ -16,6 +203,22 @@
 
   }
   return self;
+}
+
+-(PTPRequest *)allocRequest {
+  return [PTPCanonRequest alloc];
+}
+
+-(PTPResponse *)allocResponse {
+  return [PTPCanonResponse alloc];
+}
+
+-(PTPEvent *)allocEvent {
+  return [PTPCanonEvent alloc];
+}
+
+-(PTPDeviceInfo *)allocDeviceInfo {
+  return [PTPCanonDeviceInfo alloc];
 }
 
 @end
