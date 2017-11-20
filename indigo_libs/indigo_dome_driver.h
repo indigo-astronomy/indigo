c8682e9f94ccd89c9774103625791ed81de78b3c
Rumen G.Bogdanovski <rumenastro@gmail.com>
2017-11-20 22:09:47 +0200

    indigo_dome_driver: fix warning

--- indigo_libs/indigo_dome_driver.h
+++ indigo_libs/indigo_dome_driver.h
@@ -32,19 +32,19 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-	
+
 /** Main dome group name string.
 */
 #define DOME_MAIN_GROUP									"Dome"
-	
+
 /** Device context pointer.
  */
 #define DOME_CONTEXT                					((indigo_dome_context *)device->device_context)
-	
+
 /** DOME_SPEED property pointer, property is mandatory, property change request should be fully handled by indigo_dome_change_property
  */
 #define DOME_SPEED_PROPERTY								(DOME_CONTEXT->dome_speed_property)
-	
+
 /** DOME_SPEED.SPEED property item pointer.
  */
 #define DOME_SPEED_ITEM									(DOME_SPEED_PROPERTY->items+0)
@@ -52,64 +52,64 @@ extern "C" {
 /** DOME_DIRECTION property pointer, property is mandatory, property change request should be fully handled by indigo_dome_change_property.
  */
 #define DOME_DIRECTION_PROPERTY							(DOME_CONTEXT->dome_direction_property)
-	
+
 /** DOME_DIRECTION.MOVE_CLOCKWISE property item pointer.
  */
 #define DOME_DIRECTION_MOVE_CLOCKWISE_ITEM				(DOME_DIRECTION_PROPERTY->items+0)
-	
+
 /** DOME_DIRECTION.MOVE_COUNTERCLOCKWISE property item pointer.
  */
 #define DOME_DIRECTION_MOVE_COUNTERCLOCKWISE_ITEM		(DOME_DIRECTION_PROPERTY->items+1)
-	
+
 /** DOME_STEPS property pointer, property is mandatory, property change request should be fully handled by dome driver
  */
 #define DOME_STEPS_PROPERTY								(DOME_CONTEXT->dome_steps_property)
-	
+
 /** DOME_STEPS.STEPS property item pointer.
  */
 #define DOME_STEPS_ITEM									(DOME_STEPS_PROPERTY->items+0)
-	
+
 /** DOME_HORIZONTAL_COORDINATES property pointer, property is mandatory, property change request should be fully handled by dome driver
  */
 #define DOME_HORIZONTAL_COORDINATES_PROPERTY			(DOME_CONTEXT->dome_position_property)
-	
+
 /** DOME_HORIZONTAL_COORDINATES.AZ property item pointer.
  */
 #define DOME_HORIZONTAL_COORDINATES_AZ_ITEM								(DOME_HORIZONTAL_COORDINATES_PROPERTY->items+0)
-	
+
 /** DOME_HORIZONTAL_COORDINATES.ALT property item pointer.
  */
 #define DOME_HORIZONTAL_COORDINATES_ALT_ITEM								(DOME_HORIZONTAL_COORDINATES_PROPERTY->items+1)
-	
+
 /** DOME_ABORT_MOTION property pointer, property is mandatory, property change request should be fully handled by dome driver
  */
 #define DOME_ABORT_MOTION_PROPERTY						(DOME_CONTEXT->dome_abort_motion_property)
-	
+
 /** DOME_ABORT_MOTION.ABORT_MOTION property item pointer.
  */
 #define DOME_ABORT_MOTION_ITEM							(DOME_ABORT_MOTION_PROPERTY->items+0)
-	
+
 /** DOME_SHUTTER property pointer, property is mandatory, property change request should be fully handled by dome driver
  */
 #define DOME_SHUTTER_PROPERTY								(DOME_CONTEXT->dome_shutter_property)
-	
+
 /** DOME_SHUTTER.OPENED property item pointer.
  */
 #define DOME_SHUTTER_OPENED_ITEM							(DOME_SHUTTER_PROPERTY->items+0)
-	
+
 /** DOME_SHUTTER.CLOSED property item pointer.
  */
 #define DOME_SHUTTER_CLOSED_ITEM							(DOME_SHUTTER_PROPERTY->items+1)
-	
+
 //----------------------------------------------
 /** DOME_PARK property pointer, property is mandatory, property change request should be fully handled by device driver.
  */
 #define DOME_PARK_PROPERTY														(DOME_CONTEXT->dome_park_property)
-	
+
 /** DOME_PARK.PARKED property item pointer.
  */
 #define DOME_PARK_PARKED_ITEM												(DOME_PARK_PROPERTY->items+0)
-	
+
 /** DOME_PARK.UNPARKED property item pointer.
  */
 #define DOME_PARK_UNPARKED_ITEM											(DOME_PARK_PROPERTY->items+1)
@@ -118,7 +118,7 @@ extern "C" {
 /** DOME_MEASUREMENT property pointer, property is mandatory
  */
 #define DOME_DIMENSION_PROPERTY										(DOME_CONTEXT->dome_measurement_property)
-	
+
 /** DOME_MEASUREMENT.RADIUS property item pointer.
  */
 #define DOME_RADIUS_ITEM														(DOME_DIMENSION_PROPERTY->items+0)
@@ -126,23 +126,23 @@ extern "C" {
 /** DOME_MEASUREMENT.SHUTTER_WIDTH property item pointer.
  */
 #define DOME_SHUTTER_WIDTH_ITEM											(DOME_DIMENSION_PROPERTY->items+1)
-	
+
 /** DOME_MEASUREMENT.NORTH_DISPLACEMENT property item pointer.
  */
 #define DOME_NORTH_DISPLACEMENT_ITEM								(DOME_DIMENSION_PROPERTY->items+2)
-	
+
 /** DOME_MEASUREMENT.EAST_DISPLACEMENT property item pointer.
  */
 #define DOME_EAST_DISPLACEMENT_ITEM									(DOME_DIMENSION_PROPERTY->items+3)
-	
+
 /** DOME_MEASUREMENT.UP_DISPLACEMENT property item pointer.
  */
 #define DOME_UP_DISPLACEMENT_ITEM										(DOME_DIMENSION_PROPERTY->items+4)
-	
+
 /** DOME_MEASUREMENT.OTA_OFFSET property item pointer.
  */
 #define DOME_OTA_OFFSET_ITEM												(DOME_DIMENSION_PROPERTY->items+5)
-	
+
 /** Dome device context structure.
  */
 typedef struct {
@@ -169,11 +169,12 @@ extern indigo_result indigo_dome_change_property(indigo_device *device, indigo_c
 /** Detach callback function.
  */
 extern indigo_result indigo_dome_detach(indigo_device *device);
-	
+/** Update dome coordinates.
+ */
+extern indigo_result indigo_update_dome_coordinates(indigo_device *device, const char *message);
+
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* indigo_dome_h */
-
-
