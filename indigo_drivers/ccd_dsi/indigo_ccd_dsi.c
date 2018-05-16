4cd59c0dcbad732cffc1d7a1a218df67bf537a8c
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2018-05-16 21:52:52 +0200

    missing variable restored in indigo_ccd_dsi.c :)

--- indigo_drivers/ccd_dsi/indigo_ccd_dsi.c
+++ indigo_drivers/ccd_dsi/indigo_ccd_dsi.c
@@ -505,7 +505,7 @@ static indigo_result ccd_detach(indigo_device *device) {
 
 
 // -------------------------------------------------------------------------------- hot-plug support
-//static pthread_mutex_t device_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t device_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 #define MAX_DEVICES                   32
 #define NOT_FOUND                    (-1)
