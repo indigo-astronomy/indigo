8de5dd6dad5a1422374d17c836cdf2572fbd7cac
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2018-09-22 19:56:19 +0200

    read serial command fixed

--- indigo_drivers/guider_cgusbst4/indigo_guider_cgusbst4.c
+++ indigo_drivers/guider_cgusbst4/indigo_guider_cgusbst4.c
@@ -95,9 +95,8 @@ static bool cgusbst4_command(indigo_device *device, char *command, char *respons
 	// read response
 	if (response != NULL) {
 		int index = 0;
-		int remains = max;
 		int timeout = 3;
-		while (remains > 0) {
+		while (index < max) {
 			fd_set readout;
 			FD_ZERO(&readout);
 			FD_SET(PRIVATE_DATA->handle, &readout);
