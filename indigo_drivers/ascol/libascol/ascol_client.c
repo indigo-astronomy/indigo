61abb43eca2e689d3477ad9f74ff2ffc8fdbe60e
Rumen G.Bogdanovski <rumenastro@gmail.com>
2018-10-31 15:22:09 +0200

    ascol: fixed dms2dd()

--- indigo_drivers/ascol/libascol/ascol_client.c
+++ indigo_drivers/ascol/libascol/ascol_client.c
@@ -6,6 +6,7 @@ int main() {
 	printf("OPEN: %d\n", fd);
 
 	char cmd[80] = "TRRD\n";
+	char response[80];
 	int res = write_telescope(fd, cmd);
 	printf("WROTE: %d -> %s", res, cmd);
 
@@ -13,4 +14,27 @@ int main() {
 	res = read_telescope(fd, res_str, 80);
 
 	printf("READ: %d <- >%s<\n", res, res_str);
+
+	double dd;
+	strcpy(response, "+1030003.6");
+	res = dms2dd(&dd, response);
+	printf("dms2dd() = %2d: %s -> %.9lf\n", res, response, dd);
+	strcpy(response, "+103003.6");
+	res = dms2dd(&dd, response);
+	printf("dms2dd() = %2d: %s -> %.9lf\n", res, response, dd);
+	strcpy(response, "+1003");
+	res = dms2dd(&dd, response);
+	printf("dms2dd() = %2d: %s -> %.9lf\n", res, response, dd);
+	strcpy(response, "+103");
+	res = dms2dd(&dd, response);
+	printf("dms2dd() = %2d: %s -> %.9lf\n", res, response, dd);
+	strcpy(response, "103003");
+	res = dms2dd(&dd, response);
+	printf("dms2dd() = %2d: %s -> %.9lf\n", res, response, dd);
+	strcpy(response, "103003.6");
+	res = dms2dd(&dd, response);
+	printf("dms2dd() = %2d: %s -> %.9lf\n", res, response, dd);
+	strcpy(response, "-103036.");
+	res = dms2dd(&dd, response);
+	printf("dms2dd() = %2d: %s -> %.9lf\n", res, response, dd);
 }
