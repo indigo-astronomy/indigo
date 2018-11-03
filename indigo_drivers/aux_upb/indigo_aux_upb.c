b302af6797572382e521bdb5f0ec48501b0d9e85
Peter Polakovic <peter.polakovic@gmail.com>
2018-11-03 23:35:47 +0100

    UPB specific Makefile to make INDI mini-package

--- indigo_drivers/aux_upb/indigo_aux_upb.c
+++ indigo_drivers/aux_upb/indigo_aux_upb.c
@@ -985,7 +985,7 @@ indigo_result indigo_aux_upb(indigo_driver_action action, indigo_driver_info *in
 		focuser_detach
 	);
 
-	SET_DRIVER_INFO(info, "PegasusAstro Ultimate Powebox", __FUNCTION__, DRIVER_VERSION, false, last_action);
+	SET_DRIVER_INFO(info, "Ultimate Powebox", __FUNCTION__, DRIVER_VERSION, false, last_action);
 
 	if (action == last_action)
 		return INDIGO_OK;
