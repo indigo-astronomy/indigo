22f287627a2d6eda6b28a78e0552d8b39572301b
Thomas Stibor <thomas@stibor.net>
2018-12-03 17:27:33 +0100

    Add in test suite script image format tests
    The image format test checks whether selected image
    format (FITS, XISF, JPEG, RAW) and taken/stored images matches by
    employing the "file" command and "head -c 8 XISF0100" resp.

--- indigo_test/test_suite.sh
+++ indigo_test/test_suite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Written by Thomas Stibor <thomas@stibor.net>
-VERSION="0.1.1"
+VERSION="0.1.2"
 
 # ANSI color codes.
 COL_R='\033[0;31m'
@@ -9,8 +9,9 @@ COL_G='\033[0;32m'
 COL__='\033[0m' # No color.
 
 # Arguments
-TRAVIS_TEST=0
+DRIVER_TEST=0
 CAPTURE_TEST=""
+FORMAT_TEST=""
 
 #---------------- Helper functions -----------------#
 __realpath() {
@@ -59,8 +60,9 @@ __hash_index() {
 
 __usage() {
     echo -e "usage: ${0}\n" \
-	   "\t-t, --travis-test\n" \
+	   "\t-d, --driver-test\n" \
 	   "\t-c, --capture-test <indigo_ccd_driver>\n" \
+	   "\t-f, --format-test <indigo_ccd_driver>\n" \
 	   "\tversion ${VERSION}"
     exit 1
 }
@@ -209,9 +211,79 @@ __test_capture() {
 
 	# Check FITS file exists.
 	fn="${TMPDIR}IMAGE_$(printf "%03d" ${cnt}).fits"
-	[[ ! -f  ${fn} ]] && { echo "exposure '${e} secs' failed, FIT file '${fn}' does not exist"; exit 1; }
+	[[ ! -f  ${fn} ]] && { __log_error "exposure '${e} secs' failed, FIT file '${fn}' does not exist"; exit 1; }
 	__log_info "FIT file '${fn}' of size $(wc -c ${fn} | awk '{print $1}') bytes successfully created"
 	
+    done
+}
+
+__test_format() {
+
+    [[ "$#" -ne 1 ]] && { log_error "wrong number of arguments"; exit 1; }
+    [[ -z $(__hash_func "${1}") ]] &&
+	{ echo "driver '${1}' does not exist test suite"; exit 1; }
+
+    local TMPDIR="$(mktemp -q -d -t "$(basename "$0").XXXXXX" 2>/dev/null || mktemp -q -d)/"
+    declare -a IMG_FORMAT=("FITS" "XISF" "JPEG" "RAW")
+
+    # Load CCD driver.
+    __set_verify_property "Server.LOAD.DRIVER=${1}" \
+			  "Server.DRIVERS" \
+			  "$(__hash_func ${1}) = ON" \
+			  "1"
+
+    # Verify driver is loaded.
+    __set_verify_property "CCD Imager Simulator.CONNECTION.CONNECTED=ON" \
+			  "CCD Imager Simulator.CONNECTION" \
+			  "CONNECTED = ON" \
+			  "1"
+
+    # Set local mode.
+    __set_verify_property "CCD Imager Simulator.CCD_UPLOAD_MODE.LOCAL=ON" \
+			  "CCD Imager Simulator.CCD_UPLOAD_MODE" \
+			  "LOCAL = ON" \
+			  "1"
+
+    # Set directory.
+    __set_verify_property "CCD Imager Simulator.CCD_LOCAL_MODE.DIR=${TMPDIR}" \
+			  "CCD Imager Simulator.CCD_LOCAL_MODE" \
+			  "DIR = \"${TMPDIR}\"" \
+			  "1"
+
+    local fn=""
+    for f in "${IMG_FORMAT[@]}"
+    do
+	__set_verify_property "CCD Imager Simulator.CCD_IMAGE_FORMAT.${f}=ON" \
+			      "CCD Imager Simulator.CCD_IMAGE_FORMAT" \
+			      "${f} = ON" \
+			      "1"
+
+	__set_verify_property "CCD Imager Simulator.CCD_EXPOSURE.EXPOSURE=0.5" \
+			      "CCD Imager Simulator.CCD_EXPOSURE" \
+			      "EXPOSURE = 0" \
+			      "1"
+
+	fn="${TMPDIR}IMAGE_$(printf "%03d" 001).`echo "${f}" | awk '{print tolower($0)}'`"
+	local file_size=$(wc -c ${fn} | awk '{print $1}')
+	[[ ! -f  ${fn} ]] && { __log_error "exposure '0.5 secs' failed, ${f} file '${fn}' does not exist"; exit 1; }
+	[[ ${file_size} -eq 0 ]] && { __log_error "file '${fn}' has size 0 bytes"; exit 1; }
+
+	if [[ "${f}" == "FITS" ]]; then
+	    $(file -b "${fn}" | grep -q "^FITS image data")
+	    [[ $? -ne 0 ]] && { __log_error "file '${fn}' is not in format '${f}'"; exit 1; }
+	elif [[ "${f}" == "XISF" ]]; then
+	    $(head -c 8 "${fn}" | grep -q "^XISF0100")
+	    [[ $? -ne 0 ]] && { __log_error "file '${fn}' is not in format '${f}'"; exit 1; }
+	elif [[ "${f}" == "JPEG" ]]; then
+	    $(file -b "${fn}" | grep -q "^JPEG image data")
+	    [[ $? -ne 0 ]] && { __log_error "file '${fn}' is not in format '${f}'"; exit 1; }
+	elif [[ "${f}" == "RAW" ]]; then
+	    $(file -b "${fn}" | grep -q "^data")
+	    [[ $? -ne 0 ]] && { __log_error "file '${fn}' is not in format '${f}'"; exit 1; }
+	fi
+
+	__log_info "${f} file '${fn}' of size ${file_size} bytes successfully created"
+
     done
 }
 
@@ -224,12 +296,17 @@ while [[ $# -gt 0 ]]
 do
     arg="$1"
     case $arg in
-	-t|--travis-test)
-	    TRAVIS_TEST=1
+	-d|--driver-test)
+	    DRIVER_TEST=1
 	    ;;
 	-c|--capture-test)
 	    CAPTURE_TEST="$2"
 	    [[ -z "${CAPTURE_TEST}" ]] && { echo "<indigo_ccd_driver> argument is missing"; __usage; }
+	    shift
+	    ;;
+	-f|--format-test)
+	    FORMAT_TEST="$2"
+	    [[ -z "${FORMAT_TEST}" ]] && { echo "<indigo_ccd_driver> argument is missing"; __usage; }
 	    shift
 	    ;;
 	*)
@@ -241,19 +318,22 @@ do
 done
 
 # Check for missing arguments.
-[[ ${TRAVIS_TEST} -eq 0 ]] && [[ -z "${CAPTURE_TEST}" ]] &&
+[[ ${DRIVER_TEST} -eq 0 ]] && [[ -z "${CAPTURE_TEST}" ]] && [[ -z "${FORMAT_TEST}" ]] &&
     { echo "missing which test to run"; __usage; }
 
 #------------------- Tests INDIGO ------------------#
-# If capture test is chosen, then first make sure we have in
+# If capture/format test is chosen, then first make sure we have in
 # assoziative array the proper mapping, before we start the indigo server.
 [[ "${CAPTURE_TEST}" ]] && [[ -z $(__hash_func "${CAPTURE_TEST}") ]] &&
     { echo "driver '${CAPTURE_TEST}' does not exist test suite"; exit 1; }
+[[ "${FORMAT_TEST}" ]] && [[ -z $(__hash_func "${FORMAT_TEST}") ]] &&
+    { echo "driver '${FORMAT_TEST}' does not exist test suite"; exit 1; }
 
 __start_indigo_server
 
-[[ ${TRAVIS_TEST} -ne 0 ]] && __test_load_drivers
+[[ ${DRIVER_TEST} -ne 0 ]] && __test_load_drivers
 [[ ! -z "${CAPTURE_TEST}" ]] && __test_capture "${CAPTURE_TEST}"
+[[ ! -z "${FORMAT_TEST}" ]] && __test_format "${FORMAT_TEST}"
 
 __stop_indigo_server
 
