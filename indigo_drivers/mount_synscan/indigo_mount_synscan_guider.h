13693f9b1a0ecaf7b000a4467558f7891b9b3208
David Hulse <david.hulse@angie.ai>
2018-12-25 23:16:24 +0200

    Refactor synscan driver to use timer threads and locking
    Reorganise code for better maintenance

---
+++ indigo_drivers/mount_synscan/indigo_mount_synscan_guider.h
@@ -0,0 +1,15 @@
+//
+//  indigo_mount_synscan_guider.h
+//  indigo
+//
+//  Created by David Hulse on 24/12/2018.
+//  Copyright © 2018 CloudMakers, s. r. o. All rights reserved.
+//
+
+#ifndef indigo_mount_synscan_guider_h
+#define indigo_mount_synscan_guider_h
+
+#include "indigo_driver.h"
+#include "indigo_guider_driver.h"
+
+#endif /* indigo_mount_synscan_guider_h */
