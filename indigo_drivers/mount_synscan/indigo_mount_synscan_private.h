162ebc8b2f98ddec76e85d07fd1c5c6bcadda598
Peter Polakovic <peter.polakovic@gmail.com>
2019-01-24 18:55:36 +0100

    driver version incremented [skip ci]

--- indigo_drivers/mount_synscan/indigo_mount_synscan_private.h
+++ indigo_drivers/mount_synscan/indigo_mount_synscan_private.h
@@ -15,7 +15,7 @@
 //#include "indigo_timer.h"
 #include "indigo_mount_synscan_driver.h"
 
-#define DRIVER_VERSION			0x0002
+#define DRIVER_VERSION			0x0003
 #define DRIVER_NAME					"indigo_mount_synscan"
 
 #define PRIVATE_DATA        ((synscan_private_data *)device->private_data)
