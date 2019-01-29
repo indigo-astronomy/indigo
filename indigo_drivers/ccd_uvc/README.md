55dd2b9bf5c5487070432c757b97af26ab9f145b
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2019-01-29 17:13:35 +0100

    UVC CCD driver added

---
+++ indigo_drivers/ccd_uvc/README.md
@@ -0,0 +1,22 @@
+# UVC (USB Video Class) CCD driver
+
+
+## Supported devices
+
+* UVC cameras
+
+This driver supports hot-plug (multiple devices).
+
+## Supported platforms
+
+This driver is platform independent.
+
+## License
+
+INDIGO Astronomy open-source license.
+
+## Use
+
+indigo_server indigo_ccd_uvc
+
+## Status: Under development
---
+++ indigo_drivers/ccd_uvc/indigo_ccd_uvc.h
@@ -0,0 +1,47 @@
+// Copyright (c) 2019 CloudMakers, s. r. o.
+// All rights reserved.
+//
+// You can use this software under the terms of 'INDIGO Astronomy
+// open-source license' (see LICENSE.md).
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// version history
+// 2.0 by Peter Polakovic <peter.polakovic@cloudmakers.eu>
+
+/** INDIGO UVC CCD driver
+ \file indigo_ccd_uvc.h
+ */
+
+#ifndef ccd_uvc_h
+#define ccd_uvc_h
+
+#include "indigo_driver.h"
+#include "indigo_ccd_driver.h"
+#include "indigo_guider_driver.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Register UVC CCD hot-plug callback
+ */
+
+extern indigo_result indigo_ccd_uvc(indigo_driver_action action, indigo_driver_info *info);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ccd_uvc_h */
+
