705bf22cca3353c99e6b077afc70e1d654c5a642
Rumen G.Bogdanovski <rumen@skyarchive.org>
2019-05-15 11:40:58 +0300

    indigo_io: [PLEASE TEST WITH MOUNTS] rework indigo_open_serialX(); add indigo_open_serial_with_config()

--- indigo_libs/indigo_io.c
+++ indigo_libs/indigo_io.c
@@ -53,10 +53,202 @@
 
 #if defined(INDIGO_LINUX) || defined(INDIGO_MACOS)
 
+typedef struct {
+	int value;
+	size_t len;
+	char *str;
+} sbaud_rate;
+#define BR(str,val) { val, sizeof(str), str }
+
+static sbaud_rate br[] = {
+	BR(     "50", B50),
+	BR(     "75", B75),
+	BR(    "110", B110),
+	BR(    "134", B134),
+	BR(    "150", B150),
+	BR(    "200", B200),
+	BR(    "300", B300),
+	BR(    "600", B600),
+	BR(   "1200", B1200),
+	BR(   "1800", B1800),
+	BR(   "2400", B2400),
+	BR(   "4800", B4800),
+	BR(   "9600", B9600),
+	BR(  "19200", B19200),
+	BR(  "38400", B38400),
+	BR(  "57600", B57600),
+	BR( "115200", B115200),
+	BR( "230400", B230400),
+#if !defined(__APPLE__) && !defined(__MACH__)
+	BR( "460800", B460800),
+	BR( "500000", B500000),
+	BR( "576000", B576000),
+	BR( "921600", B921600),
+	BR("1000000", B1000000),
+	BR("1152000", B1152000),
+	BR("1500000", B1500000),
+	BR("2000000", B2000000),
+	BR("2500000", B2500000),
+	BR("3000000", B3000000),
+	BR("3500000", B3500000),
+	BR("4000000", B4000000),
+#endif /* not OSX */
+	BR(       "", 0),
+};
+
+
+/* map string to actual baudrate value */
+static int map_str_baudrate(const char *baudrate) {
+	sbaud_rate *brp = br;
+	while (strncmp(brp->str, baudrate, brp->len)) {
+		if (brp->str[0]=='\0') return -1;
+		brp++;
+	}
+
+	return brp->value;
+}
+
+
+static int configure_tty_options(struct termios *options, const char *baudrate) {
+	int cbits=CS8, cpar=0, ipar=IGNPAR, bstop=0;
+	int baudr=0;
+	char *mode;
+
+	/* firmat is 9600-8N1, so split baudrate from the rest */
+	mode = strchr(baudrate, '-');
+	if (mode == NULL) {
+		errno = EINVAL;
+		return -1;
+	}
+	*mode = '\0';
+	++mode;
+
+	baudr = map_str_baudrate(baudrate);
+	if (baudr == -1) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	if(strlen(mode) != 3) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	switch(mode[0]) {
+		case '8': cbits = CS8; break;
+		case '7': cbits = CS7; break;
+		case '6': cbits = CS6; break;
+		case '5': cbits = CS5; break;
+		default :
+			errno = EINVAL;
+			return -1;
+			break;
+	}
+
+	switch(mode[1]) {
+		case 'N':
+		case 'n':
+			cpar = 0;
+			ipar = IGNPAR;
+			break;
+		case 'E':
+		case 'e':
+			cpar = PARENB;
+			ipar = INPCK;
+			break;
+		case 'O':
+		case 'o':
+			cpar = (PARENB | PARODD);
+			ipar = INPCK;
+			break;
+		default :
+			errno = EINVAL;
+			return -1;
+			break;
+	}
+
+	switch(mode[2]) {
+		case '1': bstop = 0; break;
+		case '2': bstop = CSTOPB; break;
+		default :
+			errno = EINVAL;
+			return -1;
+			break;
+	}
+
+	memset(options, 0, sizeof(*options));  /* clear options struct */
+
+	options->c_cflag = cbits | cpar | bstop | CLOCAL | CREAD;
+	options->c_iflag = ipar;
+	options->c_oflag = 0;
+	options->c_lflag = 0;
+	options->c_cc[VMIN] = 0;       /* block untill n bytes are received */
+	options->c_cc[VTIME] = 50;     /* block untill a timer expires (n * 100 mSec.) */
+
+	cfsetispeed(options, baudr);
+	cfsetospeed(options, baudr);
+
+	return 0;
+}
+
+
+static int open_tty(const char *tty_name, const struct termios *options, struct termios *old_options) {
+	int tty_fd;
+
+	tty_fd = open(tty_name, O_RDWR | O_NOCTTY | O_SYNC);
+	if (tty_fd == -1) {
+		return -1;
+	}
+
+	if (old_options) {
+		if (tcgetattr(tty_fd, old_options) == -1) {
+			close(tty_fd);
+			return -1;
+		}
+	}
+
+	if (tcsetattr(tty_fd, TCSANOW, options) == -1) {
+		close(tty_fd);
+		return -1;
+	}
+
+	return tty_fd;
+}
+
+
+static void close_tty(int tty_fd, struct termios *old_options) {
+	int status;
+
+	if(old_options) tcsetattr(tty_fd, TCSANOW, old_options);
+	close(tty_fd);
+}
+
+
+
 int indigo_open_serial(const char *dev_file) {
 	return indigo_open_serial_with_speed(dev_file, 9600);
 }
 
+
+int indigo_open_serial_with_speed(const char *dev_file, int speed) {
+	char baud_str[32];
+
+	snprintf(baud_str, sizeof(baud_str), "%d-8N1", speed);
+	return indigo_open_serial_with_config(dev_file, baud_str);
+}
+
+/* baudconfig is in form "9600-8N1" */
+int indigo_open_serial_with_config(const char *dev_file, const char *baudconfig) {
+	struct termios to;
+
+	int res = configure_tty_options(&to, baudconfig);
+	if (res == -1) return res;
+
+	return open_tty(dev_file, &to, NULL);
+}
+
+
+/*
 int indigo_open_serial_with_speed(const char *dev_file, int speed) {
 	int dev_fd;
 	struct termios options;
@@ -87,6 +279,8 @@ int indigo_open_serial_with_speed(const char *dev_file, int speed) {
 
 	return dev_fd;
 }
+*/
+
 #endif
 
 int indigo_open_tcp(const char *host, int port) {
