6ab05f284a36ca5652fc932b0215a5796c045ad4
Thomas Stibor <thomas@stibor.net>
2019-06-08 11:48:01 +0200

    Disable/stop hostapd daemon in Wifi infrastructure mode
    If INDIGO Sky is in Wifi infrastructure mode and
    hostapd daemon is running, then this causes a malfunctioning
    of mDNS after some time.

---
+++ tools/rpi_ctrl_fix.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+RPI_CTRL="/usr/bin/rpi_ctrl.sh"
+DHCPCD_CONF="/etc/dhcpcd.conf"
+
+[ ! -f ${RPI_CTRL} ] && { echo "no patch applied because no INDIGO Sky installation"; exit 0; }
+[ ! -f ${DHCPCD_CONF} ] && { echo "cannot apply patch because ${DHCPCD_CONF} is missing"; exit 0; }
+
+if ! grep -Eq "^interface wlan0|^static ip_address=192.168.235.1/24|^nohook wpa_supplicant" /etc/dhcpcd.conf; then
+
+    if ! systemctl is-active --quiet hostapd; then
+	echo "INDIGO Sky is in WiFi infrastructure mode and hostapd daemon is not active"
+	exit 0
+    fi
+    
+    echo "INDIGO Sky is in WiFi infrastructure mode, try to stop and disable hostapd daemon"
+    # Stop hostapd daemon.
+    systemctl stop hostapd
+    if [ $? -eq 0 ]; then
+	echo "Daemon hostapd stopped"
+    else
+	echo "Cannot stop hostapd"
+    fi
+    # Disable hostapd daemon.
+    systemctl disable hostapd
+    if [ $? -eq 0 ]; then
+	echo "Daemon hostapd disabled"
+    else
+	echo "Cannot disable hostapd"
+    fi
+else
+    echo "INDIGO Sky is in WiFi access point mode, no software maintainance is required"
+fi
+
