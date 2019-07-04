4d68563f950987eaee146775b67068eb251ba7f7
Peter Polakovic <peter.polakovic@gmail.com>
2019-07-04 20:41:27 +0200

    ao_sx: simulator added, asynchronous command processing, code cleanup

---
+++ indigo_drivers/ao_sx/ao_simulator/ao_simulator.ino
@@ -0,0 +1,102 @@
+// SX AO simulator for Arduino
+//
+// Copyright (c) 2019 CloudMakers, s. r. o.
+// All rights reserved.
+//
+// You can use this software under the terms of 'INDIGO Astronomy
+// open-source license' (see LICENSE.md).
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+int limit = 50;
+int ra = 0;
+int dec = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(1000);
+  while (!Serial)
+    ;
+}
+
+void loop() {
+  char command = Serial.read();
+  char params[7] = { 0 };
+  if (command == 'K' || command == 'R') {
+    ra = dec = 0;
+    Serial.write("K");
+  } else if (command == 'G') {
+    Serial.readBytes(params, 6);
+    int steps = atoi(params + 1);
+    switch (params[0]) {
+      case 'N':
+        dec += steps;
+        if (dec > limit) {
+          dec == limit;
+          Serial.write("L");
+        } else {
+          Serial.write("G");
+        }
+        break;
+      case 'S':
+        dec -= steps;
+        if (dec < -limit) {
+          dec == -limit;
+          Serial.write("L");
+        } else {
+          Serial.write("G");
+        }
+        break;
+      case 'T':
+        ra += steps;
+        if (ra > limit) {
+          ra == limit;
+          Serial.write("L");
+        } else {
+          Serial.write("G");
+        }
+        break;
+      case 'W':
+        ra -= steps;
+        if (ra < -limit) {
+          ra == limit;
+          Serial.write("L");
+        } else {
+          Serial.write("G");
+        }
+        break;
+    }
+  } else if (command == 'M') {
+    Serial.readBytes(params, 6);
+    Serial.write("M");
+  } else if (command == 'L') {
+    char state = 0x30;
+    if (ra == limit) {
+      state |= 0x08;
+    } else if (ra == -limit) {
+      state |= 0x02;
+    }
+    if (dec == limit) {
+      state |= 0x04;
+    } else if (dec == -limit) {
+      state |= 0x01;
+    }
+    Serial.write(state);
+  } else if (command == 'V') {
+    Serial.write("V123");
+  } else if (command == 'X') {
+    Serial.write("Y");
+  } else if (command == 'U') {
+    Serial.write("Z");
+  }
+}
--- indigo_drivers/ao_sx/indigo_ao_sx.c
+++ indigo_drivers/ao_sx/indigo_ao_sx.c
@@ -23,7 +23,7 @@
  \file indigo_ao_sx.c
  */
 
-#define DRIVER_VERSION 0x0001
+#define DRIVER_VERSION 0x0002
 #define DRIVER_NAME	"indigo_ao_sx"
 
 #include <stdlib.h>
@@ -48,37 +48,14 @@
 typedef struct {
 	int handle;
 	int device_count;
-	pthread_mutex_t port_mutex;
+	pthread_mutex_t mutex;
 } sx_private_data;
 
-static bool sx_command(indigo_device *device, char *command, char *response, int max);
+// -------------------------------------------------------------------------------- Low level communication routines
 
-static bool sx_open(indigo_device *device) {
-	if (PRIVATE_DATA->device_count++ > 0)
-		return true;
-	char *name = DEVICE_PORT_ITEM->text.value;
-	PRIVATE_DATA->handle = indigo_open_serial(name);
-	if (PRIVATE_DATA->handle >= 0) {
-		INDIGO_DRIVER_LOG(DRIVER_NAME, "connected to %s", name);
-		char response[5];
-		if (sx_command(device, "X", response, 1) && *response == 'Y') {
-			if (sx_command(device, "V", response, 4) && *response == 'V') {
-				return true;
-			}
-		}
-	} else {
-		INDIGO_DRIVER_ERROR(DRIVER_NAME, "failed to connect to %s", name);
-	}
-	PRIVATE_DATA->device_count = 0;
-	return false;
-}
-
-static bool sx_command(indigo_device *device, char *command, char *response, int max) {
-	pthread_mutex_lock(&PRIVATE_DATA->port_mutex);
+static bool sx_flush(indigo_device *device) {
 	char c;
 	struct timeval tv;
-	*response = 0;
-	// flush
 	while (true) {
 		fd_set readout;
 		FD_ZERO(&readout);
@@ -87,20 +64,22 @@ static bool sx_command(indigo_device *device, char *command, char *response, int
 		tv.tv_usec = 1000;
 		long result = select(PRIVATE_DATA->handle+1, &readout, NULL, NULL, &tv);
 		if (result == 0)
-			break;
+			return true;
 		if (result < 0) {
-			pthread_mutex_unlock(&PRIVATE_DATA->port_mutex);
 			return false;
 		}
 		result = read(PRIVATE_DATA->handle, &c, 1);
 		if (result < 1) {
-			pthread_mutex_unlock(&PRIVATE_DATA->port_mutex);
 			return false;
 		}
 	}
-	// write command
+}
+
+static bool sx_command(indigo_device *device, char *command, char *response, int max) {
+	char c;
+	struct timeval tv;
+	*response = 0;
 	indigo_write(PRIVATE_DATA->handle, command, strlen(command));
-	// read response
 	if (response != NULL) {
 		int index = 0;
 		int timeout = *command == 'K' || *command == 'R' ? 15 : 1;
@@ -116,24 +95,41 @@ static bool sx_command(indigo_device *device, char *command, char *response, int
 			result = read(PRIVATE_DATA->handle, &c, 1);
 			if (result < 1) {
 				INDIGO_DRIVER_ERROR(DRIVER_NAME, "Failed to read from %s -> %s (%d)", DEVICE_PORT_ITEM->text.value, strerror(errno), errno);
-				pthread_mutex_unlock(&PRIVATE_DATA->port_mutex);
 				return false;
 			}
-			if (c < 0)
-				c = ':';
-			if (c == '#')
-				break;
 			response[index++] = c;
 		}
 		response[index] = 0;
 	}
-	pthread_mutex_unlock(&PRIVATE_DATA->port_mutex);
 	INDIGO_DRIVER_DEBUG(DRIVER_NAME, "Command %s -> %s", command, response != NULL ? response : "NULL");
 	return true;
 }
 
+static bool sx_open(indigo_device *device) {
+	if (PRIVATE_DATA->device_count++ > 0)
+		return true;
+	char *name = DEVICE_PORT_ITEM->text.value;
+	PRIVATE_DATA->handle = indigo_open_serial(name);
+	if (PRIVATE_DATA->handle >= 0) {
+		char response[5];
+		if (sx_flush(device)) {
+			if (sx_command(device, "X", response, 1) && *response == 'Y') {
+				if (sx_command(device, "V", response, 4) && *response == 'V') {
+					INDIGO_DRIVER_LOG(DRIVER_NAME, "connected to %s", name);
+					return true;
+				}
+			}
+		}
+		INDIGO_DRIVER_ERROR(DRIVER_NAME, "handshake failed on %s", name);
+	} else {
+		INDIGO_DRIVER_ERROR(DRIVER_NAME, "failed to connect to %s", name);
+	}
+	PRIVATE_DATA->device_count = 0;
+	return false;
+}
+
 static void sx_close(indigo_device *device) {
-	if (PRIVATE_DATA->device_count-- > 0)
+	if (--PRIVATE_DATA->device_count > 0)
 		return;
 	if (PRIVATE_DATA->handle > 0) {
 		close(PRIVATE_DATA->handle);
@@ -158,6 +154,88 @@ static indigo_result ao_attach(indigo_device *device) {
 	return INDIGO_FAILED;
 }
 
+static void ao_connection_handler(indigo_device *device) {
+	pthread_mutex_lock(&PRIVATE_DATA->mutex);
+	if (CONNECTION_CONNECTED_ITEM->sw.value) {
+		CONNECTION_PROPERTY->state = INDIGO_BUSY_STATE;
+		indigo_update_property(device, CONNECTION_PROPERTY, NULL);
+		if (sx_open(device)) {
+			char response[2];
+			if (sx_command(device, "L", response, 1)) {
+				AO_GUIDE_DEC_PROPERTY->state = INDIGO_OK_STATE;
+				AO_GUIDE_RA_PROPERTY->state = INDIGO_OK_STATE;
+				if (response[0] & 0x05)
+					AO_GUIDE_DEC_PROPERTY->state = INDIGO_ALERT_STATE;
+				if (response[0] & 0x0A)
+					AO_GUIDE_RA_PROPERTY->state = INDIGO_ALERT_STATE;
+			}
+			CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
+		} else {
+			CONNECTION_PROPERTY->state = INDIGO_ALERT_STATE;
+			indigo_set_switch(CONNECTION_PROPERTY, CONNECTION_DISCONNECTED_ITEM, true);
+		}
+	} else {
+		sx_close(device);
+		CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
+	}
+	indigo_ao_change_property(device, NULL, CONNECTION_PROPERTY);
+	pthread_mutex_unlock(&PRIVATE_DATA->mutex);
+}
+
+static void ao_guide_dec_handler(indigo_device *device) {
+	char response[2], command[16];
+	pthread_mutex_lock(&PRIVATE_DATA->mutex);
+	if (AO_GUIDE_NORTH_ITEM->number.value > 0) {
+		sprintf(command, "GN%05.0f", AO_GUIDE_NORTH_ITEM->number.value);
+		sx_command(device, command, response, 1);
+	} else if (AO_GUIDE_SOUTH_ITEM->number.value > 0) {
+		sprintf(command, "GS%05.0f", AO_GUIDE_SOUTH_ITEM->number.value);
+		sx_command(device, command, response, 1);
+	}
+	AO_GUIDE_NORTH_ITEM->number.value = AO_GUIDE_SOUTH_ITEM->number.value = 0;
+	AO_GUIDE_DEC_PROPERTY->state = *response == 'G' ? INDIGO_OK_STATE : INDIGO_ALERT_STATE;
+	indigo_update_property(device, AO_GUIDE_DEC_PROPERTY, NULL);
+	pthread_mutex_unlock(&PRIVATE_DATA->mutex);
+}
+
+static void ao_guide_ra_handler(indigo_device *device) {
+	char response[2], command[16];
+	pthread_mutex_lock(&PRIVATE_DATA->mutex);
+	if (AO_GUIDE_WEST_ITEM->number.value > 0) {
+		sprintf(command, "GW%05.0f", AO_GUIDE_WEST_ITEM->number.value);
+		sx_command(device, command, response, 1);
+	} else if (AO_GUIDE_EAST_ITEM->number.value > 0) {
+		sprintf(command, "GT%05.0f", AO_GUIDE_EAST_ITEM->number.value);
+		sx_command(device, command, response, 1);
+	}
+	AO_GUIDE_WEST_ITEM->number.value = AO_GUIDE_EAST_ITEM->number.value = 0;
+	AO_GUIDE_RA_PROPERTY->state = *response == 'G' ? INDIGO_OK_STATE : INDIGO_ALERT_STATE;
+	indigo_update_property(device, AO_GUIDE_RA_PROPERTY, NULL);
+	pthread_mutex_unlock(&PRIVATE_DATA->mutex);
+}
+
+static void ao_reset_handler(indigo_device *device) {
+	char response[2] = { 0 };
+	pthread_mutex_lock(&PRIVATE_DATA->mutex);
+	if (AO_CENTER_ITEM->sw.value) {
+		sx_command(device, "K", response, 1);
+		AO_GUIDE_DEC_PROPERTY->state = INDIGO_OK_STATE;
+		indigo_update_property(device, AO_GUIDE_DEC_PROPERTY, NULL);
+		AO_GUIDE_RA_PROPERTY->state = INDIGO_OK_STATE;
+		indigo_update_property(device, AO_GUIDE_RA_PROPERTY, NULL);
+	} else if (AO_UNJAM_ITEM->sw.value) {
+		sx_command(device, "R", response, 1);
+		AO_GUIDE_DEC_PROPERTY->state = INDIGO_OK_STATE;
+		indigo_update_property(device, AO_GUIDE_DEC_PROPERTY, NULL);
+		AO_GUIDE_RA_PROPERTY->state = INDIGO_OK_STATE;
+		indigo_update_property(device, AO_GUIDE_RA_PROPERTY, NULL);
+	}
+	AO_CENTER_ITEM->sw.value = AO_UNJAM_ITEM->sw.value = false;
+	AO_RESET_PROPERTY->state = *response == 'K' ? INDIGO_OK_STATE : INDIGO_ALERT_STATE;
+	indigo_update_property(device, AO_RESET_PROPERTY, NULL);
+	pthread_mutex_unlock(&PRIVATE_DATA->mutex);
+}
+
 static indigo_result ao_change_property(indigo_device *device, indigo_client *client, indigo_property *property) {
 	assert(device != NULL);
 	assert(DEVICE_CONTEXT != NULL);
@@ -165,78 +243,22 @@ static indigo_result ao_change_property(indigo_device *device, indigo_client *cl
 	if (indigo_property_match(CONNECTION_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- CONNECTION
 		indigo_property_copy_values(CONNECTION_PROPERTY, property, false);
-		if (CONNECTION_CONNECTED_ITEM->sw.value) {
-			CONNECTION_PROPERTY->state = INDIGO_BUSY_STATE;
-			indigo_update_property(device, CONNECTION_PROPERTY, NULL);
-			if (sx_open(device)) {
-				char response[2];
-				if (sx_command(device, "L", response, 1)) {
-					AO_GUIDE_DEC_PROPERTY->state = INDIGO_OK_STATE;
-					AO_GUIDE_RA_PROPERTY->state = INDIGO_OK_STATE;
-					if (response[0] & 0x05)
-						AO_GUIDE_DEC_PROPERTY->state = INDIGO_ALERT_STATE;
-					if (response[0] & 0x0A)
-						AO_GUIDE_RA_PROPERTY->state = INDIGO_ALERT_STATE;
-				}
-				CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
-			} else {
-				CONNECTION_PROPERTY->state = INDIGO_ALERT_STATE;
-				indigo_set_switch(CONNECTION_PROPERTY, CONNECTION_DISCONNECTED_ITEM, true);
-			}
-		} else {
-			sx_close(device);
-			CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
-		}
+		indigo_set_timer(device, 0, ao_connection_handler);
+		return INDIGO_OK;
 	} else if (indigo_property_match(AO_GUIDE_DEC_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- AO_GUIDE_DEC
 		indigo_property_copy_values(AO_GUIDE_DEC_PROPERTY, property, false);
-		char response[2], command[16];
-		if (AO_GUIDE_NORTH_ITEM->number.value > 0) {
-			sprintf(command, "GN%05.0f", AO_GUIDE_NORTH_ITEM->number.value);
-			sx_command(device, command, response, 1);
-		} else if (AO_GUIDE_SOUTH_ITEM->number.value > 0) {
-			sprintf(command, "GS%05.0f", AO_GUIDE_SOUTH_ITEM->number.value);
-			sx_command(device, command, response, 1);
-		}
-		AO_GUIDE_NORTH_ITEM->number.value = AO_GUIDE_SOUTH_ITEM->number.value = 0;
-		AO_GUIDE_DEC_PROPERTY->state = *response == 'G' ? INDIGO_OK_STATE : INDIGO_ALERT_STATE;
-		indigo_update_property(device, AO_GUIDE_DEC_PROPERTY, NULL);
+		indigo_set_timer(device, 0, ao_guide_dec_handler);
 		return INDIGO_OK;
 	} else if (indigo_property_match(AO_GUIDE_RA_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- AO_GUIDE_RA
 		indigo_property_copy_values(AO_GUIDE_RA_PROPERTY, property, false);
-		char response[2], command[16];
-		if (AO_GUIDE_WEST_ITEM->number.value > 0) {
-			sprintf(command, "GW%05.0f", AO_GUIDE_WEST_ITEM->number.value);
-			sx_command(device, command, response, 1);
-		} else if (AO_GUIDE_EAST_ITEM->number.value > 0) {
-			sprintf(command, "GT%05.0f", AO_GUIDE_EAST_ITEM->number.value);
-			sx_command(device, command, response, 1);
-		}
-		AO_GUIDE_WEST_ITEM->number.value = AO_GUIDE_EAST_ITEM->number.value = 0;
-		AO_GUIDE_RA_PROPERTY->state = *response == 'G' ? INDIGO_OK_STATE : INDIGO_ALERT_STATE;
-		indigo_update_property(device, AO_GUIDE_RA_PROPERTY, NULL);
+		indigo_set_timer(device, 0, ao_guide_ra_handler);
 		return INDIGO_OK;
 	} else if (indigo_property_match(AO_RESET_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- AO_RESET
 		indigo_property_copy_values(AO_RESET_PROPERTY, property, false);
-		char response[2] = { 0 };
-		if (AO_CENTER_ITEM->sw.value) {
-			sx_command(device, "K", response, 1);
-			AO_GUIDE_DEC_PROPERTY->state = INDIGO_OK_STATE;
-			indigo_update_property(device, AO_GUIDE_DEC_PROPERTY, NULL);
-			AO_GUIDE_RA_PROPERTY->state = INDIGO_OK_STATE;
-			indigo_update_property(device, AO_GUIDE_RA_PROPERTY, NULL);
-		} else if (AO_UNJAM_ITEM->sw.value) {
-			sx_command(device, "R", response, 1);
-			AO_GUIDE_DEC_PROPERTY->state = INDIGO_OK_STATE;
-			indigo_update_property(device, AO_GUIDE_DEC_PROPERTY, NULL);
-			AO_GUIDE_RA_PROPERTY->state = INDIGO_OK_STATE;
-			indigo_update_property(device, AO_GUIDE_RA_PROPERTY, NULL);
-		}
-		AO_CENTER_ITEM->sw.value = AO_UNJAM_ITEM->sw.value = false;
-		AO_RESET_PROPERTY->state = *response == 'K' ? INDIGO_OK_STATE : INDIGO_ALERT_STATE;
-		indigo_update_property(device, AO_RESET_PROPERTY, NULL);
+		indigo_set_timer(device, 0, ao_reset_handler);
 		return INDIGO_OK;
 		// --------------------------------------------------------------------------------
 	}
@@ -266,6 +288,57 @@ static indigo_result guider_attach(indigo_device *device) {
 	return INDIGO_FAILED;
 }
 
+static void guider_connection_handler(indigo_device *device) {
+	pthread_mutex_lock(&PRIVATE_DATA->mutex);
+	if (CONNECTION_CONNECTED_ITEM->sw.value) {
+		CONNECTION_PROPERTY->state = INDIGO_BUSY_STATE;
+		indigo_update_property(device, CONNECTION_PROPERTY, NULL);
+		if (sx_open(device)) {
+			CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
+		} else {
+			CONNECTION_PROPERTY->state = INDIGO_ALERT_STATE;
+			indigo_set_switch(CONNECTION_PROPERTY, CONNECTION_DISCONNECTED_ITEM, true);
+		}
+	} else {
+		sx_close(device);
+		CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
+	}
+	indigo_guider_change_property(device, NULL, CONNECTION_PROPERTY);
+	pthread_mutex_unlock(&PRIVATE_DATA->mutex);
+}
+
+static void guider_guide_dec_handler(indigo_device *device) {
+	char response[2], command[16];
+	pthread_mutex_lock(&PRIVATE_DATA->mutex);
+	if (GUIDER_GUIDE_NORTH_ITEM->number.value > 0) {
+		sprintf(command, "MN%05.0f", GUIDER_GUIDE_NORTH_ITEM->number.value / 10);
+		sx_command(device, command, response, 1);
+	} else if (GUIDER_GUIDE_SOUTH_ITEM->number.value > 0) {
+		sprintf(command, "MS%05.0f", GUIDER_GUIDE_SOUTH_ITEM->number.value / 10);
+		sx_command(device, command, response, 1);
+	}
+	GUIDER_GUIDE_NORTH_ITEM->number.value = GUIDER_GUIDE_SOUTH_ITEM->number.value = 0;
+	GUIDER_GUIDE_DEC_PROPERTY->state = *response == 'M' ? INDIGO_OK_STATE : INDIGO_ALERT_STATE;
+	indigo_update_property(device, GUIDER_GUIDE_DEC_PROPERTY, NULL);
+	pthread_mutex_unlock(&PRIVATE_DATA->mutex);
+}
+
+static void guider_guide_ra_handler(indigo_device *device) {
+	char response[2], command[16];
+	pthread_mutex_lock(&PRIVATE_DATA->mutex);
+	if (GUIDER_GUIDE_WEST_ITEM->number.value > 0) {
+		sprintf(command, "MW%05.0f", GUIDER_GUIDE_WEST_ITEM->number.value / 10);
+		sx_command(device, command, response, 1);
+	} else if (GUIDER_GUIDE_EAST_ITEM->number.value > 0) {
+		sprintf(command, "MT%05.0f", GUIDER_GUIDE_EAST_ITEM->number.value / 10);
+		sx_command(device, command, response, 1);
+	}
+	GUIDER_GUIDE_WEST_ITEM->number.value = GUIDER_GUIDE_EAST_ITEM->number.value = 0;
+	GUIDER_GUIDE_RA_PROPERTY->state = *response == 'M' ? INDIGO_OK_STATE : INDIGO_ALERT_STATE;
+	indigo_update_property(device, GUIDER_GUIDE_RA_PROPERTY, NULL);
+	pthread_mutex_unlock(&PRIVATE_DATA->mutex);
+}
+
 static indigo_result guider_change_property(indigo_device *device, indigo_client *client, indigo_property *property) {
 	assert(device != NULL);
 	assert(DEVICE_CONTEXT != NULL);
@@ -273,48 +346,17 @@ static indigo_result guider_change_property(indigo_device *device, indigo_client
 	if (indigo_property_match(CONNECTION_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- CONNECTION
 		indigo_property_copy_values(CONNECTION_PROPERTY, property, false);
-		if (CONNECTION_CONNECTED_ITEM->sw.value) {
-			CONNECTION_PROPERTY->state = INDIGO_BUSY_STATE;
-			indigo_update_property(device, CONNECTION_PROPERTY, NULL);
-			if (sx_open(device)) {
-				CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
-			} else {
-				CONNECTION_PROPERTY->state = INDIGO_ALERT_STATE;
-				indigo_set_switch(CONNECTION_PROPERTY, CONNECTION_DISCONNECTED_ITEM, true);
-			}
-		} else {
-			sx_close(device);
-			CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
-		}
+		CONNECTION_PROPERTY->state = INDIGO_BUSY_STATE;
+		indigo_set_timer(device, 0, guider_connection_handler);
 	} else if (indigo_property_match(GUIDER_GUIDE_DEC_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- GUIDER_GUIDE_DEC
 		indigo_property_copy_values(GUIDER_GUIDE_DEC_PROPERTY, property, false);
-		char response[2], command[16];
-		if (GUIDER_GUIDE_NORTH_ITEM->number.value > 0) {
-			sprintf(command, "MN%05.0f", GUIDER_GUIDE_NORTH_ITEM->number.value / 10);
-			sx_command(device, command, response, 1);
-		} else if (GUIDER_GUIDE_SOUTH_ITEM->number.value > 0) {
-			sprintf(command, "MS%05.0f", GUIDER_GUIDE_SOUTH_ITEM->number.value / 10);
-			sx_command(device, command, response, 1);
-		}
-		GUIDER_GUIDE_NORTH_ITEM->number.value = GUIDER_GUIDE_SOUTH_ITEM->number.value = 0;
-		GUIDER_GUIDE_DEC_PROPERTY->state = *response == 'M' ? INDIGO_OK_STATE : INDIGO_ALERT_STATE;
-		indigo_update_property(device, GUIDER_GUIDE_DEC_PROPERTY, NULL);
+		indigo_set_timer(device, 0, guider_guide_dec_handler);
 		return INDIGO_OK;
 	} else if (indigo_property_match(GUIDER_GUIDE_RA_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- GUIDER_GUIDE_RA
 		indigo_property_copy_values(GUIDER_GUIDE_RA_PROPERTY, property, false);
-		char response[2], command[16];
-		if (GUIDER_GUIDE_WEST_ITEM->number.value > 0) {
-			sprintf(command, "MW%05.0f", GUIDER_GUIDE_WEST_ITEM->number.value / 10);
-			sx_command(device, command, response, 1);
-		} else if (GUIDER_GUIDE_EAST_ITEM->number.value > 0) {
-			sprintf(command, "MT%05.0f", GUIDER_GUIDE_EAST_ITEM->number.value / 10);
-			sx_command(device, command, response, 1);
-		}
-		GUIDER_GUIDE_WEST_ITEM->number.value = GUIDER_GUIDE_EAST_ITEM->number.value = 0;
-		GUIDER_GUIDE_RA_PROPERTY->state = *response == 'M' ? INDIGO_OK_STATE : INDIGO_ALERT_STATE;
-		indigo_update_property(device, GUIDER_GUIDE_RA_PROPERTY, NULL);
+		indigo_set_timer(device, 0, guider_guide_ra_handler);
 		return INDIGO_OK;
 		// --------------------------------------------------------------------------------
 	}
@@ -328,7 +370,8 @@ static indigo_result guider_detach(indigo_device *device) {
 	INDIGO_DEVICE_DETACH_LOG(DRIVER_NAME, device->name);
 	return indigo_ao_detach(device);
 }
-// --------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------- INDIGO driver implementation
 
 static sx_private_data *private_data = NULL;
 static indigo_device *ao = NULL;
@@ -366,6 +409,7 @@ indigo_result indigo_ao_sx(indigo_driver_action action, indigo_driver_info *info
 			private_data = malloc(sizeof(sx_private_data));
 			assert(private_data != NULL);
 			memset(private_data, 0, sizeof(sx_private_data));
+			pthread_mutex_init(&private_data->mutex, NULL);
 			ao = malloc(sizeof(indigo_device));
 			assert(ao != NULL);
 			memcpy(ao, &ao_template, sizeof(indigo_device));
