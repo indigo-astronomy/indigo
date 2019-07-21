7df1b934b1e8b6fe9a71e3b3ef459b52c85a775e
Peter Polakovic <peter.polakovic@gmail.com>
2019-07-21 10:36:22 +0200

    Makefile: docker build support added

---
+++ tools/build_debs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Copyright (c) 2019 CloudMakers, s. r. o.
+# All rights reserved.
+#
+# You can use this software under the terms of 'INDIGO Astronomy
+# open-source license'
+# (see https://github.com/indigo-astronomy/indigo/blob/master/LICENSE.md).
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+echo FROM $1 >Dockerfile
+cat >>Dockerfile <<EOF
+LABEL maintainer="peter.polakovic@cloudmakers.eu"
+RUN apt-get -y update && apt-get -y install wget unzip build-essential autoconf autotools-dev libtool cmake libudev-dev libavahi-compat-libdnssd-dev libusb-1.0-0-dev fxload libcurl4-gnutls-dev libgphoto2-dev libz-dev git curl bsdmainutils
+RUN wget https://github.com/indigo-astronomy/indigo/archive/master.zip
+RUN unzip master.zip
+RUN rm master.zip
+WORKDIR indigo-master
+RUN make package
+EOF
+docker build -t indigo .
+docker create --name indigo indigo
+docker cp indigo:/indigo-master/$2 .
+docker container rm indigo
+docker image rm indigo
+rm Dockerfile
