ad238f9f0ce573657469bc582eb6108a92d2d315
Peter Polakovic <peter.polakovic@gmail.com>
2019-08-17 21:25:24 +0200

    FOCUSER_ABORT_MOTION fix reveted

--- indigo_drivers/focuser_nstep/indigo_focuser_nstep.c
+++ indigo_drivers/focuser_nstep/indigo_focuser_nstep.c
@@ -341,7 +341,7 @@ static void focuser_steps_handler(indigo_device *device) {
 }
 
 static void focuser_abort_handler(indigo_device *device) {
-	//pthread_mutex_lock(&PRIVATE_DATA->mutex);
+	pthread_mutex_lock(&PRIVATE_DATA->mutex);
 	if (FOCUSER_ABORT_MOTION_ITEM->sw.value) {
 		FOCUSER_ABORT_MOTION_ITEM->sw.value = false;
 		if (nstep_command(device, ":F10000#", NULL, 0)) {
@@ -355,7 +355,7 @@ static void focuser_abort_handler(indigo_device *device) {
 		}
 	}
 	indigo_update_property(device, FOCUSER_ABORT_MOTION_PROPERTY, NULL);
-	//pthread_mutex_unlock(&PRIVATE_DATA->mutex);
+	pthread_mutex_unlock(&PRIVATE_DATA->mutex);
 }
 
 static void focuser_phase_wiring_handler(indigo_device *device) {
