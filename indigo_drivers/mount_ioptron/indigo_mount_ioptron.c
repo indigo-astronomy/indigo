5ab6fac250d4f2da356f4301018cd18c52b2228a
Peter Polakovic <peter.polakovic@gmail.com>
2019-09-10 22:07:18 +0200

    mount_ioptron: set date skiped for protocol 0

--- indigo_drivers/mount_ioptron/indigo_mount_ioptron.c
+++ indigo_drivers/mount_ioptron/indigo_mount_ioptron.c
@@ -1092,10 +1092,12 @@ static indigo_result mount_change_property(indigo_device *device, indigo_client 
 				MOUNT_SET_HOST_TIME_PROPERTY->state = INDIGO_ALERT_STATE;
 			} else {
 				if (PRIVATE_DATA->gotonova || PRIVATE_DATA->protocol <= 0x0100)
-					sprintf(command, ":SC%02d/%02d/%02d#", tm.tm_mon + 1, tm.tm_mday, tm.tm_year - 100);
+					sprintf(command, ":SC%02d/%02d/%02d#", tm.tm_mon + 1, tm.tm_mday, tm.tm_year % 100);
 				else if (PRIVATE_DATA->protocol >= 0x0200)
-					sprintf(command, ":SC%02d%02d%02d#", tm.tm_year - 100, tm.tm_mon + 1, tm.tm_mday);
-				if (!ieq_command(device, command, response, 1) || *response != '1') {
+					sprintf(command, ":SC%02d%02d%02d#", tm.tm_year % 100, tm.tm_mon + 1, tm.tm_mday);
+				if (PRIVATE_DATA->gotonova || PRIVATE_DATA->protocol < 0x0100)
+					MOUNT_SET_HOST_TIME_PROPERTY->state = INDIGO_OK_STATE;
+				else if (!ieq_command(device, command, response, 1) || *response != '1') {
 					MOUNT_SET_HOST_TIME_PROPERTY->state = INDIGO_ALERT_STATE;
 				} else {
 					if (PRIVATE_DATA->gotonova)
