7689ec72f6a8c3eee2152069939b6c40f1566a2f
Rumen G.Bogdanovski <rumenastro@gmail.com>
2019-11-01 15:28:16 +0200

    indigo_dome_nexdome: implement OPEN and CLOSE in v3 simulator

---
+++ indigo_drivers/dome_nexdome/nexdome_simulator_v3/nexdome_simulator_v3_open_close_test.py
@@ -0,0 +1,54 @@
+import serial
+from time import sleep
+import sys
+from sys import platform as _platform
+
+
+def main():
+
+    if len(sys.argv) > 1:
+        serial_port_name = sys.argv[1]
+    elif _platform == "linux" or _platform == "linux2":
+        serial_port_name = '/dev/ttyACM0'
+        print "Connecting to "
+    elif _platform == "win32" or _platform == "win64":
+        serial_port_name = 'COM7'
+
+    print "Connecting to " + serial_port_name + "..."
+    
+    
+    try:
+        #Connect to serial port
+        serial_port = serial.Serial(
+                  port = serial_port_name,\
+                  baudrate = 9600,\
+                  parity = serial.PARITY_NONE,\
+                  stopbits = serial.STOPBITS_ONE,\
+                  bytesize = serial.EIGHTBITS,\
+                  timeout = 0)
+    
+        serial_port.write("@OPS\n")
+        received = ""
+        while 0 > received.find(":SES"):
+            sleep(0.1);
+            received = serial_port.read(1000)
+            if received:
+                print received;
+
+        serial_port.write("@CLS\n")
+        received = ""
+        while 0 > received.find(":SES"):
+            sleep(0.1);
+            received = serial_port.read(1000)
+            if received:
+                print received;
+
+
+    
+        serial_port.close()
+    
+    except serial.serialutil.SerialException:
+        print 'Serial port not available!'
+    
+if __name__ == "__main__":
+    main()
