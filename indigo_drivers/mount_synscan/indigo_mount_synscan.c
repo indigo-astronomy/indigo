7be4bf601b3b92702e9f4a4dba8cc6056aac0162
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2019-11-01 15:12:25 +0100

    mount_synscan: try to guess correct baudrate on new EQ6-R

--- indigo_drivers/mount_synscan/indigo_mount_synscan.c
+++ indigo_drivers/mount_synscan/indigo_mount_synscan.c
@@ -86,6 +86,8 @@ static indigo_result mount_attach(indigo_device *device) {
 		DEVICE_PORTS_PROPERTY->hidden = false;
 		// -------------------------------------------------------------------------------- DEVICE_PORT
 		DEVICE_PORT_PROPERTY->hidden = false;
+		// -------------------------------------------------------------------------------- DEVICE_BAUDRATE
+		DEVICE_BAUDRATE_PROPERTY->hidden = false;
 		// -------------------------------------------------------------------------------- MOUNT_ALIGNMENT_MODE
 		indigo_set_switch(MOUNT_ALIGNMENT_MODE_PROPERTY, MOUNT_ALIGNMENT_MODE_NEAREST_POINT_ITEM, true);
 		MOUNT_ALIGNMENT_MODE_PROPERTY->hidden = false;
