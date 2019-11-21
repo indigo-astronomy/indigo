024da8452fe625b6b520613e24d119b8f6134d51
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2019-11-21 15:27:47 +0100

    file header updated

--- indigo_libs/indigo/indigo_guider_utils.h
+++ indigo_libs/indigo/indigo_guider_utils.h
@@ -1,19 +1,28 @@
+// Copyright (c) 2019 CloudMakers, s. r. o.
+// All rights reserved.
 //
-//  indigo_guider_utils.h
-//  indigo
+// You can use this software under the terms of 'INDIGO Astronomy
+// open-source license' (see LICENSE.md).
 //
-//  Created by Peter Polakovic on 14/04/2019.
-//  Copyright © 2019 CloudMakers, s. r. o. All rights reserved.
-//
-//  Based on libguider by Rumen Bogdanovski
-//  Copyright © 2015 Rumen Bogdanovski All rights reserved.
-//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// version history
+// 2.0 by Peter Polakovic <peter.polakovic@cloudmakers.eu>
 
 #ifndef indigo_guider_utils_h
 #define indigo_guider_utils_h
 
 #include <stdio.h>
-
 
 typedef enum { none = 0, centroid, donuts } indigo_guide_algorithm;
 
