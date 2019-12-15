44f8a5d3f9d9cd5e39a532996eef16fa2fd937d0
Peter Polakovic <peter.polakovic@gmail.com>
2019-12-15 19:37:59 +0100

    mount_synscan: baudrate detection fixed

--- indigo_drivers/mount_synscan/indigo_mount_synscan_private.h
+++ indigo_drivers/mount_synscan/indigo_mount_synscan_private.h
@@ -15,7 +15,7 @@
 //#include "indigo_timer.h"
 #include "indigo_mount_synscan_driver.h"
 
-#define DRIVER_VERSION			0x000D
+#define DRIVER_VERSION			0x000E
 #define DRIVER_NAME					"indigo_mount_synscan"
 
 #define PRIVATE_DATA        ((synscan_private_data *)device->private_data)
