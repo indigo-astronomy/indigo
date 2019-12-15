476805a5426f599380b8aa556ad7c4ca40ed710b
Peter Polakovic <peter.polakovic@gmail.com>
2019-12-15 18:43:20 +0100

    mount_synscan: disconnection detection removed

--- indigo_drivers/mount_synscan/indigo_mount_synscan_protocol.c
+++ indigo_drivers/mount_synscan/indigo_mount_synscan_protocol.c
@@ -205,10 +205,10 @@ static bool synscan_command(indigo_device* device, const char* cmd, char* r) {
 		}
 	}
 
-	if (nretries == 2 && !PRIVATE_DATA->udp) {
-		indigo_send_message(device, "Lost connection");
-		indigo_device_disconnect(NULL, device->name);
-	}
+//	if (nretries == 2 && !PRIVATE_DATA->udp && cmd[1] != 'q') {
+//		indigo_send_message(device, "Lost connection");
+//		indigo_device_disconnect(NULL, device->name);
+//	}
 	
 	//  Mount command failed
 	pthread_mutex_unlock(&PRIVATE_DATA->port_mutex);
