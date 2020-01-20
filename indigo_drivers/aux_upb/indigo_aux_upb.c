36624f6142a5cfb8f65df14422fb146558c3c6ef
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2020-01-20 10:22:30 +0100

    aux_upb: max focuser speed fixed

--- indigo_drivers/aux_upb/indigo_aux_upb.c
+++ indigo_drivers/aux_upb/indigo_aux_upb.c
@@ -1307,7 +1307,7 @@ static indigo_result focuser_attach(indigo_device *device) {
 		FOCUSER_TEMPERATURE_PROPERTY->hidden = false;
 		// -------------------------------------------------------------------------------- FOCUSER_SPEED
 		FOCUSER_SPEED_ITEM->number.min = 100;
-		FOCUSER_SPEED_ITEM->number.max = 500;
+		FOCUSER_SPEED_ITEM->number.max = 1000;
 		FOCUSER_SPEED_ITEM->number.step = 1;
 		// -------------------------------------------------------------------------------- FOCUSER_STEPS
 		FOCUSER_STEPS_ITEM->number.min = 1;
