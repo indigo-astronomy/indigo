5069ffaad40811e1609590fcb9376bb0c38382ac
Rumen G.Bogdanovski <rumenastro@gmail.com>
2020-04-30 12:20:08 +0300

    indigo_wheel_manual: created the driver

---
+++ indigo_drivers/wheel_manual/README.md
@@ -0,0 +1,22 @@
+# CCD Manual Filter wheel driver
+
+## Supported devices
+
+* Manual filter wheel
+
+All devices are present on startup. The driver does not control any hardware. It should be used 
+to keep a record of the used filters with a manual filter wheels.
+
+## Supported platforms
+
+This driver is platform independent.
+
+## License
+
+INDIGO Astronomy open-source license.
+
+## Use
+
+indigo_server indigo_wheel_manual
+
+## Status: Stable
---
+++ indigo_drivers/wheel_manual/indigo_wheel_manual.c
@@ -0,0 +1,139 @@
+// Copyright (c) 2020 Rumen G.Bogdanovski
+// All rights reserved.
+//
+// You can use this software under the terms of 'INDIGO Astronomy
+// open-source license' (see LICENSE.md).
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// version history
+// 2.0 by Rumen G.Bogdanovski <rumen@skyarchive.org>
+
+/** INDIGO Manual filter wheel driver
+ \file indigo_wheel_manual.c
+ */
+
+#define DRIVER_VERSION 0x0001
+#define DRIVER_NAME	"indigo_wheel_manual"
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include <indigo/indigo_driver_xml.h>
+
+#include "indigo_wheel_manual.h"
+
+// gp_bits is used as boolean
+#define is_connected                     gp_bits
+
+// -------------------------------------------------------------------------------- INDIGO wheel device implementation
+
+#define FILTER_COUNT	8
+
+static indigo_result wheel_attach(indigo_device *device) {
+	assert(device != NULL);
+	if (indigo_wheel_attach(device, DRIVER_VERSION) == INDIGO_OK) {
+		// -------------------------------------------------------------------------------- WHEEL_SLOT, WHEEL_SLOT_NAME
+		WHEEL_SLOT_ITEM->number.max = WHEEL_SLOT_NAME_PROPERTY->count = WHEEL_SLOT_OFFSET_PROPERTY->count = FILTER_COUNT;
+		WHEEL_SLOT_ITEM->number.value = WHEEL_SLOT_ITEM->number.target = 1;
+		// --------------------------------------------------------------------------------
+		INDIGO_DEVICE_ATTACH_LOG(DRIVER_NAME, device->name);
+		return indigo_wheel_enumerate_properties(device, NULL, NULL);
+	}
+	return INDIGO_FAILED;
+}
+
+static indigo_result wheel_change_property(indigo_device *device, indigo_client *client, indigo_property *property) {
+	assert(device != NULL);
+	assert(DEVICE_CONTEXT != NULL);
+	assert(property != NULL);
+	if (indigo_property_match(CONNECTION_PROPERTY, property)) {
+		// -------------------------------------------------------------------------------- CONNECTION
+		indigo_property_copy_values(CONNECTION_PROPERTY, property, false);
+		CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
+	} else if (indigo_property_match(WHEEL_SLOT_PROPERTY, property)) {
+		// -------------------------------------------------------------------------------- WHEEL_SLOT
+		indigo_property_copy_values(WHEEL_SLOT_PROPERTY, property, false);
+		if (WHEEL_SLOT_ITEM->number.value < 1 || WHEEL_SLOT_ITEM->number.value > WHEEL_SLOT_ITEM->number.max) {
+			WHEEL_SLOT_PROPERTY->state = INDIGO_ALERT_STATE;
+			indigo_update_property(device, WHEEL_SLOT_PROPERTY, NULL);
+		} else {
+			char message[INDIGO_VALUE_SIZE];
+			WHEEL_SLOT_PROPERTY->state = INDIGO_OK_STATE;
+			int slot = (int)WHEEL_SLOT_ITEM->number.value - 1;
+			sprintf(message, "Make sure filter '%s' is selected on the device", WHEEL_SLOT_NAME_PROPERTY->items[slot].text.value);
+			indigo_update_property(device, WHEEL_SLOT_PROPERTY, message);
+		}
+		return INDIGO_OK;
+		// --------------------------------------------------------------------------------
+	}
+	return indigo_wheel_change_property(device, client, property);
+}
+
+static indigo_result wheel_detach(indigo_device *device) {
+	assert(device != NULL);
+	if (CONNECTION_CONNECTED_ITEM->sw.value)
+		indigo_device_disconnect(NULL, device->name);
+	INDIGO_DEVICE_DETACH_LOG(DRIVER_NAME, device->name);
+	return indigo_wheel_detach(device);
+}
+
+// --------------------------------------------------------------------------------
+
+static indigo_device *filter_wheel = NULL;
+
+indigo_result indigo_wheel_manual(indigo_driver_action action, indigo_driver_info *info) {
+
+	static indigo_device filter_wheel_template = INDIGO_DEVICE_INITIALIZER(
+		FILTER_WHEEL_NAME,
+		wheel_attach,
+		indigo_wheel_enumerate_properties,
+		wheel_change_property,
+		NULL,
+		wheel_detach
+	);
+
+	static indigo_driver_action last_action = INDIGO_DRIVER_SHUTDOWN;
+
+	SET_DRIVER_INFO(info, "Manual filter wheel", __FUNCTION__, DRIVER_VERSION, true, last_action);
+
+	if (action == last_action)
+		return INDIGO_OK;
+
+	switch(action) {
+		case INDIGO_DRIVER_INIT:
+			last_action = action;
+			filter_wheel = malloc(sizeof(indigo_device));
+			assert(filter_wheel != NULL);
+			memcpy(filter_wheel, &filter_wheel_template, sizeof(indigo_device));
+			filter_wheel->private_data = NULL;
+			indigo_attach_device(filter_wheel);
+			break;
+
+		case INDIGO_DRIVER_SHUTDOWN:
+			VERIFY_NOT_CONNECTED(filter_wheel);
+			last_action = action;
+			if (filter_wheel != NULL) {
+				indigo_detach_device(filter_wheel);
+				free(filter_wheel);
+				filter_wheel = NULL;
+			}
+			break;
+
+		case INDIGO_DRIVER_INFO:
+			break;
+	}
+	return INDIGO_OK;
+}
