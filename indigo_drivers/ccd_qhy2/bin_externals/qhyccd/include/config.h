268794aa47f6c3a901bc5fc3f392330d884e9e3a
Rumen G.Bogdanovski <rumenastro@gmail.com>
2020-05-12 12:53:41 +0300

    Revert "ccd_qhy2: SDK updated to 2020.05.08"
    This reverts commit db88fe663dcd06a383cdf2c30b3a9a36bc20d5d1.

--- indigo_drivers/ccd_qhy2/bin_externals/qhyccd/include/config.h
+++ indigo_drivers/ccd_qhy2/bin_externals/qhyccd/include/config.h
@@ -1,7 +1,21 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
+/*
+ 20, 2, 3,1
 
+0204
+294
+411
+411eris
+550
+6060
+QHYCCD  (Beijing) Technology Co., Ltd.
+ 
+system_profiler SPUSBDataType
+ 
+/lib/firmware/qhy
+*/
 
 
 
@@ -9,32 +23,25 @@
 #define CALLBACK_MODE_SUPPORT		1
 
 
+
 #if defined (_WIN32)
 #define QHYCCD_OPENCV_SUPPORT
 #define WINDOWS_PTHREAD_SUPPORT		1
 #define WINPCAP_MODE_SUPPORT		0
-#define PCIE_MODE_SUPPORT			1
+#define PCIE_MODE_SUPPORT			0
 #define CYUSB_MODE_SUPPORT  		1
 #define WINUSB_MODE_SUPPORT  		1
 #define LIBUSB_MODE_SUPPORT  		0
-#define PCIE_MODE_TEST  			1
+#define PCIE_CAPTURE_TEST			0
 #else
 #undef  QHYCCD_OPENCV_SUPPORT
 #define WINDOWS_PTHREAD_SUPPORT		0
 #define WINPCAP_MODE_SUPPORT		0
-
-#if defined(__arm__) || defined (__arm64__) || defined (__aarch64__) || defined(__APPLE__)||defined (__ANDROID__)
 #define PCIE_MODE_SUPPORT			0
-#elif defined(__i386__) || defined(__x86_64__)
-#define PCIE_MODE_SUPPORT			1
-#else
-#define PCIE_MODE_SUPPORT			0
-#endif
-
-
 #define CYUSB_MODE_SUPPORT  		0
 #define WINUSB_MODE_SUPPORT  		0
 #define LIBUSB_MODE_SUPPORT  		1
+#define PCIE_CAPTURE_TEST			0
 #endif
 
 
