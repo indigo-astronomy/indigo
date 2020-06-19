5044887cf8738a3504e2e9ebf3d2300db1b99477
Peter Polakovic <peter.polakovic@gmail.com>
2020-06-19 12:11:12 +0200

    double connection fixed in rotator drivers

--- indigo_drivers/rotator_simulator/indigo_rotator_simulator.c
+++ indigo_drivers/rotator_simulator/indigo_rotator_simulator.c
@@ -102,6 +102,8 @@ static indigo_result rotator_change_property(indigo_device *device, indigo_clien
 	assert(property != NULL);
 	if (indigo_property_match(CONNECTION_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- CONNECTION
+		if (indigo_ignore_connection_change(device, property))
+			return INDIGO_OK;
 		indigo_property_copy_values(CONNECTION_PROPERTY, property, false);
 		CONNECTION_PROPERTY->state = INDIGO_BUSY_STATE;
 		indigo_update_property(device, CONNECTION_PROPERTY, NULL);
