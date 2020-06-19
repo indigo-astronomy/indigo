dbbd2ddc83f70966bd3f0fe917de7077e9d9cc48
Peter Polakovic <peter.polakovic@gmail.com>
2020-06-19 11:52:32 +0200

    double connection fixed in guider drivers

--- indigo_drivers/guider_cgusbst4/indigo_guider_cgusbst4.c
+++ indigo_drivers/guider_cgusbst4/indigo_guider_cgusbst4.c
@@ -45,9 +45,6 @@
 
 #include "indigo_guider_cgusbst4.h"
 
-// gp_bits is used as boolean
-#define is_connected                    gp_bits
-
 #define PRIVATE_DATA        ((cgusbst4_private_data *)device->private_data)
 
 typedef struct {
@@ -153,29 +150,22 @@ static indigo_result guider_attach(indigo_device *device) {
 }
 
 static void guider_connect_callback(indigo_device *device) {
-	CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
 	if (CONNECTION_CONNECTED_ITEM->sw.value) {
-		if (!device->is_connected) {
-			bool result = true;
-			result = cgusbst4_open(device);
-			if (result) {
-				char response[2];
-				result = cgusbst4_command(device, "\006", response, 1, 1) && *response == 'A';
-			}
-			if (result) {
-				device->is_connected = true;
-				CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
-			} else {
-				CONNECTION_PROPERTY->state = INDIGO_ALERT_STATE;
-				indigo_set_switch(CONNECTION_PROPERTY, CONNECTION_DISCONNECTED_ITEM, true);
-			}
+		bool result = true;
+		result = cgusbst4_open(device);
+		if (result) {
+			char response[2];
+			result = cgusbst4_command(device, "\006", response, 1, 1) && *response == 'A';
+		}
+		if (result) {
+			CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
+		} else {
+			CONNECTION_PROPERTY->state = INDIGO_ALERT_STATE;
+			indigo_set_switch(CONNECTION_PROPERTY, CONNECTION_DISCONNECTED_ITEM, true);
 		}
 	} else {
-		if (device->is_connected) {
-			cgusbst4_close(device);
-			device->is_connected = false;
-			CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
-		}
+		cgusbst4_close(device);
+		CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
 	}
 	indigo_guider_change_property(device, NULL, CONNECTION_PROPERTY);
 }
@@ -186,6 +176,8 @@ static indigo_result guider_change_property(indigo_device *device, indigo_client
 	assert(property != NULL);
 	if (indigo_property_match(CONNECTION_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- CONNECTION
+		if (indigo_ignore_connection_change(device, property))
+			return INDIGO_OK;
 		indigo_property_copy_values(CONNECTION_PROPERTY, property, false);
 		CONNECTION_PROPERTY->state = INDIGO_BUSY_STATE;
 		indigo_update_property(device, CONNECTION_PROPERTY, NULL);
