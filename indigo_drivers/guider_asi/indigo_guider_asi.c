efc6caccc0bfe41aec2aba1825f8facb5d63ba41
Rumen G.Bogdanovski <rumen@skyarchive.org>
2020-06-20 10:10:12 +0300

    indigo_guider_asi: use indigo_ignore_connection_change()

--- indigo_drivers/guider_asi/indigo_guider_asi.c
+++ indigo_drivers/guider_asi/indigo_guider_asi.c
@@ -163,7 +163,6 @@ static indigo_result guider_attach(indigo_device *device) {
 }
 
 static void guider_connect_callback(indigo_device *device) {
-	CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
 	if (CONNECTION_CONNECTED_ITEM->sw.value) {
 		if (!device->is_connected) {
 			if (asi_open(device)) {
@@ -197,6 +196,8 @@ static indigo_result guider_change_property(indigo_device *device, indigo_client
 
 	if (indigo_property_match(CONNECTION_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- CONNECTION
+		if (indigo_ignore_connection_change(device, property))
+			return INDIGO_OK;
 		indigo_property_copy_values(CONNECTION_PROPERTY, property, false);
 		CONNECTION_PROPERTY->state = INDIGO_BUSY_STATE;
 		indigo_update_property(device, CONNECTION_PROPERTY, NULL);
