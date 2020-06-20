6d8a4a99a0042dc0e18a58ece2990bfa2d87d61a
Rumen G.Bogdanovski <rumenastro@gmail.com>
2020-06-20 10:02:14 +0300

    indigo_focuser_lunatico/indigo_rotator_lunatico: use indigo_ignore_connection_change()

--- indigo_drivers/focuser_lunatico/shared/lunatico_shared.c
+++ indigo_drivers/focuser_lunatico/shared/lunatico_shared.c
@@ -1209,7 +1209,6 @@ static indigo_result aux_attach(indigo_device *device) {
 }
 
 static void handle_aux_connect_property(indigo_device *device) {
-	CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
 	if (CONNECTION_CONNECTED_ITEM->sw.value) {
 		if (!DEVICE_CONNECTED) {
 			if (lunatico_open(device)) {
@@ -1245,6 +1244,8 @@ static indigo_result aux_change_property(indigo_device *device, indigo_client *c
 	assert(property != NULL);
 	if (indigo_property_match(CONNECTION_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- CONNECTION
+		if (indigo_ignore_connection_change(device, property))
+			return INDIGO_OK;
 		indigo_property_copy_values(CONNECTION_PROPERTY, property, false);
 		CONNECTION_PROPERTY->state = INDIGO_BUSY_STATE;
 		indigo_update_property(device, CONNECTION_PROPERTY, NULL);
@@ -1422,7 +1423,6 @@ static indigo_result rotator_attach(indigo_device *device) {
 
 
 static void handle_rotator_connect_property(indigo_device *device) {
-	CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
 	if (CONNECTION_CONNECTED_ITEM->sw.value) {
 		if (!DEVICE_CONNECTED) {
 			if (lunatico_open(device)) {
@@ -1504,6 +1504,8 @@ static indigo_result rotator_change_property(indigo_device *device, indigo_clien
 	assert(property != NULL);
 	if (indigo_property_match(CONNECTION_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- CONNECTION
+		if (indigo_ignore_connection_change(device, property))
+			return INDIGO_OK;
 		indigo_property_copy_values(CONNECTION_PROPERTY, property, false);
 		CONNECTION_PROPERTY->state = INDIGO_BUSY_STATE;
 		indigo_update_property(device, CONNECTION_PROPERTY, NULL);
@@ -1902,7 +1904,6 @@ static indigo_result focuser_attach(indigo_device *device) {
 
 
 static void handle_focuser_connect_property(indigo_device *device) {
-	CONNECTION_PROPERTY->state = INDIGO_OK_STATE;
 	if (CONNECTION_CONNECTED_ITEM->sw.value) {
 		if (!DEVICE_CONNECTED) {
 			if (lunatico_open(device)) {
@@ -1981,6 +1982,8 @@ static indigo_result focuser_change_property(indigo_device *device, indigo_clien
 	assert(property != NULL);
 	if (indigo_property_match(CONNECTION_PROPERTY, property)) {
 		// -------------------------------------------------------------------------------- CONNECTION
+		if (indigo_ignore_connection_change(device, property))
+			return INDIGO_OK;
 		indigo_property_copy_values(CONNECTION_PROPERTY, property, false);
 		CONNECTION_PROPERTY->state = INDIGO_BUSY_STATE;
 		indigo_update_property(device, CONNECTION_PROPERTY, NULL);
