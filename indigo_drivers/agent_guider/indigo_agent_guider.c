67aea0cc97f229b28a4c727f898147b922e566d0
Rumen G.Bogdanovski <rumen@skyarchive.org>
2020-09-30 21:25:08 +0300

    indigo_agent_guider: change Proportional weight default to 0.75

--- indigo_drivers/agent_guider/indigo_agent_guider.c
+++ indigo_drivers/agent_guider/indigo_agent_guider.c
@@ -1034,8 +1034,8 @@ static indigo_result agent_device_attach(indigo_device *device) {
 		indigo_init_number_item(AGENT_GUIDER_SETTINGS_MAX_PULSE_ITEM, AGENT_GUIDER_SETTINGS_MAX_PULSE_ITEM_NAME, "Max pulse (s)", 0, 5, 0.01, 1);
 		indigo_init_number_item(AGENT_GUIDER_SETTINGS_AGG_RA_ITEM, AGENT_GUIDER_SETTINGS_AGG_RA_ITEM_NAME, "RA aggressivity (%)", 0, 200, 5, 90);
 		indigo_init_number_item(AGENT_GUIDER_SETTINGS_AGG_DEC_ITEM, AGENT_GUIDER_SETTINGS_AGG_DEC_ITEM_NAME, "Dec aggressivity (%)", 0, 200, 5, 90);
-		indigo_init_number_item(AGENT_GUIDER_SETTINGS_PW_RA_ITEM, AGENT_GUIDER_SETTINGS_PW_RA_ITEM_NAME,  "RA Proportional weight", 0, 1, 0, 0.5);
-		indigo_init_number_item(AGENT_GUIDER_SETTINGS_PW_DEC_ITEM, AGENT_GUIDER_SETTINGS_PW_DEC_ITEM_NAME, "Dec Proportional weight", 0, 1, 0, 0.5);
+		indigo_init_number_item(AGENT_GUIDER_SETTINGS_PW_RA_ITEM, AGENT_GUIDER_SETTINGS_PW_RA_ITEM_NAME,  "RA Proportional weight", 0, 1, 0, 0.75);
+		indigo_init_number_item(AGENT_GUIDER_SETTINGS_PW_DEC_ITEM, AGENT_GUIDER_SETTINGS_PW_DEC_ITEM_NAME, "Dec Proportional weight", 0, 1, 0, 0.75);
 		indigo_init_number_item(AGENT_GUIDER_SETTINGS_STACK_ITEM, AGENT_GUIDER_SETTINGS_STACK_ITEM_NAME, "Integral stacking", 1, MAX_STACK, 1, 1);
 		indigo_init_number_item(AGENT_GUIDER_SETTINGS_DITH_X_ITEM, AGENT_GUIDER_SETTINGS_DITH_X_ITEM_NAME, "Dithering offset X (px)", -15, 15, 0, 0);
 		indigo_init_number_item(AGENT_GUIDER_SETTINGS_DITH_Y_ITEM, AGENT_GUIDER_SETTINGS_DITH_Y_ITEM_NAME, "Dithering offset Y (px)", -15, 15, 0, 0);
