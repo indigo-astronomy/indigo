69c1264744a90d7e23621f2d7836803104755f2e
Rumen G.Bogdanovski <rumen@skyarchive.org>
2020-11-20 12:09:16 +0200

    indigo_focuser_mypro2: temperature compensation fixed

--- indigo_drivers/focuser_mypro2/indigo_focuser_mypro2.c
+++ indigo_drivers/focuser_mypro2/indigo_focuser_mypro2.c
@@ -446,7 +446,7 @@ static void compensate_focus(indigo_device *device, double new_temp) {
 		return;
 	}
 
-	PRIVATE_DATA->target_position = PRIVATE_DATA->current_position + compensation;
+	PRIVATE_DATA->target_position = PRIVATE_DATA->current_position - compensation;
 	INDIGO_DRIVER_DEBUG(DRIVER_NAME, "Compensation: PRIVATE_DATA->current_position = %d, PRIVATE_DATA->target_position = %d", PRIVATE_DATA->current_position, PRIVATE_DATA->target_position);
 
 	uint32_t current_position;
