1bd9faee7a64af65b7c4f485aa77c2c22db1c772
Peter Polakovic <peter.polakovic@gmail.com>
2021-01-08 18:30:33 +0100

    metadata generator fixed

--- tools/metainfo.sh
+++ tools/metainfo.sh
@@ -13,13 +13,27 @@ then
 
 name=`pwd`
 name=indigo_`basename $name`
+
+if [ "$name" = "indigo_ccd_qhy" ]
+then
+	description="\"QHY Camera\""
+else
+
+if [ "$name" = "indigo_ccd_qhy2" ]
+then
+	description="\"QHY Camera2\""
+else
+
 description=`grep SET_DRIVER_INFO *.c *.cpp *.m | awk -F ',' '{ print $2 }' | awk '{$1=$1;print}'`
-version=`grep SET_DRIVER_INFO *.c *.cpp *.m | awk -F "," '{ print $4 }' | awk '{$1=$1;print}'`
 
 if [ "`echo $description | cut -c1-1`" != "\"" ]
 then
 	description=`grep "define $description" *.c *.cpp *.m *.h | awk '{$1="";$2=""}1'`
 fi
+fi
+fi
+
+version=`grep SET_DRIVER_INFO *.c *.cpp *.m | awk -F "," '{ print $4 }' | awk '{$1=$1;print}'`
 
 if [ "`echo $version | cut -c1-1`" != "\"" ]
 then
