a421d4bebc6db8317dec9b4f2271750acc866b94
Peter Polakovic <peter.polakovic@gmail.com>
2021-01-10 11:29:31 +0100

    indigo_guider_utils.h renamed to indigo_raw_utils.h

---
+++ indigo_examples/solver_test.c
@@ -0,0 +1,18 @@
+//
+//  solver_test.c
+//  indigo
+//
+//  Created by Peter Polakovic on 10/01/2021.
+//  Copyright © 2021 CloudMakers, s. r. o. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include <indigo/indigo_bus.h>
+
+int main(int argc, char **argv) {
+	indigo_set_log_level(INDIGO_LOG_INFO);
+	indigo_log("solver test started");
+	
+	indigo_log("solver test finished");
+}
