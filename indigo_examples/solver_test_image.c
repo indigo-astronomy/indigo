88f76d008d50e7679beede79b9d38407ca4fc953
Peter Polakovic <peter.polakovic@gmail.com>
2021-01-10 14:03:53 +0100

    indigo_find_stars optimisations

---
+++ indigo_examples/solver_test_image.c
@@ -0,0 +1,13 @@
+//
+//  solver_test_image.c
+//  solver_test
+//
+//  Created by Peter Polakovic on 10/01/2021.
+//  Copyright © 2021 CloudMakers, s. r. o. All rights reserved.
+//
+
+#include <stdio.h>
+
+unsigned short raw_image[] = {
+	#include "../indigo_drivers/ccd_simulator/indigo_ccd_simulator_mono.h"
+};
