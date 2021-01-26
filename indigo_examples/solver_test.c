06bb47364160f3e69576b738917575b70987ea75
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2021-01-26 11:14:14 +0100

    A different distance measuring in solver - doesn't work as well

--- indigo_examples/solver_test.c
+++ indigo_examples/solver_test.c
@@ -21,6 +21,9 @@
 
 #include "indigo_cat_data.h"
 
+#define  DEG2RAD (M_PI / 180.0)
+#define  HOUR2RAD (M_PI / 12.0)
+
 // input image specs
 int raw_image_width = 1600;
 int raw_image_height = 1200;
@@ -34,20 +37,19 @@ int max_image_stars = 500;
 int max_image_quads = 500;
 int top_stars = 100;
 int centroid_radius = 8;
-int precision = 0x00FF;
+int precision = 0x001F;
 
 // to be moved to indigo_raw_utils
 
 typedef struct {
 	int index[4];
-	double scale;
+	double diameter;
 	unsigned short hash[5];
 } indigo_quad;
 
 typedef struct {
 	int hip;
 	double ra, dec;
-	double x, y;
 	double mag;
 } indigo_star;
 
@@ -123,7 +125,7 @@ indigo_result indigo_find_quads(indigo_star_detection *stars, int star_count, in
 		quad->index[1] = index_1;
 		quad->index[2] = index_2;
 		quad->index[3] = index_3;
-		quad->scale = distance[0];
+		quad->diameter = distance[0];
 		// compute hash
 		for (int i = 1; i < 6; i++)
 			quad->hash[i - 1] = (unsigned short)(distance[i] / distance[0] * precision);
@@ -162,20 +164,22 @@ static indigo_quad *match_reference_quads(indigo_quad *reference_quads, int from
 	return reference_quads + middle;
 }
 
-static void radec2xy(double ra, double dec, double *x, double *y) {
-	double ra_r = ra / 12 * M_PI;
-	double r = 90 - 90 * cos(dec / 180 * M_PI);
-	*x = r * sin(ra_r);
-	*y = r * cos(ra_r);
+static double spherical_distance(indigo_star *star_1, indigo_star *star_2) {
+	double dec_1 = star_1->dec * DEG2RAD;
+	double dec_2 = star_2->dec * DEG2RAD;
+	double sin_d1 = sin(dec_1);
+	double cos_d1 = cos(dec_1);
+	double sin_d2 = sin(dec_2);
+	double cos_d2 = cos(dec_2);
+	double cos_delta_a = cos(fabs(star_1->ra - star_2->ra) * HOUR2RAD);
+	return acos(sin_d1 * sin_d2 + cos_d1 * cos_d2 * cos_delta_a) / DEG2RAD;
 }
 
 indigo_result indigo_match_quads(indigo_quad *image_quads, int image_quads_count, double ra, double dec, double radius, int reference_star_count, double max_size) {
-	double x, y;
-	radec2xy(ra, dec, &x, &y);
-	double min_x = x - radius;
-	double max_x = x + radius;
-	double min_y = y - radius;
-	double max_y = y + radius;
+	double min_ra = ra - radius / 15;
+	double max_ra = ra + radius / 15;
+	double min_dec = dec - radius;
+	double max_dec = dec + radius;
 	int item_count = 16 * 1024;
 	int last_item = 0;
 	indigo_star_entry *star_entry = indigo_star_data;
@@ -184,16 +188,13 @@ indigo_result indigo_match_quads(indigo_quad *image_quads, int image_quads_count
 		ra = star_entry->ra;
 		dec = star_entry->dec;
 		indigo_app_star(star_entry->promora, star_entry->promodec, star_entry->px, star_entry->rv, &ra, &dec);
-		radec2xy(ra, dec, &x, &y);
-		if (min_x < x && x < max_x && min_y < y && y < max_y) {
+		if (min_ra < ra && ra < max_ra && min_dec < dec && dec < max_dec) {
 			if (last_item == item_count)
 				reference_stars = indigo_safe_realloc(reference_stars, (item_count *= 2) * sizeof(indigo_star));
 			star = reference_stars + (last_item++);
 			star->hip = star_entry->hip;
 			star->ra = ra;
 			star->dec = dec;
-			star->x = x;
-			star->y = y;
 			star->mag = star_entry->mag;
 			//printf("%.5f\t%.5f\t%.5f\t%.5f\n", ra, dec, x, y);
 		}
@@ -211,36 +212,24 @@ indigo_result indigo_match_quads(indigo_quad *image_quads, int image_quads_count
 	indigo_quad *reference_quads = indigo_safe_malloc(item_count * sizeof(indigo_quad));
 	for (int index_0 = 0; index_0 < reference_star_count; index_0++) {
 		for (int index_1 = index_0 + 1; index_1 < reference_star_count; index_1++) {
-			x = reference_stars[index_0].x - reference_stars[index_1].x;
-			y = reference_stars[index_0].y - reference_stars[index_1].y;
-			double distance_0 = sqrt(x * x + y * y);
+			double distance_0 = spherical_distance(reference_stars + index_0, reference_stars + index_1);
 			if (max_size < distance_0)
 				continue;
 			for (int index_2 = index_1 + 1; index_2 < reference_star_count; index_2++) {
-				x = reference_stars[index_0].x - reference_stars[index_2].x;
-				y = reference_stars[index_0].y - reference_stars[index_2].y;
-				double distance_1 = sqrt(x * x + y * y);
+				double distance_1 = spherical_distance(reference_stars + index_0, reference_stars + index_2);
 				if (max_size < distance_1)
 					continue;
-				x = reference_stars[index_1].x - reference_stars[index_2].x;
-				y = reference_stars[index_1].y - reference_stars[index_2].y;
-				double distance_2 = sqrt(x * x + y * y);
+				double distance_2 = spherical_distance(reference_stars + index_1, reference_stars + index_2);
 				if (max_size < distance_2)
 					continue;
 				for (int index_3 = index_2 + 1; index_3 < reference_star_count; index_3++) {
-					x = reference_stars[index_0].x - reference_stars[index_3].x;
-					y = reference_stars[index_0].y - reference_stars[index_3].y;
-					double distance_3 = sqrt(x * x + y * y);
+					double distance_3 = spherical_distance(reference_stars + index_0, reference_stars + index_3);
 					if (max_size < distance_3)
 						continue;
-					x = reference_stars[index_1].x - reference_stars[index_3].x;
-					y = reference_stars[index_1].y - reference_stars[index_3].y;
-					double distance_4 = sqrt(x * x + y * y);
+					double distance_4 = spherical_distance(reference_stars + index_1, reference_stars + index_3);
 					if (max_size < distance_4)
 						continue;
-					x = reference_stars[index_2].x - reference_stars[index_3].x;
-					y = reference_stars[index_2].y - reference_stars[index_3].y;
-					double distance_5 = sqrt(x * x + y * y);
+					double distance_5 = spherical_distance(reference_stars + index_2, reference_stars + index_3);
 					if (max_size < distance_5)
 						continue;
 					double distance[6] = { distance_0, distance_1, distance_2, distance_3, distance_4, distance_5 };
@@ -252,7 +241,7 @@ indigo_result indigo_match_quads(indigo_quad *image_quads, int image_quads_count
 					quad->index[1] = index_1;
 					quad->index[2] = index_2;
 					quad->index[3] = index_3;
-					quad->scale = distance[0];
+					quad->diameter = distance[0];
 					for (int i = 1; i < 6; i++)
 						quad->hash[i - 1] = (unsigned short)(distance[i] / distance[0] * precision);
 				}
@@ -267,9 +256,9 @@ indigo_result indigo_match_quads(indigo_quad *image_quads, int image_quads_count
 		indigo_quad *image_quad = image_quads + i;
 		indigo_quad *reference_quad = match_reference_quads(reference_quads, 0, reference_quad_count, image_quad);
 		if (reference_quad) {
-			indigo_log("image quad(%03d) = { %03d, %03d, %03d, %03d, %04x%04x%04x%04x%04x }", i, image_quad->index[0], image_quad->index[1], image_quad->index[2], image_quad->index[3], image_quad->hash[0], image_quad->hash[1], image_quad->hash[2], image_quad->hash[3], image_quad->hash[4]);
-			indigo_log("reference quad  =  { %03d, %03d, %03d, %03d, %04x%04x%04x%04x%04x }", reference_quad->index[0], reference_quad->index[1], reference_quad->index[2], reference_quad->index[3], reference_quad->hash[0], reference_quad->hash[1], reference_quad->hash[2], reference_quad->hash[3], reference_quad->hash[4]);
-			indigo_log("scale           = %g -> %.0f%%", reference_quad->scale / image_quad->scale, reference_quad->scale / image_quad->scale / pixel_scale * 100);
+			indigo_log("image quad           = { %d, %d, %d, %d, %04x:%04x:%04x:%04x:%04x }", image_quad->index[0], image_quad->index[1], image_quad->index[2], image_quad->index[3], image_quad->hash[0], image_quad->hash[1], image_quad->hash[2], image_quad->hash[3], image_quad->hash[4]);
+			indigo_log("reference quad       = { %d, %d, %d, %d, %04x:%04x:%04x:%04x:%04x }", reference_quad->index[0], reference_quad->index[1], reference_quad->index[2], reference_quad->index[3], reference_quad->hash[0], reference_quad->hash[1], reference_quad->hash[2], reference_quad->hash[3], reference_quad->hash[4]);
+			indigo_log("scale                = %g -> %.0f%%", reference_quad->diameter / image_quad->diameter, reference_quad->diameter / image_quad->diameter / pixel_scale * 100);
 		}
 	}
 	
@@ -300,7 +289,7 @@ int main(int argc, char **argv) {
 	double max_size = 0;
 	result = indigo_find_quads(image_stars, top_stars, image_quads, &image_quads_found, &max_size);
 	
-	indigo_match_quads(image_quads, image_quads_found, 6.5, 5.0, 3.0, 200, max_size * pixel_scale * 1.1);
+	indigo_match_quads(image_quads, image_quads_found, 6.5, 5.0, 5.0, 200, max_size * pixel_scale * 1.1);
 	
 	indigo_log("solver test finished");
 }
