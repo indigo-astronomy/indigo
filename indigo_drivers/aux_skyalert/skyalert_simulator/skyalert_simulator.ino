705c0d3f849147a6c5fbfeddc289b8d6d9a9e318
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2021-02-04 21:43:44 +0100

    aux_skyalert: driver added

---
+++ indigo_drivers/aux_skyalert/skyalert_simulator/skyalert_simulator.ino
@@ -0,0 +1,46 @@
+// Interactive Astronomy SkyAlert simulator for Arduino
+//
+// Copyright (c) 2021 CloudMakers, s. r. o.
+// All rights reserved.
+//
+// You can use this software under the terms of 'INDIGO Astronomy
+// open-source license' (see LICENSE.md).
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifdef ARDUINO_SAM_DUE
+#define Serial SerialUSB
+#endif
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(500);
+  while (!Serial)
+    ;
+}
+
+void loop() {
+  String command = Serial.readStringUntil('\r');
+  if (command.equals("send")) {
+    Serial.print("Data\r");
+    Serial.print("20.3\r");
+    Serial.print("1\r");
+    Serial.print("1008\r");
+    Serial.print("751\r");
+    Serial.print("66.3\r");
+    Serial.print("415\r");
+    Serial.print("1\r");
+    Serial.print("1.8m\r");
+    Serial.print("101791.83\r");
+  }
+}
