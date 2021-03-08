3ba536c4ddd92cc4b9b26c86a7c15e3883f73581
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2021-03-08 16:15:05 +0100

    mount_ioptron: arduino warnings silenced

--- indigo_drivers/mount_ioptron/ioptron_simulator/ioptron_3.0_simulator/ioptron_3.0_simulator.ino
+++ indigo_drivers/mount_ioptron/ioptron_simulator/ioptron_3.0_simulator/ioptron_3.0_simulator.ino
@@ -47,7 +47,7 @@ void parseCommand() {
       Serial.write("2017100120171001#");
     } else if (strncmp(command, "GLS", 3) == 0) {
       Serial.write(lon);
-      Serial.write(formatLong((atol(lat) / 100 + 90 * 60 * 60) * 100, false, 8));
+      Serial.write(formatLong((atol(lat) / 100L + 90L * 60L * 60L) * 100L, false, 8));
       Serial.write('0');
       if (parked)
         Serial.write('6');
