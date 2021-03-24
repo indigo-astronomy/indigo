f1676970c461c0cca22e7e3bf7d6e34acded5a1e
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2021-03-24 23:16:09 +0100

    agent_alpaca: mount api added, unfinished

--- indigo_drivers/agent_alpaca/alpaca_common.h
+++ indigo_drivers/agent_alpaca/alpaca_common.h
@@ -56,6 +56,10 @@ typedef struct indigo_alpaca_device_struct {
 	char device_uid[40];
 	pthread_mutex_t mutex;
 	bool connected;
+	char utcdate[64];
+	double latitude;
+	double longitude;
+	double elevation;
 	union {
 		struct {
 			uint32_t count;
@@ -76,6 +80,30 @@ typedef struct indigo_alpaca_device_struct {
 			uint32_t position;
 			double temperature;
 		} focuser;
+		struct {
+			bool cansetguiderates;
+			double guideratedeclination;
+			double guideraterightascension;
+			bool canpark;
+			bool canunpark;
+			bool canslew;
+			bool cansync;
+			bool cansettracking;
+			bool athome;
+			bool atpark;
+			double siderealtime;
+			int equatorialsystem;
+			double declination;
+			double rightascension;
+			double altitude;
+			double azimuth;
+			bool targetdeclinationset;
+			bool targetrightascensionset;
+			double targetdeclination;
+			double targetrightascension;
+			bool slewing;
+			bool tracking;
+		} mount;
 		struct {
 			bool canpulseguide;
 			bool cansetguiderates;
@@ -110,6 +138,10 @@ extern long indigo_alpaca_wheel_set_command(indigo_alpaca_device *alpaca_device,
 extern void indigo_alpaca_focuser_update_property(indigo_alpaca_device *alpaca_device, indigo_property *property);
 extern long indigo_alpaca_focuser_get_command(indigo_alpaca_device *alpaca_device, int version, char *command, char *buffer, long buffer_length);
 extern long indigo_alpaca_focuser_set_command(indigo_alpaca_device *alpaca_device, int version, char *command, char *buffer, long buffer_length, char *param_1, char *param_2);
+
+extern void indigo_alpaca_mount_update_property(indigo_alpaca_device *alpaca_device, indigo_property *property);
+extern long indigo_alpaca_mount_get_command(indigo_alpaca_device *alpaca_device, int version, char *command, char *buffer, long buffer_length);
+extern long indigo_alpaca_mount_set_command(indigo_alpaca_device *alpaca_device, int version, char *command, char *buffer, long buffer_length, char *param_1, char *param_2);
 
 extern void indigo_alpaca_guider_update_property(indigo_alpaca_device *alpaca_device, indigo_property *property);
 extern long indigo_alpaca_guider_get_command(indigo_alpaca_device *alpaca_device, int version, char *command, char *buffer, long buffer_length);
