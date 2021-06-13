c329798c1f380011cd5606fa039dcad720456667
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-06-13 03:02:58 +0300

    indigo_server: by default indigo_use_blob_compression on Arm linux is now disabled - too slow to be useful

--- indigo_server/indigo_server.c
+++ indigo_server/indigo_server.c
@@ -1632,6 +1632,10 @@ int main(int argc, const char * argv[]) {
 	server_argv[0] = argv[0];
 	indigo_main_argc = argc;
 	indigo_main_argv = argv;
+#ifdef RPI_MANAGEMENT
+	/* compression takes forever on RPi for large images */
+	indigo_use_blob_compression = false;
+#endif /* RPI_MANAGEMENT */
 	for (int i = 1; i < argc; i++) {
 		if (!strcmp(argv[i], "--") || !strcmp(argv[i], "--do-not-fork")) {
 			do_fork = false;
