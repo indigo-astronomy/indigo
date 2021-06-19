5d647ddb14d4ad9c5acd394edb2f2bce49f709de
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-06-19 16:13:44 +0300

    indigo_dome_beaver: skeleton addded - connects and disconnects

---
+++ indigo_drivers/dome_beaver/indigo_dome_beaver.h
@@ -0,0 +1,47 @@
+// Copyright (c) 2021 Rumen G. Bogdanovski
+// All rights reserved.
+//
+// You can use this software under the terms of 'INDIGO Astronomy
+// open-source license' (see LICENSE.md).
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// version history
+// 2.0 by Rumen G. Bogdanovski <rumen@skyarchive.org>
+
+/** INDIGO Nexdome Beaver Dome driver
+ \file indigo_dome_beaver.h
+ */
+
+#ifndef dome_beaver_h
+#define dome_beaver_h
+
+#include <indigo/indigo_driver.h>
+#include <indigo/indigo_dome_driver.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DOME_BEAVER_NAME              "Nexdome Beaver Dome"
+
+/** Create beaver dome device instance
+ */
+
+extern indigo_result indigo_dome_beaver(indigo_driver_action action, indigo_driver_info *info);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* dome_beaver_h */
