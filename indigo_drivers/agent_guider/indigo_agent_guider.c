2e58b41b8cfb9efa0b9960762e38e86cf253c1dc
Rumen G.Bogdanovski <rumenastro@gmail.com>
2021-10-30 11:52:36 +0300

    indigo_agent_guider: fixed callibration near the poles - should be polished a bit

--- indigo_drivers/agent_guider/indigo_agent_guider.c
+++ indigo_drivers/agent_guider/indigo_agent_guider.c
@@ -23,7 +23,7 @@
  \file indigo_agent_guider.c
  */
 
-#define DRIVER_VERSION 0x0013
+#define DRIVER_VERSION 0x0014
 #define DRIVER_NAME	"indigo_agent_guider"
 
 #include <stdlib.h>
@@ -851,7 +851,8 @@ static void _calibrate_process(indigo_device *device, bool will_guide) {
 					break;
 				}
 				indigo_update_property(device, AGENT_GUIDER_STATS_PROPERTY, "Clearing RA backlash");
-				for (int i = 0; i < AGENT_GUIDER_SETTINGS_BL_STEPS_ITEM->number.value; i++) {
+				/* cos(87deg) = 0.05 => so 20 is ok for 87 declination */
+				for (int i = 0; i < AGENT_GUIDER_SETTINGS_BL_STEPS_ITEM->number.value * 20; i++) {
 					if (!guide_and_capture_frame(device, AGENT_GUIDER_SETTINGS_STEP_ITEM->number.value, 0)) {
 						DEVICE_PRIVATE_DATA->phase = FAILED;
 						break;
@@ -939,43 +940,40 @@ static void _calibrate_process(indigo_device *device, bool will_guide) {
 					break;
 				}
 				indigo_update_property(device, AGENT_GUIDER_STATS_PROPERTY, "Moving west");
-				for (int i = 0; i < AGENT_GUIDER_SETTINGS_CAL_STEPS_ITEM->number.value; i++) {
+				/* we need more iterations closer to the pole */
+				for (int i = 0; i < AGENT_GUIDER_SETTINGS_CAL_STEPS_ITEM->number.value * 5; i++) {
 					if (!guide_and_capture_frame(device, AGENT_GUIDER_SETTINGS_STEP_ITEM->number.value, 0)) {
 						DEVICE_PRIVATE_DATA->phase = FAILED;
 						break;
 					}
 					indigo_update_property(device, AGENT_GUIDER_STATS_PROPERTY, NULL);
-					if (DEVICE_PRIVATE_DATA->drift > AGENT_GUIDER_SETTINGS_CAL_DRIFT_ITEM->number.value) {
-						if (i < AGENT_GUIDER_SETTINGS_CAL_STEPS_ITEM->number.value / 5) {
-							change_step(device, 0.5);
-							break;
-						} else {
-							last_drift = DEVICE_PRIVATE_DATA->drift;
-							double ra_angle = atan2(-AGENT_GUIDER_STATS_DRIFT_Y_ITEM->number.value, AGENT_GUIDER_STATS_DRIFT_X_ITEM->number.value);
-							if (!AGENT_GUIDER_DEC_MODE_NONE_ITEM->sw.value) {
-								double dif_p = PI - fabs(fabs(ra_angle - dec_angle + PI2) - PI);
-								double dif_m = PI - fabs(fabs(ra_angle - dec_angle - PI2) - PI);
-								//indigo_send_message(device, "ra angle = %g, dec angle = %g, dif_p = %g, dif_m = %g", 180.0 * ra_angle / M_PI, 180.0 * dec_angle / M_PI, dif_p, dif_m);
-								if (dif_p < dif_m) {
-									dec_angle -= PI2;
-								} else {
-									dec_angle += PI2;
-									AGENT_GUIDER_SETTINGS_SPEED_DEC_ITEM->number.value = AGENT_GUIDER_SETTINGS_SPEED_DEC_ITEM->number.target = -AGENT_GUIDER_SETTINGS_SPEED_DEC_ITEM->number.target;
-								}
-								AGENT_GUIDER_SETTINGS_ANGLE_ITEM->number.value = AGENT_GUIDER_SETTINGS_ANGLE_ITEM->number.target = round(180 * atan2((sin(dec_angle) + sin(ra_angle)) / 2, (cos(dec_angle) + cos(ra_angle)) / 2) / PI);
+					/* even if we do not reach the do not reach AGENT_GUIDER_SETTINGS_CAL_DRIFT it is ok. We are close enough to te pole and tracking is less sensitive to RA errors */
+					if (DEVICE_PRIVATE_DATA->drift > AGENT_GUIDER_SETTINGS_CAL_DRIFT_ITEM->number.value || i+1 >= AGENT_GUIDER_SETTINGS_CAL_STEPS_ITEM->number.value * 5) {
+						last_drift = DEVICE_PRIVATE_DATA->drift;
+						double ra_angle = atan2(-AGENT_GUIDER_STATS_DRIFT_Y_ITEM->number.value, AGENT_GUIDER_STATS_DRIFT_X_ITEM->number.value);
+						if (!AGENT_GUIDER_DEC_MODE_NONE_ITEM->sw.value) {
+							double dif_p = PI - fabs(fabs(ra_angle - dec_angle + PI2) - PI);
+							double dif_m = PI - fabs(fabs(ra_angle - dec_angle - PI2) - PI);
+							//indigo_send_message(device, "ra angle = %g, dec angle = %g, dif_p = %g, dif_m = %g", 180.0 * ra_angle / M_PI, 180.0 * dec_angle / M_PI, dif_p, dif_m);
+							if (dif_p < dif_m) {
+								dec_angle -= PI2;
 							} else {
-								AGENT_GUIDER_SETTINGS_ANGLE_ITEM->number.value = AGENT_GUIDER_SETTINGS_ANGLE_ITEM->number.target = round(180 * atan2(sin(ra_angle), cos(ra_angle)) / PI);
+								dec_angle += PI2;
+								AGENT_GUIDER_SETTINGS_SPEED_DEC_ITEM->number.value = AGENT_GUIDER_SETTINGS_SPEED_DEC_ITEM->number.target = -AGENT_GUIDER_SETTINGS_SPEED_DEC_ITEM->number.target;
 							}
-							AGENT_GUIDER_SETTINGS_SPEED_RA_ITEM->number.value = AGENT_GUIDER_SETTINGS_SPEED_RA_ITEM->number.target = round(1000 * last_drift / (i * AGENT_GUIDER_SETTINGS_STEP_ITEM->number.value)) / 1000;
-							indigo_update_property(device, AGENT_GUIDER_SETTINGS_PROPERTY, NULL);
-							last_count = i;
-							DEVICE_PRIVATE_DATA->phase = MOVE_EAST;
-							break;
+							AGENT_GUIDER_SETTINGS_ANGLE_ITEM->number.value = AGENT_GUIDER_SETTINGS_ANGLE_ITEM->number.target = round(180 * atan2((sin(dec_angle) + sin(ra_angle)) / 2, (cos(dec_angle) + cos(ra_angle)) / 2) / PI);
+						} else {
+							AGENT_GUIDER_SETTINGS_ANGLE_ITEM->number.value = AGENT_GUIDER_SETTINGS_ANGLE_ITEM->number.target = round(180 * atan2(sin(ra_angle), cos(ra_angle)) / PI);
 						}
+						AGENT_GUIDER_SETTINGS_SPEED_RA_ITEM->number.value = AGENT_GUIDER_SETTINGS_SPEED_RA_ITEM->number.target = round(1000 * last_drift / (i * AGENT_GUIDER_SETTINGS_STEP_ITEM->number.value)) / 1000;
+						if (fabs(AGENT_GUIDER_SETTINGS_SPEED_RA_ITEM->number.value * 20 - AGENT_GUIDER_SETTINGS_SPEED_DEC_ITEM->number.value) < 0) {
+							indigo_send_message(device, "Seems like the telescope is pointing less than 2° from the pole. RA callibration may be off");
+						}
+						indigo_update_property(device, AGENT_GUIDER_SETTINGS_PROPERTY, NULL);
+						last_count = i;
+						DEVICE_PRIVATE_DATA->phase = MOVE_EAST;
+						break;
 					}
-				}
-				if (DEVICE_PRIVATE_DATA->phase == MOVE_WEST) {
-					change_step(device, 2);
 				}
 				break;
 			}
