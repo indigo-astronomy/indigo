062e2a26855f548cb5e8201bee3691f11391a99c
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2021-10-30 20:43:08 +0200

    agent_astrometry: FOV parsing fixed

--- indigo_drivers/agent_astrometry/indigo_agent_astrometry.c
+++ indigo_drivers/agent_astrometry/indigo_agent_astrometry.c
@@ -23,7 +23,7 @@
  \file indigo_agent_astrometry.c
  */
 
-#define DRIVER_VERSION 0x0009
+#define DRIVER_VERSION 0x000A
 #define DRIVER_NAME	"indigo_agent_astrometry"
 
 #include <stdio.h>
@@ -243,9 +243,17 @@ static bool execute_command(indigo_device *device, char *command, ...) {
 			AGENT_PLATESOLVER_WCS_RA_ITEM->number.value = d1 / 15;
 			AGENT_PLATESOLVER_WCS_DEC_ITEM->number.value = d2;
 			INDIGO_PLATESOLVER_DEVICE_PRIVATE_DATA->failed = false;
-		} else if (sscanf(line, "Field size: %lg x %lg", &d1, &d2) == 2) {
+		} else if (sscanf(line, "Field size: %lg x %lg degrees", &d1, &d2) == 2) {
 			AGENT_PLATESOLVER_WCS_WIDTH_ITEM->number.value = d1;
 			AGENT_PLATESOLVER_WCS_HEIGHT_ITEM->number.value = d2;
+			AGENT_PLATESOLVER_WCS_SCALE_ITEM->number.value = (d1 / ASTROMETRY_DEVICE_PRIVATE_DATA->frame_width + d2 / ASTROMETRY_DEVICE_PRIVATE_DATA->frame_height) / 2;
+		} else if (sscanf(line, "Field size: %lg x %lg arcminutes", &d1, &d2) == 2) {
+			AGENT_PLATESOLVER_WCS_WIDTH_ITEM->number.value = d1 / 60.0;
+			AGENT_PLATESOLVER_WCS_HEIGHT_ITEM->number.value = d2 / 60.0;
+			AGENT_PLATESOLVER_WCS_SCALE_ITEM->number.value = (d1 / ASTROMETRY_DEVICE_PRIVATE_DATA->frame_width + d2 / ASTROMETRY_DEVICE_PRIVATE_DATA->frame_height) / 2;
+		} else if (sscanf(line, "Field size: %lg x %lg arcseconds", &d1, &d2) == 2) {
+			AGENT_PLATESOLVER_WCS_WIDTH_ITEM->number.value = d1 / 3600.0;
+			AGENT_PLATESOLVER_WCS_HEIGHT_ITEM->number.value = d2 / 3600.0;
 			AGENT_PLATESOLVER_WCS_SCALE_ITEM->number.value = (d1 / ASTROMETRY_DEVICE_PRIVATE_DATA->frame_width + d2 / ASTROMETRY_DEVICE_PRIVATE_DATA->frame_height) / 2;
 		} else if (sscanf(line, "Field rotation angle: up is %lg", &d1) == 1) {
 			AGENT_PLATESOLVER_WCS_ANGLE_ITEM->number.value = d1;
