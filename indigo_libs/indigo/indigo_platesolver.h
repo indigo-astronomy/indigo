b6fac74caff304d0788c904256b23dc9be648652
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2021-12-19 19:07:38 +0100

    indigo_platesolver: WO BLOB AGENT_PLATESOLVER_IMAGE added

--- indigo_libs/indigo/indigo_platesolver.h
+++ indigo_libs/indigo/indigo_platesolver.h
@@ -79,6 +79,9 @@ extern "C" {
 #define AGENT_PLATESOLVER_ABORT_PROPERTY			(INDIGO_PLATESOLVER_DEVICE_PRIVATE_DATA->abort_property)
 #define AGENT_PLATESOLVER_ABORT_ITEM					(AGENT_PLATESOLVER_ABORT_PROPERTY->items+0)
 
+#define AGENT_PLATESOLVER_IMAGE_PROPERTY			(INDIGO_PLATESOLVER_DEVICE_PRIVATE_DATA->image_property)
+#define AGENT_PLATESOLVER_IMAGE_ITEM					(AGENT_PLATESOLVER_IMAGE_PROPERTY->items+0)
+
 /** Plate solver  structure.
  */
 typedef struct {
@@ -95,6 +98,7 @@ typedef struct {
 	indigo_property *wcs_property;
 	indigo_property *sync_mode_property;
 	indigo_property *abort_property;
+	indigo_property *image_property;
 	indigo_property *polar_alignment_error_property;
 	indigo_property *polar_alignment_settings_property;
 	indigo_property_state eq_coordinates_state;
