b52f608e0d817ce226638f45c093afcafa32acc3
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2022-03-04 12:02:53 +0100

    focuser_nfocus: multiinstance support added

--- indigo_drivers/focuser_nfocus/indigo_focuser_nfocus.c
+++ indigo_drivers/focuser_nfocus/indigo_focuser_nfocus.c
@@ -102,6 +102,7 @@ static indigo_result focuser_attach(indigo_device *device) {
 		// -------------------------------------------------------------------------------- FOCUSER_POSITION
 		FOCUSER_POSITION_PROPERTY->hidden = true;
 		// --------------------------------------------------------------------------------
+		ADDITIONAL_INSTANCES_PROPERTY->hidden = DEVICE_CONTEXT->is_additional_instance;
 		pthread_mutex_init(&PRIVATE_DATA->mutex, NULL);
 		INDIGO_DEVICE_ATTACH_LOG(DRIVER_NAME, device->name);
 		return indigo_focuser_enumerate_properties(device, NULL, NULL);
