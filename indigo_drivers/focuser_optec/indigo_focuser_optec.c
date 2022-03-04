78d73d7204484a67b593889c95d79755b3d446ee
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2022-03-04 12:23:58 +0100

    focuser_optec: multiinstance support added

--- indigo_drivers/focuser_optec/indigo_focuser_optec.c
+++ indigo_drivers/focuser_optec/indigo_focuser_optec.c
@@ -145,6 +145,7 @@ static indigo_result focuser_attach(indigo_device *device) {
 		// -------------------------------------------------------------------------------- FOCUSER_REVERSE_MOTION
 		FOCUSER_REVERSE_MOTION_PROPERTY->hidden = false;
 		// --------------------------------------------------------------------------------
+		ADDITIONAL_INSTANCES_PROPERTY->hidden = DEVICE_CONTEXT->is_additional_instance;
 		pthread_mutex_init(&PRIVATE_DATA->mutex, NULL);
 		INDIGO_DEVICE_ATTACH_LOG(DRIVER_NAME, device->name);
 		return indigo_focuser_enumerate_properties(device, NULL, NULL);
