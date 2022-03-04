c2428185b1ddde61b5d35c48e854baae98a0d23b
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2022-03-04 11:33:21 +0100

    aux_astromechanics: multiinstance support added

--- indigo_drivers/aux_astromechanics/indigo_aux_astromechanics.c
+++ indigo_drivers/aux_astromechanics/indigo_aux_astromechanics.c
@@ -117,6 +117,7 @@ static indigo_result aux_attach(indigo_device *device) {
 		// -------------------------------------------------------------------------------- INFO
 		strcpy(INFO_DEVICE_MODEL_ITEM->text.value, "ASTROMECHANICS Light Pollution Meter");
 		// --------------------------------------------------------------------------------
+		ADDITIONAL_INSTANCES_PROPERTY->hidden = DEVICE_CONTEXT->is_additional_instance;
 		pthread_mutex_init(&PRIVATE_DATA->mutex, NULL);
 		INDIGO_DEVICE_ATTACH_LOG(DRIVER_NAME, device->name);
 		return indigo_aux_enumerate_properties(device, NULL, NULL);
