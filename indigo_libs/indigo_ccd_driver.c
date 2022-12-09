bce834ec19ca7fe56a96ba3d2a7ee84c780d8426
Rumen G.Bogdanovski <rumenastro@gmail.com>
2022-12-10 01:03:38 +0200

    indigo_ccd_driver: indigo_cancel_timer_sync() causes deadlock indigo_cancel_timer() does the job

--- indigo_libs/indigo_ccd_driver.c
+++ indigo_libs/indigo_ccd_driver.c
@@ -510,7 +510,7 @@ indigo_result indigo_ccd_change_property(indigo_device *device, indigo_client *c
 				}
 			}
 			if (CCD_EXPOSURE_ITEM->number.value >= 1) {
-				indigo_cancel_timer_sync(device, &CCD_CONTEXT->countdown_timer);
+				indigo_cancel_timer(device, &CCD_CONTEXT->countdown_timer);
 				indigo_set_timer(device, 1.0, countdown_timer_callback, &CCD_CONTEXT->countdown_timer);
 			}
 		}
