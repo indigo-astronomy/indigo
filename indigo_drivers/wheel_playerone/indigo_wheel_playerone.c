63cfffd2f45eb3f7d8375a29cb2f5846c10cfffa
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2023-01-13 20:30:36 +0100

    wheel_playerone: added to XCode project, removed arm64 exclusion

--- indigo_drivers/wheel_playerone/indigo_wheel_playerone.c
+++ indigo_drivers/wheel_playerone/indigo_wheel_playerone.c
@@ -40,8 +40,6 @@
 #include <indigo/indigo_driver_xml.h>
 #include "indigo_wheel_playerone.h"
 
-#if !(defined(__APPLE__) && defined(__arm64__)) && !(defined(__linux__) && defined(__i386__))
-
 #if defined(INDIGO_FREEBSD)
 #include <libusb.h>
 #else
@@ -455,22 +453,3 @@ indigo_result indigo_wheel_playerone(indigo_driver_action action, indigo_driver_
 
 	return INDIGO_OK;
 }
-
-#else
-
-indigo_result indigo_wheel_playerone(indigo_driver_action action, indigo_driver_info *info) {
-	static indigo_driver_action last_action = INDIGO_DRIVER_SHUTDOWN;
-
-	SET_DRIVER_INFO(info, "Player One Filter Wheel", __FUNCTION__, DRIVER_VERSION, true, last_action);
-
-	switch(action) {
-		case INDIGO_DRIVER_INIT:
-		case INDIGO_DRIVER_SHUTDOWN:
-			return INDIGO_UNSUPPORTED_ARCH;
-		case INDIGO_DRIVER_INFO:
-			break;
-	}
-	return INDIGO_OK;
-}
-
-#endif
