b954ec28cbf8de624d6c2b5e9c145c7ce5b9635d
Rumen G.Bogdanovski <rumenastro@gmail.com>
2023-03-25 20:57:51 +0200

    indigo_log_analizer.pl: pretty complete

--- indigo_tools/indigo_log_analizer.pl
+++ indigo_tools/indigo_log_analizer.pl
@@ -41,63 +41,68 @@ sub print_help() {
 	print "INDIGO Log Analyzer version $VERSION\n".
 	      "usage: $N trace [options] [device1][.property1] [device2][.property2] ... < logfile\n".
 		  "       $N match [options] pattern1 [pattern2] ... < logfile\n".
-		  "       $N requests [options] [client1] [client2] ... < logfile\n".
+		  "       $N list_requests [options] [client1] [client2] ... < logfile\n".
+		  "       $N list_clients [options] < logfile\n".
+		  "       $N list_devices [options] < logfile\n".
 	      "options:\n".
 	      "       -v verbose output\n".
+		  "       -w black and white output\n".
 	      "       -h print help\n";
 }
 
-sub split_line ($){
+sub parse_record ($){
 	my ($line) = @_;
 	my %fields;
 
 	my @split1 = split (/\s+/, $line, 6);
-	if (($split1[2] eq "B" || $split1[2] eq "0") && $split1[4] =~ /Define|Remove|Update|Change/ && !($split1[5] =~ /^-/)) {
-		$fields{time} = $split1[0];
-		$fields{app} = $split1[1];
-		$fields{socket} = $split1[2];
-		$fields{direction} = $split1[3];
-		$fields{action} = $split1[4];
-		my @split2 = split (/\'/, $split1[5]);
-		$fields{device} = $split2[1];
-		$fields{property} = $split2[3];
-		my @split3 = split (/\s+/, $split2[4]);
-		$fields{type} = $split3[1];
-		$fields{permission} = $split3[2];
-		$fields{state} = $split3[3];
+	if (($split1[2] eq "B" || $split1[2] eq "0") && !($split1[5] =~ /^-/)) {
+		if ($split1[4] =~ /Define|Remove|Update|Change|Enable|Enumerate/) {
+			$fields{time} = $split1[0];
+			$fields{app} = $split1[1];
+			$fields{socket} = $split1[2];
+			$fields{direction} = $split1[3];
+			$fields{action} = $split1[4];
+			my @split2 = split (/\'/, $split1[5]);
+			$fields{device} = $split2[1];
+			$fields{property} = $split2[3];
+			my @split3 = split (/\s+/, $split2[4]);
+			$fields{type} = $split3[1];
+			$fields{permission} = $split3[2];
+			$fields{state} = $split3[3];
 
-		$line =~ /.*\'(.*?)\'\s+\{.*/;
-		if ($1 ne "") {
-			$fields{client} = $1;
-		}
+			$line =~ /.*\'(.*?)\'\s+\{.*/;
+			if ($1 ne "") {
+				$fields{client} = $1;
+			}
 
-		if ($fields{direction} =~ /\+/) {
-			while (my $l = <STDIN>) {
-				if ($l =~ /\}/) {
-					last;
-				}
-				if ($fields{type} eq "BLOB") {
-					$l =~ /\'(.*?)\'\s+(.*)/;
-					my $key = $1;
-					my $value = $2;
-					$fields{items}{$1} = $2;
-				} else {
-					my @kv = split(/\=/, $l);
-					$kv[0] =~ /.*\'(.*?)\'.*/;
-					my $key = $1;
-					my $value;
-					if ($kv[1] =~ /\'/) {
-						$kv[1] =~ /.*\'(.*?)\'.*/;
-						$value = $1;
-					} else {
-						$kv[1] =~ /^\s+(.*?)\s.*/;
-						$value = $1;
+			if ($fields{direction} =~ /\+/) {
+				while (my $l = <STDIN>) {
+					if ($l =~ /\}/) {
+						last;
 					}
-					$fields{items}{$key} = $value;
+					if ($fields{type} eq "BLOB") {
+						$l =~ /\'(.*?)\'\s+(.*)/;
+						my $key = $1;
+						my $value = $2;
+						$fields{items}{$1} = $2;
+					} else {
+						my @kv = split(/\=/, $l);
+						$kv[0] =~ /.*\'(.*?)\'.*/;
+						my $key = $1;
+						my $value;
+						if ($kv[1] =~ /\'/) {
+							$kv[1] =~ /.*\'(.*?)\'.*/;
+							$value = $1;
+						} else {
+							$kv[1] =~ /^\s+(.*?)\s.*/;
+							$value = $1;
+						}
+						$fields{items}{$key} = $value;
+					}
 				}
 			}
+			return %fields;
 		}
-		return %fields;
 	} else {
 		return undef;
 	}
@@ -112,6 +117,10 @@ sub print_line (%$) {
 		$act = "==";
 	} elsif ($fields{action} eq "Remove") {
 		$act = "--";
+	} elsif ($fields{action} eq "Enable") {
+		$act = "**";
+	} elsif ($fields{action} eq "Enumerate") {
+		$act = "##";
 	}
 
 	if ($verbose) {
@@ -124,7 +133,19 @@ sub print_line (%$) {
 
 		my $message2 = "    '$fields{device}'.'$fields{property}'\n";
 
-		if ($fields{state} eq "Ok") {
+		if ($fields{action} eq "Enumerate") {
+			print MAGENTA $message;
+			print MAGENTA $message2;
+			foreach my $key (keys %{$fields{items}}) {
+				print MAGENTA "    $key = $fields{items}{$key}\n";
+			}
+		} elsif ($fields{action} eq "Enable") {
+			print CYAN $message;
+			print CYAN $message2;
+			foreach my $key (keys %{$fields{items}}) {
+				print CYAN "    $key = $fields{items}{$key}\n";
+			}
+		} elsif ($fields{state} eq "Ok") {
 			print GREEN $message;
 			print GREEN $message2;
 			foreach my $key (keys %{$fields{items}}) {
@@ -157,7 +178,12 @@ sub print_line (%$) {
 		} else {
 			$message .= "\n";
 		}
-		if ($fields{state} eq "Ok") {
+
+		if ($fields{action} eq "Enumerate") {
+			print MAGENTA $message;
+		} elsif ($fields{action} eq "Enable") {
+			print CYAN $message;
+		} elsif ($fields{state} eq "Ok") {
 			print GREEN $message;
 		} elsif ($fields{state} eq "Busy"){
 			print YELLOW $message;
@@ -169,43 +195,46 @@ sub print_line (%$) {
 	}
 }
 
-sub trace {
+sub trace_cmd {
 	my @params = @_;
-
-	if ($#params < 0) {
-		print RED "trace: Wrong parameters.\n";
-		return undef;
-	}
 
 	while (my $line = <STDIN>) {
 		chomp($line);
-		my %fields = split_line($line);
+
+		my %fields = parse_record($line);
+		if ($fields{time} eq "") {
+			next;
+		}
+
+		if ($#params < 0) {
+			print_line(%fields);
+			next;
+		}
+
 		foreach my $property (@params) {
 			my @pattern = split(/\./, $property);
 			if ($#pattern == 1) {
-				if (%fields{device} eq $pattern[0] && %fields{property} eq $pattern[1]) {
+				if ((%fields{device} eq $pattern[0] && %fields{property} eq $pattern[1]) || (%fields{device} eq "")) {
 					print_line(%fields);
 					last;
 				}
 			} elsif ($#pattern == 0) {
-				if (%fields{device} eq $pattern[0]) {
+				if ((%fields{device} eq $pattern[0]) || (%fields{device} eq "")) {
 					print_line(%fields);
 					last;
 				}
-			} elsif ($#pattern < 0) {
-				print_line(%fields);
 			}
 		}
 	}
 	return 1;
 }
 
-sub requests {
+sub list_requests_cmd {
 	my @clients = @_;
 
 	while (my $line = <STDIN>) {
 		chomp($line);
-		my %fields = split_line($line);
+		my %fields = parse_record($line);
 		if ($#clients < 0 && $fields{action} eq "Change") {
 			print_line(%fields);
 		}
@@ -219,7 +248,67 @@ sub requests {
 	return 1;
 }
 
-sub match {
+sub list_devices_cmd {
+	my @params = @_;
+
+	if ($#params >= 0) {
+		print RED "list_devices: Wrong parameters.\n";
+		return undef;
+	}
+
+	while (my $line = <STDIN>) {
+		chomp($line);
+
+		my @split1 = split (/\s+/, $line, 6);
+		my $time = $split1[0];
+
+		$line =~ /.*\s+Attach\s+device\s+\'(.*?)\'$/;
+		if ($1 ne "") {
+			print GREEN "$time ++ '$1' attached\n";
+			next;
+		}
+
+		$line =~ /.*\s+Detach\s+device\s+\'(.*?)\'$/;
+		if ($1 ne "") {
+			print RED "$time -- '$1' detached\n";
+			next;
+		}
+	}
+	return 1;
+}
+
+sub list_clients_cmd {
+	my @params = @_;
+
+	if ($#params >= 0) {
+		print RED "list_clients: Wrong parameters.\n";
+		return undef;
+	}
+
+	while (my $line = <STDIN>) {
+		chomp($line);
+
+		my @split1 = split (/\s+/, $line, 6);
+		my $time = $split1[0];
+
+		$line =~ /.*\s+Attach\s+client\s+\'(.*?)\'$/;
+		if ($1 ne "") {
+			print GREEN "$time ++ '$1' attached\n";
+			next;
+		}
+
+		$line =~ /.*\s+Detach\s+client\s+\'(.*?)\'$/;
+		if ($1 ne "") {
+			print RED "$time -- '$1' detached\n";
+			next;
+		}
+	}
+	return 1;
+}
+
+
+
+sub match_cmd {
 	my @paterns = @_;
 
 	if ($#paterns < 0) {
@@ -246,8 +335,12 @@ sub main() {
 	my %options = ();
 	my $command = shift @ARGV;
 
-	if (getopts("vh", \%options) == undef) {
+	if (getopts("vhw", \%options) == undef) {
 		exit 1;
+	}
+
+	if (defined($options{w})) {
+		$ENV{ANSI_COLORS_DISABLED} = 1;
 	}
 
 	if (defined($options{h}) or !defined($command) or $command eq "-h") {
@@ -260,27 +353,43 @@ sub main() {
 	}
 
 	if ($command eq "match") {
-		if (!match(@ARGV)) {
-			$verbose && print RED "trace returned error.\n";
+		if (!match_cmd(@ARGV)) {
+			$verbose && print RED "match returned error\n";
 			exit 1;
 		}
-		$verbose && print WHITE "match cmplete.\n";
+		$verbose && print WHITE "match cmplete\n";
 		exit 0;
 
-	} elsif ($command eq "requests") {
-		if (!requests(@ARGV)) {
-			$verbose && print RED "requests returned error.\n";
+	} elsif ($command eq "list_requests") {
+		if (!list_requests_cmd(@ARGV)) {
+			$verbose && print RED "list_requests returned error\n";
 			exit 1;
 		}
-		$verbose && print WHITE "requests cmplete.\n";
+		$verbose && print WHITE "list_requests cmplete\n";
+		exit 0;
+
+	} elsif ($command eq "list_devices") {
+		if (!list_devices_cmd(@ARGV)) {
+			$verbose && print RED "list_devices returned error\n";
+			exit 1;
+		}
+		$verbose && print WHITE "list_devices cmplete\n";
+		exit 0;
+
+	} elsif ($command eq "list_clients") {
+		if (!list_clients_cmd(@ARGV)) {
+			$verbose && print RED "list_clients returned error\n";
+			exit 1;
+		}
+		$verbose && print WHITE "list_clients cmplete\n";
 		exit 0;
 
 	} elsif ($command eq "trace") {
-		if (!trace(@ARGV)) {
-			$verbose && print RED "trace returned error.\n";
+		if (!trace_cmd(@ARGV)) {
+			$verbose && print RED "trace returned error\n";
 			exit 1;
 		}
-		$verbose && print WHITE "trace cmplete.\n";
+		$verbose && print WHITE "trace cmplete\n";
 		exit 0;
 
 	} else {
