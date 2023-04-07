35fad4ba01a11599571c5175e657bf75efc3ae73
Rumen G.Bogdanovski <rumenastro@gmail.com>
2023-04-08 02:02:43 +0300

    indigo_agent_astrometry: add hint to each catalog index item

--- indigo_drivers/agent_astrometry/indigo_agent_astrometry.c
+++ indigo_drivers/agent_astrometry/indigo_agent_astrometry.c
@@ -694,7 +694,7 @@ static indigo_result agent_device_attach(indigo_device *device) {
 		char name[INDIGO_NAME_SIZE], label[INDIGO_VALUE_SIZE], path[INDIGO_VALUE_SIZE];
 		bool present;
 		AGENT_ASTROMETRY_INDEX_41XX_PROPERTY = indigo_init_switch_property(NULL, device->name, AGENT_ASTROMETRY_INDEX_41XX_PROPERTY_NAME, "Index managememt", "Installed Tycho-2 catalog indexes", INDIGO_OK_STATE, INDIGO_RW_PERM, INDIGO_ANY_OF_MANY_RULE, 13);
-		strcpy(AGENT_ASTROMETRY_INDEX_41XX_PROPERTY->hints,"warn_on_clear:\"Delete Tycho-2 catalog index file?\";");
+		strcpy(AGENT_ASTROMETRY_INDEX_41XX_PROPERTY->hints,"warn_on_clear:\"Delete Tycho-2 index file?\";");
 		if (AGENT_ASTROMETRY_INDEX_41XX_PROPERTY == NULL)
 			return INDIGO_FAILED;
 		for (int i = 19; i >=7; i--) {
@@ -715,6 +715,7 @@ static indigo_result agent_device_attach(indigo_device *device) {
 				}
 			}
 			indigo_init_switch_item(AGENT_ASTROMETRY_INDEX_41XX_PROPERTY->items - (i - 19), name, label, present);
+			sprintf((AGENT_ASTROMETRY_INDEX_41XX_PROPERTY->items - (i - 19))->hints, "warn_on_clear:\"Delete Tycho-2 index 41%02d?\";", i);
 			if (present) {
 				char long_label[INDIGO_VALUE_SIZE];
 				snprintf(long_label, INDIGO_VALUE_SIZE, "Tycho-2 %s", label);
@@ -722,7 +723,7 @@ static indigo_result agent_device_attach(indigo_device *device) {
 			}
 		}
 		AGENT_ASTROMETRY_INDEX_42XX_PROPERTY = indigo_init_switch_property(NULL, device->name, AGENT_ASTROMETRY_INDEX_42XX_PROPERTY_NAME, "Index managememt", "Installed 2MASS catalog indexes", INDIGO_OK_STATE, INDIGO_RW_PERM, INDIGO_ANY_OF_MANY_RULE, 20);
-		strcpy(AGENT_ASTROMETRY_INDEX_42XX_PROPERTY->hints,"warn_on_clear:\"Delete 2MASS catalog index file?\";");
+		strcpy(AGENT_ASTROMETRY_INDEX_42XX_PROPERTY->hints, "warn_on_clear:\"Delete 2MASS index file?\";");
 		if (AGENT_ASTROMETRY_INDEX_42XX_PROPERTY == NULL)
 			return INDIGO_FAILED;
 		for (int i = 19; i >=0; i--) {
@@ -743,6 +744,7 @@ static indigo_result agent_device_attach(indigo_device *device) {
 				}
 			}
 			indigo_init_switch_item(AGENT_ASTROMETRY_INDEX_42XX_PROPERTY->items - (i - 19), name, label, present);
+			sprintf((AGENT_ASTROMETRY_INDEX_42XX_PROPERTY->items - (i - 19))->hints, "warn_on_clear:\"Delete 2MASS index 42%02d?\";", i);
 			if (present) {
 				char long_label[INDIGO_VALUE_SIZE];
 				snprintf(long_label, INDIGO_VALUE_SIZE, "2MASS %s", label);
