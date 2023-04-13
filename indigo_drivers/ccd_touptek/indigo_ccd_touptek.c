46f766c4c53506494ee7db1960e8181dfdea09db
Rumen G.Bogdanovski <rumenastro@gmail.com>
2023-04-13 11:46:59 +0300

    indigo_ccd_ssg: disable apple silicon binary - no sdk

--- indigo_drivers/ccd_touptek/indigo_ccd_touptek.c
+++ indigo_drivers/ccd_touptek/indigo_ccd_touptek.c
@@ -153,6 +153,8 @@
 
 #endif
 
+#if !(defined(__APPLE__) && defined(__arm64__) && defined(STARSHOOTG))
+
 #define PRIVATE_DATA        							((DRIVER_PRIVATE_DATA *)device->private_data)
 
 #define X_CCD_ADVANCED_PROPERTY						(PRIVATE_DATA->advanced_property)
@@ -1647,3 +1649,21 @@ indigo_result ENTRY_POINT(indigo_driver_action action, indigo_driver_info *info)
 
 	return INDIGO_OK;
 }
+
+#else
+
+indigo_result ENTRY_POINT(indigo_driver_action action, indigo_driver_info *info) {
+	static indigo_driver_action last_action = INDIGO_DRIVER_SHUTDOWN;
+
+	SET_DRIVER_INFO(info, DRIVER_LABEL, __FUNCTION__, DRIVER_VERSION, true, last_action);
+
+	switch(action) {
+		case INDIGO_DRIVER_INIT:
+		case INDIGO_DRIVER_SHUTDOWN:
+			return INDIGO_UNSUPPORTED_ARCH;
+		case INDIGO_DRIVER_INFO:
+			break;
+	}
+	return INDIGO_OK;
+}
+#endif
