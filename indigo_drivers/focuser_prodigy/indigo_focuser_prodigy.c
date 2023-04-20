2ca4e7f3f719040576fc03ae5a64861d3d5d733d
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2023-04-20 21:25:32 +0200

    focuser_prodigy: typo fixed

--- indigo_drivers/focuser_prodigy/indigo_focuser_prodigy.c
+++ indigo_drivers/focuser_prodigy/indigo_focuser_prodigy.c
@@ -97,6 +97,8 @@ static bool prodigy_command(indigo_device *device, char *command, char *response
 
 // -------------------------------------------------------------------------------- INDIGO focuser device implementation
 
+static indigo_result focuser_enumerate_properties(indigo_device *device, indigo_client *client, indigo_property *property);
+
 static indigo_result focuser_attach(indigo_device *device) {
 	assert(device != NULL);
 	assert(PRIVATE_DATA != NULL);
@@ -107,7 +109,7 @@ static indigo_result focuser_attach(indigo_device *device) {
 		FOCUSER_BACKLASH_ITEM->number.max = 9999;
 		FOCUSER_BACKLASH_ITEM->number.target = FOCUSER_BACKLASH_ITEM->number.value = 100;
 		// -------------------------------------------------------------------------------- X_FOCUSER_PARK
-		X_FOCUSER_PARK_PROPERTY= indigo_init_switch_property(NULL, device->name, "X_FOCUSER_PARK", FOCUSER_MAIN_GROUP, "Park", INDIGO_OK_STATE, INDIGO_RW_PERM, INDIGO_ONE_OF_MANY_RULE, 1);
+		X_FOCUSER_PARK_PROPERTY = indigo_init_switch_property(NULL, device->name, "X_FOCUSER_PARK", FOCUSER_MAIN_GROUP, "Park", INDIGO_OK_STATE, INDIGO_RW_PERM, INDIGO_ONE_OF_MANY_RULE, 1);
 		if (X_FOCUSER_PARK_PROPERTY == NULL)
 			return INDIGO_FAILED;
 		indigo_init_switch_item(X_FOCUSER_PARK_ITEM, "PARK", "Park", false);
@@ -153,7 +155,7 @@ static indigo_result focuser_attach(indigo_device *device) {
 		ADDITIONAL_INSTANCES_PROPERTY->hidden = DEVICE_CONTEXT->base_device != NULL;
 		pthread_mutex_init(&PRIVATE_DATA->mutex, NULL);
 		INDIGO_DEVICE_ATTACH_LOG(DRIVER_NAME, device->name);
-		return indigo_focuser_enumerate_properties(device, NULL, NULL);
+		return focuser_enumerate_properties(device, NULL, NULL);
 	}
 	return INDIGO_FAILED;
 }
