b70ed89f3addf44a8bc006a34fc020313f1648fa
Rumen G.Bogdanovski <rumenastro@gmail.com>
2023-10-09 10:49:06 +0300

    prepare 2.0-248 release

--- indigo_libs/indigo/indigo_config.h
+++ indigo_libs/indigo/indigo_config.h
@@ -28,7 +28,7 @@
 
 /** INDIGO Build number
  */
-#define INDIGO_BUILD "247"
+#define INDIGO_BUILD "248"
 
 /** INDIGO Build commit
  */
--- indigo_windows/gcc/build_windows_sdk.sh
+++ indigo_windows/gcc/build_windows_sdk.sh
@@ -2,7 +2,7 @@
 
 export SAVED_PATH=$PATH 
 
-export VERSION=2.0-247
+export VERSION=2.0-248
 
 export PATH=/c/Qt/Tools/mingw810_32/bin/:$SAVED_PATH
 export ARCH=x86
