956d1841f3235967287ed0b4418574aeb5bd5801
Rumen G.Bogdanovski <rumenastro@gmail.com>
2023-11-20 23:12:05 +0200

    indigo_agent_alpaca: better way to skip agents

--- indigo_drivers/agent_alpaca/indigo_agent_alpaca.c
+++ indigo_drivers/agent_alpaca/indigo_agent_alpaca.c
@@ -517,7 +517,7 @@ static indigo_result agent_define_property(indigo_client *client, indigo_device 
 			if (!strcmp(item->name, INFO_DEVICE_INTERFACE_ITEM_NAME)) {
 				alpaca_device->indigo_interface = atoll(item->text.value);
 				if (IS_DEVICE_TYPE(alpaca_device, INDIGO_INTERFACE_AGENT)) {
-					continue;
+					alpaca_device->device_type = NULL;
 				} else if (IS_DEVICE_TYPE(alpaca_device, INDIGO_INTERFACE_CCD)) {
 					alpaca_device->ccd.ccdtemperature = NAN;
 					alpaca_device->device_type = "Camera";
