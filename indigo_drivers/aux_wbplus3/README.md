6234c4ed66c939dcf0503a0d7a2ffe6ac2823c38
Rumen G.Bogdanovski <rumenastro@gmail.com>
2024-01-12 03:09:21 +0200

    indigo_aux_wbplus3: driver work in progress

---
+++ indigo_drivers/aux_wbplus3/README.md
@@ -0,0 +1,24 @@
+# WandererBox Plus V3 powerbox driver
+
+https://www.wandererastro.com
+
+## Supported devices
+* WandererBox Plus V3 Powerbox
+
+Single device is present on the first startup (no hot-plug support). Additional devices can be configured on runtime.
+
+## Supported platforms
+
+This driver is platform independent.
+
+## License
+
+INDIGO Astronomy open-source license.
+
+## Use
+
+indigo_server indigo_aux_wbplus3
+
+## Status: Under Development
+
+Tested with physical device.
---
+++ indigo_drivers/aux_wbplus3/indigo_aux_wbplus3.h
@@ -0,0 +1,47 @@
+// Copyright (C) 2024 Rumen G. Bogdanovski
+// All rights reserved.
+//
+// You can use this software under the terms of 'INDIGO Astronomy
+// open-source license' (see LICENSE.md).
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// version history
+// 2.0 by Rumen G. Bogdanovski <rumenastro@gmail.com>
+
+/** INDIGO WandererBox Plus V3 aux driver
+ \file indigo_aux_wbplus3.h
+ */
+
+#ifndef aux_wbplus3_h
+#define aux_wbplus3_h
+
+#include <indigo/indigo_bus.h>
+#include <indigo/indigo_driver.h>
+#include <indigo/indigo_aux_driver.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Register WBPlus3 driver
+ */
+
+extern indigo_result indigo_aux_wbplus3(indigo_driver_action action, indigo_driver_info *info);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* aux_wbplus3_h */
+
