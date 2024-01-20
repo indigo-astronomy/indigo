3f4e23a531964ba28d57e447f3e9bcaf50cd55bf
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2024-01-20 08:37:45 +0100

    mount_synscan: sanity check removed again, 2aabf06 reverted

--- indigo_drivers/mount_synscan/indigo_mount_synscan_private.h
+++ indigo_drivers/mount_synscan/indigo_mount_synscan_private.h
@@ -15,7 +15,7 @@
 //#include "indigo_timer.h"
 #include "indigo_mount_synscan_driver.h"
 
-#define DRIVER_VERSION			0x0012
+#define DRIVER_VERSION			0x0011
 #define DRIVER_NAME					"indigo_mount_synscan"
 
 #define PRIVATE_DATA        ((synscan_private_data *)device->private_data)
