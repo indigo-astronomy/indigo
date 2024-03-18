113f515a1d5e3ea02516364def13cba229611048
Rumen G.Bogdanovski <rumenastro@gmail.com>
2024-03-18 13:39:59 +0200

    indigo_agent_mount: add parallactic angle and derotation rate to AGENT_MOUNT_DISPLAY_COORDINATES

--- indigo_libs/indigo_rotator_driver.c
+++ indigo_libs/indigo_rotator_driver.c
@@ -37,7 +37,7 @@
 #include <indigo/indigo_rotator_driver.h>
 
 double indigo_range360(double angle) {
-	return fmod(angle + (360000), 360);
+	return fmod(angle + (3600000), 360);
 }
 
 void indigo_rotator_load_calibration(indigo_device *device) {
