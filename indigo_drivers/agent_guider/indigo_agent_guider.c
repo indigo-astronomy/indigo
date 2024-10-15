ec05d7bc5354f8fa3cf5d0ac5dd127004b6706dd
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2024-10-15 22:05:06 +0200

    agent_guider: temporary fix on restore_subframe

--- indigo_drivers/agent_guider/indigo_agent_guider.c
+++ indigo_drivers/agent_guider/indigo_agent_guider.c
@@ -827,7 +827,10 @@ static void restore_subframe(indigo_device *device) {
 		indigo_usleep(0.5 * ONE_SECOND_DELAY);
 		/* TRICKY: capture_raw_frame() should be here in order to have the correct frame and correct selection
 			 so that HFD, FWHM etc are evaluated correctly, but selection property should not be updated. */
+		indigo_property_state state = AGENT_ABORT_PROCESS_PROPERTY->state;
+		AGENT_ABORT_PROCESS_PROPERTY->state = INDIGO_OK_STATE;
 		capture_raw_frame(device);
+		AGENT_ABORT_PROCESS_PROPERTY->state = state;
 		indigo_update_property(device, AGENT_GUIDER_SELECTION_PROPERTY, NULL);
 		DEVICE_PRIVATE_DATA->saved_frame_left = 0;
 		DEVICE_PRIVATE_DATA->saved_frame_top = 0;
