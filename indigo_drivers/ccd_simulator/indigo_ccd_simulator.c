9f01dd46b19278c89d8911b59fc22fd1c5186476
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2024-10-31 22:56:49 +0100

    ccd_simulator: pixel size added to bahtinov simulator

--- indigo_drivers/ccd_simulator/indigo_ccd_simulator.c
+++ indigo_drivers/ccd_simulator/indigo_ccd_simulator.c
@@ -817,6 +817,7 @@ static indigo_result ccd_attach(indigo_device *device) {
 			CCD_INFO_HEIGHT_ITEM->number.value = CCD_FRAME_HEIGHT_ITEM->number.max = CCD_FRAME_TOP_ITEM->number.max = CCD_FRAME_HEIGHT_ITEM->number.value = BAHTINOV_HEIGHT;
 			CCD_INFO_MAX_HORIZONAL_BIN_ITEM->number.value = CCD_BIN_HORIZONTAL_ITEM->number.max = 1;
 			CCD_INFO_MAX_VERTICAL_BIN_ITEM->number.value = CCD_BIN_VERTICAL_ITEM->number.max = 1;
+			CCD_INFO_PIXEL_SIZE_ITEM->number.value = CCD_INFO_PIXEL_WIDTH_ITEM->number.value = CCD_INFO_PIXEL_HEIGHT_ITEM->number.value = 3.75;
 			CCD_IMAGE_FORMAT_PROPERTY->count = 7;
 			CCD_OFFSET_PROPERTY->hidden = true;
 			CCD_GAMMA_PROPERTY->hidden = true;
