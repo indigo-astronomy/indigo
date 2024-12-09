fb42f3dfa52314f2793676a951d105562df04349
Rumen Bogdanovski <rumenastro@gmail.com>
2024-12-09 16:47:09 +0200

    add asi arm macos SDK

--- indigo_drivers/wheel_asi/indigo_wheel_asi.c
+++ indigo_drivers/wheel_asi/indigo_wheel_asi.c
@@ -38,8 +38,6 @@
 #include <indigo/indigo_driver_xml.h>
 #include "indigo_wheel_asi.h"
 
-#if !(defined(__APPLE__) && defined(__arm64__))
-
 #if defined(INDIGO_FREEBSD)
 #include <libusb.h>
 #else
@@ -613,21 +611,3 @@ indigo_result indigo_wheel_asi(indigo_driver_action action, indigo_driver_info *
 	return INDIGO_OK;
 }
 
-#else
-
-indigo_result indigo_wheel_asi(indigo_driver_action action, indigo_driver_info *info) {
-	static indigo_driver_action last_action = INDIGO_DRIVER_SHUTDOWN;
-
-	SET_DRIVER_INFO(info, "ZWO ASI Filter Wheel", __FUNCTION__, DRIVER_VERSION, true, last_action);
-
-	switch(action) {
-		case INDIGO_DRIVER_INIT:
-		case INDIGO_DRIVER_SHUTDOWN:
-			return INDIGO_UNSUPPORTED_ARCH;
-		case INDIGO_DRIVER_INFO:
-			break;
-	}
-	return INDIGO_OK;
-}
-
-#endif
