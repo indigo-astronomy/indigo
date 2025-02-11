89d1b1113ca7efd46e12491e83eb72040c9ae82d
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2025-02-11 15:44:47 +0100

    cleanup

--- indigo_drivers/aux_arteskyflat/indigo_aux_arteskyflat.c
+++ indigo_drivers/aux_arteskyflat/indigo_aux_arteskyflat.c
@@ -53,13 +53,13 @@ typedef struct {
 } arteskyflat_private_data;
 
 static bool artesky_command(indigo_device *device, char *command, char *response) {
-	indigo_uni_discard(PRIVATE_DATA->handle, 1000);
+	if (indigo_uni_discard(PRIVATE_DATA->handle, 1000) < 0) {
+		return false;
+	}
 	if (indigo_uni_printf(PRIVATE_DATA->handle, "%s\n", command) < 0) {
-		INDIGO_DRIVER_DEBUG(DRIVER_NAME, "%d <- // Failed to read (%s)", PRIVATE_DATA->handle->index, indigo_uni_strerror(PRIVATE_DATA->handle));
 		return false;
 	}
 	if (indigo_uni_read_section(PRIVATE_DATA->handle, response, 10, "\n", "\r\n", 1000000) < 0) {
-		INDIGO_DRIVER_DEBUG(DRIVER_NAME, "%d -> // Failed to write (%s)", PRIVATE_DATA->handle->index, response, indigo_uni_strerror(PRIVATE_DATA->handle));
 		return false;
 	}
 	return true;
