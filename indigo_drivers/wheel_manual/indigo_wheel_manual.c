82da4f4f9a5a7e451aad360b1c1d01b966601f3c
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2025-05-04 19:56:03 +0200

    wheel_manual: cleanup

--- indigo_drivers/wheel_manual/indigo_wheel_manual.c
+++ indigo_drivers/wheel_manual/indigo_wheel_manual.c
@@ -1,7 +1,7 @@
 // Copyright (c) 2020-2025 Rumen G.Bogdanovski
 // All rights reserved.
 
-// You can use this software under the terms of 'INDIGO Astronomy
+// You may use this software under the terms of 'INDIGO Astronomy
 // open-source license' (see LICENSE.md).
 
 // THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS
