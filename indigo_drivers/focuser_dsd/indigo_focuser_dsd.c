e40c203d37e30462d4eb14dbe1b6339fd309da92
Rumen G.Bogdanovski <rumenastro@gmail.com>
2025-10-06 21:55:36 +0300

    indigo_focuser_dsd: fix self lock

--- indigo_drivers/focuser_dsd/indigo_focuser_dsd.c
+++ indigo_drivers/focuser_dsd/indigo_focuser_dsd.c
@@ -184,12 +184,10 @@ static bool dsd_command(indigo_device *device, const char *command, char *respon
 }
 
 static void dsd_close(indigo_device *device) {
-	pthread_mutex_lock(&PRIVATE_DATA->port_mutex);
 	if (PRIVATE_DATA->handle != NULL) {
 		indigo_uni_close(&PRIVATE_DATA->handle);
 		INDIGO_DRIVER_LOG(DRIVER_NAME, "Disconnected from %s", DEVICE_PORT_ITEM->text.value);
 	}
-	pthread_mutex_unlock(&PRIVATE_DATA->port_mutex);
 }
 
 static bool dsd_get_info(indigo_device *device, char *board, char *firmware) {
