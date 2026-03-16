9164ebe6a32ee533cd933a3c797aa97e23031fa7
Rumen G.Bogdanovski <rumenastro@gmail.com>
2026-03-17 01:20:08 +0200

    indigo_agent_imager: disk usage will not flood the log if path does not exist

--- indigo_drivers/agent_imager/indigo_agent_imager.c
+++ indigo_drivers/agent_imager/indigo_agent_imager.c
@@ -23,7 +23,7 @@
  \file indigo_agent_imager.c
  */
 
-#define DRIVER_VERSION 0x0037
+#define DRIVER_VERSION 0x0038
 #define DRIVER_NAME	"indigo_agent_imager"
 
 #include <stdio.h>
@@ -378,7 +378,11 @@ static bool get_disk_usage(const char* path, double* total_mb, double* free_mb, 
 		return true;
 	}
 
-	INDIGO_DRIVER_ERROR(DRIVER_NAME, "statvfs() failed for '%s': %s", path, strerror(errno));
+	if (errno != ENOENT) {
+		INDIGO_DRIVER_ERROR(DRIVER_NAME, "statvfs() failed for '%s': %s", path, strerror(errno));
+	} else {
+		INDIGO_DRIVER_DEBUG(DRIVER_NAME, "statvfs() failed for '%s': Path not found", path);
+	}
 	return false;
 #endif
 }
