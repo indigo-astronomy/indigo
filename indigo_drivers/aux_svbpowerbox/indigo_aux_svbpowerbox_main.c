7dc1e01cb870414c260dfe829cbf7062d9dd1311
Rumen G.Bogdanovski <rumenastro@gmail.com>
2026-04-03 17:20:41 +0300

    indigo_aux_svbpowerbox: migrated to autogenerator

--- indigo_drivers/aux_svbpowerbox/indigo_aux_svbpowerbox_main.c
+++ indigo_drivers/aux_svbpowerbox/indigo_aux_svbpowerbox_main.c
@@ -1,9 +1,9 @@
 // Copyright (C) 2026 Rumen G. Bogdanovski
 // All rights reserved.
-//
-// You can use this software under the terms of 'INDIGO Astronomy
+
+// You may use this software under the terms of 'INDIGO Astronomy
 // open-source license' (see LICENSE.md).
-//
+
 // THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS
 // OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -16,12 +16,7 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// version history
-// 2.0 by Rumen G. Bogdanovski <rumenastro@gmail.com>
-
-/** INDIGO SVBONY PowerBox aux driver main
- \file indigo_aux_svbpowerbox_main.c
- */
+// This file generated from indigo_aux_svbpowerbox.driver
 
 #include <indigo/indigo_driver_xml.h>
 
