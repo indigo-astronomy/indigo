103b412c1734b12239c12dc48244fe8800f6604e
Keith <5109586+keithbe@users.noreply.github.com>
2026-04-15 08:32:18 +1000

    simplify negation of angles

--- indigo_drivers/mount_nexstaraux/indigo_mount_nexstaraux.c
+++ indigo_drivers/mount_nexstaraux/indigo_mount_nexstaraux.c
@@ -324,12 +324,8 @@ static void position_timer_callback(indigo_device *device) {
 	}
 	if (raw_alt != -1 && raw_azm != -1) {
 		if (MOUNT_GEOGRAPHIC_COORDINATES_LATITUDE_ITEM->number.value < 0) {
-			if (raw_alt != 0){
-				raw_alt = 0x1000000 - raw_alt;
-			}
-			if (raw_azm != 0) {
-				raw_azm = 0x1000000 - raw_azm;
-			}
+			raw_alt = (0x1000000 - raw_alt) & 0x0FFFFFF;
+			raw_azm = (0x1000000 - raw_azm) & 0x0FFFFFF;
 		}
 		double ha = fmod(((double)raw_azm / 0x1000000) * 24 + 12, 24);
 		double ra = fmod(indigo_lst(NULL, MOUNT_GEOGRAPHIC_COORDINATES_LONGITUDE_ITEM->number.value) - ha + 24, 24);
@@ -418,12 +414,8 @@ static void mount_equatorial_coordinates_handler(indigo_device *device) {
 	int32_t raw_azm = (int32_t)((fmod(ha + 12, 24) / 24.0) * 0x1000000) % 0x1000000;
 	int32_t raw_alt = (int32_t)((dec / 360.0) * 0x1000000) % 0x1000000;
 	if (MOUNT_GEOGRAPHIC_COORDINATES_LATITUDE_ITEM->number.value < 0) {
-		if (raw_alt != 0) {
-			raw_alt = 0x1000000 - raw_alt;
-		}
-		if (raw_azm != 0) {
-			raw_azm = 0x1000000 - raw_azm;
-		}
+			raw_alt = (0x1000000 - raw_alt) & 0x0FFFFFF;
+			raw_azm = (0x1000000 - raw_azm) & 0x0FFFFFF;
 	}
 	if (!nexstaraux_command_24(device, APP, AZM, MOUNT_ON_COORDINATES_SET_SYNC_ITEM->sw.value ? MC_SET_POSITION : MC_GOTO_FAST, raw_azm, reply) || !nexstaraux_command_24(device, APP, ALT, MOUNT_ON_COORDINATES_SET_SYNC_ITEM->sw.value ? MC_SET_POSITION : MC_GOTO_FAST, raw_alt, reply)) {
 		MOUNT_EQUATORIAL_COORDINATES_PROPERTY->state = INDIGO_ALERT_STATE;
