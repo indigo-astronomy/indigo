aab79c25903e07154c28d0f2076ad51d1047e512
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2026-08-14 22:57:05 +0200

    Refactoring of webGUI, visual cleanup

--- indigo_server/resource/components.js
+++ indigo_server/resource/components.js
@@ -1943,7 +1943,7 @@ app.component('indigo-query-db', {
 	template: `
 		<div class="w-100">
 			<div class="input-group p-1 w-100">
-				<div class="input-group-text btn-svg">&#x1f50d;</div>
+				<div class="input-group-text input-label ok-state"><svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.099zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg></div>
 				<input type="text" class="form-control" @change="onChange">
 			</div>
 			<div v-if="result != null && result.length > 0" class="list-group list-group-flush p-1 mt-1 w-100" style="max-height: 10rem; overflow-y: scroll">
